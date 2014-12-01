6e69a2ec2e50f36800a07a46bac65da52a18bfd0
Jon Evans <jon@craftyjon.com>
2014-11-30 20:55:42 -0500

    Wake on plug [1/2]
    Change-Id: I5e50a7fd351857d76c1c2fd13c6e737d8902a2f3
    Signed-off-by: Arghya Chanda <arghyac35@gmail.com>
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Signed-off-by: Anush02198 <Anush.4376@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5621,6 +5621,13 @@ public final class Settings {
         public static final String USE_OLD_MOBILETYPE = "use_old_mobiletype";   
 
         /**
+         * Whether to wake the display when plugging or unplugging the charger
+         *
+         * @hide
+         */
+        public static final String WAKE_WHEN_PLUGGED_OR_UNPLUGGED = "wake_when_plugged_or_unplugged";
+
+        /**
          * IMPORTANT: If you add a new public settings you also have to add it to
          * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
          * it to PRIVATE_SETTINGS below. Also add a validator that can validate
