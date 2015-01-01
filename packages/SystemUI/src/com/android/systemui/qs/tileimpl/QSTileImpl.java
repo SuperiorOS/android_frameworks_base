a17ef4c37155b51b601071e2bc1c3bc70460f44b
Evisceration <eviscerationls@gmail.com>
2015-01-01 14:29:50 +0000

    Add haptic feedback to QS tiles [1/2]
    Change-Id: I792a00a2689a572110963db00362f8e9d77b316e
    Signed-off-by: Jayant-Deshmukh <jayantdeshmuk008@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSTileImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSTileImpl.java
@@ -36,6 +36,7 @@ import android.os.Handler;
 import android.os.Looper;
 import android.os.Message;
 import android.os.UserHandle;
+import android.os.Vibrator;
 import android.provider.Settings;
 import android.service.quicksettings.Tile;
 import android.text.format.DateUtils;
@@ -117,6 +118,8 @@ public abstract class QSTileImpl<TState extends State> implements QSTile, Lifecy
 
     abstract protected void handleUpdateState(TState state, Object arg);
 
+    protected Vibrator mVibrator;
+
     @Override
     public boolean isDualTarget() {
         return false;
@@ -133,6 +136,7 @@ public abstract class QSTileImpl<TState extends State> implements QSTile, Lifecy
     protected QSTileImpl(QSHost host) {
         mHost = host;
         mContext = host.getContext();
+        mVibrator = (Vibrator) mContext.getSystemService(Context.VIBRATOR_SERVICE);
     }
 
     @NonNull
@@ -193,6 +197,18 @@ public abstract class QSTileImpl<TState extends State> implements QSTile, Lifecy
 
     // safe to call from any thread
 
+    public boolean isVibrationEnabled() {
+        return (Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.QUICK_SETTINGS_TILES_VIBRATE, 0, UserHandle.USER_CURRENT) == 1);
+    }
+
+    public void vibrateTile(int duration) {
+        if (!isVibrationEnabled()) { return; }
+        if (mVibrator != null) {
+            if (mVibrator.hasVibrator()) { mVibrator.vibrate(duration); }
+        }
+    }
+
     public void addCallback(Callback callback) {
         mHandler.obtainMessage(H.ADD_CALLBACK, callback).sendToTarget();
     }
@@ -210,6 +226,7 @@ public abstract class QSTileImpl<TState extends State> implements QSTile, Lifecy
                 .addTaggedData(FIELD_STATUS_BAR_STATE,
                         mStatusBarStateController.getState())));
         mHandler.sendEmptyMessage(H.CLICK);
+        vibrateTile(60);
     }
 
     public void secondaryClick() {
@@ -229,6 +246,7 @@ public abstract class QSTileImpl<TState extends State> implements QSTile, Lifecy
                 mContext,
                 Prefs.Key.QS_LONG_PRESS_TOOLTIP_SHOWN_COUNT,
                 QuickStatusBarHeader.MAX_TOOLTIP_SHOWN_COUNT);
+        vibrateTile(60);
     }
 
     public LogMaker populate(LogMaker logMaker) {
