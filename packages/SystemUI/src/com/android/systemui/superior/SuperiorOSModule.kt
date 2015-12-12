20b4beed098bb596af75c28a34235f6dbc3c901b
Adnan Begovic <adnan@cyngn.com>
2015-12-11 16:16:02 -0800

    SystemUI: Add AmbientDisplay tile
    Squashed with:
    
    Author: Adnan Begovic <adnan@cyngn.com>
    Date:   Fri Dec 11 16:16:02 2015 -0800
    
        SystemUI: Readd AmbientDisplayTile.
    
        Change-Id: Ie7b93ba0f87d505206b5d60f193c2234d83d7eac
    
    Author: Michael Bestas <mkbestas@lineageos.org>
    Date:   Sun Sep 17 22:51:15 2017 +0300
    
        SystemUI: Hide ambient display tile if device does not support it
    
        * Add a check similar to the Settings app
    
        Change-Id: I603cd28b8dd1fadff9ffcfb6953c7b72514b5857
    
    Author: Luca Stefani <luca.stefani.ge1@gmail.com>
    Date:   Wed Mar 17 22:34:17 2021 +0100
    
        SystemUI: AmbientTile: Don't hardcode display settings intent
    
        Change-Id: Ifaa69c2f235b85ae847524c81f0ba59e2a51cf01
    
    Author: Luca Stefani <luca.stefani.ge1@gmail.com>
    Date:   Wed Mar 17 22:37:04 2021 +0100
    
        SystemUI: AmbientTile: Use SecureSetting to change doze setting
    
        Change-Id: I7f004bcf45493130ad50faacb6e390cd640e53db
    
    Author: LuK1337 <priv.luk@gmail.com>
    Date:   Wed Mar 17 22:40:49 2021 +0100
    
        SystemUI: AmbientTile: Actually start using SecureSetting
    
        Change-Id: I5d94f6d2e596dbc6b44ae4f30f479d335a067661
    
    Author: Luca Stefani <luca.stefani.ge1@gmail.com>
    Date:   Thu Mar 18 10:51:54 2021 +0100
    
        SystemUI: AmbientTile: Default the setting to 1
    
        Change-Id: Idea9dda702fdbaa0e2836e7a2573aa57cf24449e
    
    Change-Id: Ia873302af950bd8bffc14102beb202feefdbf8c7
    
    Change-Id: Iea339685f523016876f059052e66e97fd5170a81
    Signed-off-by: S I P U N <sipunkumar85@gmail.com>

--- packages/SystemUI/src/com/android/systemui/superior/SuperiorOSModule.kt
+++ packages/SystemUI/src/com/android/systemui/superior/SuperiorOSModule.kt
@@ -18,6 +18,7 @@
 package com.android.systemui.superior
 
 import com.android.systemui.qs.tileimpl.QSTileImpl
+import com.android.systemui.qs.tiles.AmbientDisplayTile
 import com.android.systemui.qs.tiles.CaffeineTile
 import com.android.systemui.qs.tiles.CellularTile
 import com.android.systemui.qs.tiles.CompassTile
@@ -37,6 +38,12 @@ import dagger.multibindings.StringKey
 
 @Module
 interface SuperiorOSModule {
+
+    /** Inject AmbientDisplayTile into tileMap in QSModule */
+    @Binds
+    @IntoMap
+    @StringKey(AmbientDisplayTile.TILE_SPEC)
+    fun bindAmbientDisplayTile(ambientDisplayTile: AmbientDisplayTile): QSTileImpl<*>
 
     /** Inject CaffeineTile into tileMap in QSModule */
     @Binds
