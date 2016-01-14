4a5c6f3e5a21bfde13662ecde6b3dba00da1adfe
beanstown106 <nbenis106@gmail.com>
2016-01-14 05:56:50 -0500

    base: Fingerprint authentication vibration [1/2]
    had a few requests for this, as most devices only vibrate
    when fingerprint authentication is unsucesful so made it configurable
    
    xyyx: adapted to 12,13-qpr2
    
    Change-Id: Ib2444f91cdba9973dc3142bba8cf6d349e983c8b
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5851,6 +5851,18 @@ public final class Settings {
         public static final String QS_TILE_LABEL_SIZE = "qs_tile_label_size";
 
         /**
+         * Whether to enable or disable vibration on succesful fingerprint auth
+         * @hide
+         */
+        public static final String FP_SUCCESS_VIBRATE = "fp_success_vibrate";
+
+        /**
+         * Whether to enable or disable vibration on failed fingerprint auth
+         * @hide
+         */
+        public static final String FP_ERROR_VIBRATE = "fp_error_vibrate";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
