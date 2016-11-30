8933cb3d347536150ae920d50846e93c884b5a18
Joe Maples <joe@frap129.org>
2016-11-30 21:13:15 +0000

    Edit Icon Toggle [1/2]
    aex edits:
    * Adapt for oreo
    * Cleanup
    
    Adds toggle for the quick tiles edit
    
    Change-Id: I496ea08eed21eea9e852d0f4760d615d2067f9e4
    Signed-off-by: Joe Maples <joe@frap129.org>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5764,6 +5764,12 @@ public final class Settings {
         public static final String QSFOOTER_SHOW_SERVICES = "qs_footer_show_services";
 
         /**
+         * Whether to show or hide the edit icon
+         * @hide
+         */
+        public static final String QS_EDIT_TOGGLE = "qs_edit_toggle";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
