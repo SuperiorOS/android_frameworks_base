09700b97a3de67a5c182fcd114307f7d52afe809
Jon Haus <haus.xda@gmail.com>
2017-01-05 00:20:06 +0100

    base: Network traffic indicator [1/2]
    SQUASH of the followings:
    
    - Network Traffic [1/2]
    - Network traffic: fix indicator not hiding on lost connection
    - Network Traffic: Bring back traffic indicator for all
    - Network Traffic: OOS style network traffic indicator
    - Network Traffic: Refactor text formatting
    - Network Traffic: Adjust size/text style for extended statusbar
    - Traffic indicators: code cleanup and improvements
    - Traffic indicators: use smaller and bold fonts in expanded statusbar
    - Traffic indicators: add properly to statusbar icons
    - Traffic indicators: Prevent logspam caused by improperly calling requestLayout()
    - NetworkTraffic: Fix speed format for 10*KB
    - base: Fix net traffic view being white on QQS Header
    
    [ReallySnow: Remove expanded QS traffic and fix the traffic showing on the QS when the lockscreen slides down]
    
    Co-authored-by: LibXZR <i@xzr.moe>
    Co-authored-by: ReallySnow <reallysnow233@gmail.com>
    Co-authored-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Co-authored-by: SpiritCroc <spiritcroc@gmail.com>
    Co-authored-by: Joey Huab <joey@evolution-x.org>
    Co-authored-by: ezio84 <brabus84@gmail.com>
    Co-authored-by: SpiritCroc <dev@spiritcroc.de>
    Co-authored-by: ganeshi4u <ganeshvarmai4u@gmail.com>
    Change-Id: I46ebe42dd1287d9dd60eae2a3631818627e7de32
    Signed-off-by: S I P U N <sipunkumar85@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarIconController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarIconController.java
@@ -17,6 +17,7 @@ package com.android.systemui.statusbar.phone;
 import static com.android.systemui.statusbar.phone.StatusBarIconHolder.TYPE_BINDABLE;
 import static com.android.systemui.statusbar.phone.StatusBarIconHolder.TYPE_ICON;
 import static com.android.systemui.statusbar.phone.StatusBarIconHolder.TYPE_MOBILE_NEW;
+import static com.android.systemui.statusbar.phone.StatusBarIconHolder.TYPE_NETWORK_TRAFFIC;
 import static com.android.systemui.statusbar.phone.StatusBarIconHolder.TYPE_WIFI_NEW;
 
 import android.annotation.Nullable;
@@ -51,6 +52,7 @@ import com.android.systemui.statusbar.pipeline.shared.ui.view.ModernStatusBarVie
 import com.android.systemui.statusbar.pipeline.wifi.ui.WifiUiAdapter;
 import com.android.systemui.statusbar.pipeline.wifi.ui.view.ModernStatusBarWifiView;
 import com.android.systemui.statusbar.pipeline.wifi.ui.viewmodel.LocationBasedWifiViewModel;
+import com.android.systemui.statusbar.policy.NetworkTrafficSB;
 import com.android.systemui.util.Assert;
 
 import java.util.ArrayList;
@@ -198,8 +200,10 @@ public interface StatusBarIconController {
 
         @Override
         public void onSetIcon(int viewIndex, StatusBarIcon icon) {
-            super.onSetIcon(viewIndex, icon);
-            mDarkIconDispatcher.applyDark((DarkReceiver) mGroup.getChildAt(viewIndex));
+            View view = mGroup.getChildAt(viewIndex);
+            if (view instanceof StatusBarIconView) {
+                ((StatusBarIconView) view).set(icon);
+            }
         }
 
         @Override
@@ -439,6 +443,9 @@ public interface StatusBarIconController {
                 case TYPE_BINDABLE:
                     // Safe cast, since only BindableIconHolders can set this tag on themselves
                     return addBindableIcon((BindableIconHolder) holder, index);
+
+                case TYPE_NETWORK_TRAFFIC:
+                    return addNetworkTraffic(index, slot);
             }
 
             return null;
@@ -465,6 +472,12 @@ public interface StatusBarIconController {
             return view;
         }
 
+        protected NetworkTrafficSB addNetworkTraffic(int index, String slot) {
+            NetworkTrafficSB view = onCreateNetworkTraffic(slot);
+            mGroup.addView(view, index, onCreateLayoutParams());
+            return view;
+        }
+
         protected StatusIconDisplayable addNewWifiIcon(int index, String slot) {
             ModernStatusBarWifiView view = onCreateModernStatusBarWifiView(slot);
             mGroup.addView(view, index, onCreateLayoutParams());
@@ -472,7 +485,6 @@ public interface StatusBarIconController {
             if (mIsInDemoMode) {
                 mDemoStatusIcons.addModernWifiView(mWifiViewModel);
             }
-
             return view;
         }
 
@@ -515,6 +527,12 @@ public interface StatusBarIconController {
                             slot,
                             mMobileIconsViewModel.viewModelForSub(subId, mLocation)
                         );
+        }
+
+        private NetworkTrafficSB onCreateNetworkTraffic(String slot) {
+            NetworkTrafficSB view = new NetworkTrafficSB(mContext);
+            view.setPadding(2, 0, 2, 0);
+            return view;
         }
 
         protected LinearLayout.LayoutParams onCreateLayoutParams() {
