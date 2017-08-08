904e857daafff9bd9c276c0740d3ca4388b90e70
Andrzej Ressel <jereksel@gmail.com>
2017-08-09 00:11:16 +0200

    base: SystemUI: Allow user to add/remove QS with one click
    Change-Id: I9250fe5fd6a3edf567f1f4d30fd86838f5ef4610
    
    Restore one-click to add/remove tiles after r20 merge
    
    Google now added the possibility to double click to move tiles
    for accessibility purposes but i actually prefer the oneclick way
    
    Change-Id: Ia44fc4e2ddc77c9cf918c2b8fea38a4e9a3c8252
    Signed-off-by: Joey Huab <joey@evolution-x.org>
    Signed-off-by: S I P U N <sipunkumar85@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/customize/TileAdapter.java
+++ packages/SystemUI/src/com/android/systemui/qs/customize/TileAdapter.java
@@ -112,7 +112,7 @@ public class TileAdapter extends RecyclerView.Adapter<Holder> implements TileSta
     private int mAccessibilityAction = ACTION_NONE;
     private int mAccessibilityFromIndex;
     private final UiEventLogger mUiEventLogger;
-    private final AccessibilityDelegateCompat mAccessibilityDelegate;
+    //private final AccessibilityDelegateCompat mAccessibilityDelegate;
     @Nullable
     private RecyclerView mRecyclerView;
     private int mNumColumns;
@@ -140,7 +140,7 @@ public class TileAdapter extends RecyclerView.Adapter<Holder> implements TileSta
                 ? context.getResources().getInteger(
                         R.integer.small_land_lockscreen_quick_settings_num_columns)
                 : context.getResources().getInteger(NUM_COLUMNS_ID);
-        mAccessibilityDelegate = new TileAdapterDelegate();
+        //mAccessibilityDelegate = new TileAdapterDelegate();
         mSizeLookup.setSpanIndexCacheEnabled(true);
         mTempTextView = new TextView(context);
         mMinTileViewHeight = context.getResources().getDimensionPixelSize(R.dimen.qs_tile_height);
@@ -416,13 +416,27 @@ public class TileAdapter extends RecyclerView.Adapter<Holder> implements TileSta
         // Don't show the side view for third party tiles, as we don't have the actual state.
         tileView.setShowSideView(position < mEditIndex || info.isSystem);
         holder.mTileView.setSelected(true);
-        holder.mTileView.setImportantForAccessibility(View.IMPORTANT_FOR_ACCESSIBILITY_YES);
+        /*holder.mTileView.setImportantForAccessibility(View.IMPORTANT_FOR_ACCESSIBILITY_YES);
         holder.mTileView.setClickable(true);
         holder.mTileView.setOnClickListener(null);
         holder.mTileView.setFocusable(true);
-        holder.mTileView.setFocusableInTouchMode(true);
+        holder.mTileView.setFocusableInTouchMode(true);*/
 
-        if (mAccessibilityAction != ACTION_NONE) {
+        holder.mTileView.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int position = holder.getLayoutPosition();
+                if (position < mEditIndex) {
+                    if (canRemoveTiles()) {
+                        move(position, mEditIndex);
+                    }
+                } else {
+                    move(position, mEditIndex);
+                }
+            }
+        });
+
+        /*if (mAccessibilityAction != ACTION_NONE) {
             holder.mTileView.setClickable(selectable);
             holder.mTileView.setFocusable(selectable);
             holder.mTileView.setFocusableInTouchMode(selectable);
@@ -444,7 +458,7 @@ public class TileAdapter extends RecyclerView.Adapter<Holder> implements TileSta
         }
         if (position == mFocusIndex) {
             focusOnHolder(holder);
-        }
+        }*/
     }
 
     private void focusOnHolder(Holder holder) {
@@ -605,7 +619,7 @@ public class TileAdapter extends RecyclerView.Adapter<Holder> implements TileSta
                 mTileView = (QSTileViewImpl) ((FrameLayout) itemView).getChildAt(0);
                 mTileView.getIcon().disableAnimation();
                 mTileView.setTag(this);
-                ViewCompat.setAccessibilityDelegate(mTileView, mAccessibilityDelegate);
+                //ViewCompat.setAccessibilityDelegate(mTileView, mAccessibilityDelegate);
             }
         }
 
