86f31104af60be9429c8635c915f3a0726df1c99
ezio84 <brabus84@gmail.com>
2017-08-31 21:24:26 +0200

    base: Add some static custom utilities
    Add api to toggle flashlight and check if device has flashlight
    
    statusbar service getter borrowed from PhoneWindowManager code
    
    use PackageManager to check if the device has flashlight
    
    use aosp flashcontroller to handle the flash toggle like the flashlight
    tile does
    
    Change-Id: I652e5f8ce60b72794618f2303ce3c043a062686a

--- core/java/com/android/internal/util/superior/SuperiorUtils.java
+++ core/java/com/android/internal/util/superior/SuperiorUtils.java
@@ -33,6 +33,7 @@ import android.os.Handler;
 import android.os.Looper;
 import android.os.PowerManager;
 import android.os.RemoteException;
+import android.os.ServiceManager;
 import android.os.SystemProperties;
 import android.os.SystemClock;
 import android.text.format.Time;
@@ -46,6 +47,8 @@ import android.view.IWindowManager;
 import android.view.WindowManagerGlobal;
 
 import com.android.internal.R;
+
+import com.android.internal.statusbar.IStatusBarService;
 
 import java.util.Arrays;
 import java.util.List;
@@ -197,6 +200,38 @@ public class SuperiorUtils {
         PowerManager pm = (PowerManager) ctx.getSystemService(Context.POWER_SERVICE);
         if (pm!= null) {
             pm.goToSleep(SystemClock.uptimeMillis());
+        }
+    }
+
+    public static boolean deviceHasFlashlight(Context ctx) {
+        return ctx.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH);
+    }
+
+    public static void toggleCameraFlash() {
+        FireActions.toggleCameraFlash();
+    }
+
+    private static final class FireActions {
+        private static IStatusBarService mStatusBarService = null;
+        private static IStatusBarService getStatusBarService() {
+            synchronized (FireActions.class) {
+                if (mStatusBarService == null) {
+                    mStatusBarService = IStatusBarService.Stub.asInterface(
+                            ServiceManager.getService("statusbar"));
+                }
+                return mStatusBarService;
+            }
+        }
+
+        public static void toggleCameraFlash() {
+            IStatusBarService service = getStatusBarService();
+            if (service != null) {
+                try {
+                    service.toggleCameraFlash();
+                } catch (RemoteException e) {
+                    // do nothing.
+                }
+            }
         }
     }
 
