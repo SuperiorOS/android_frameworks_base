f199142a404fb09c9e3782fd2399d191ec3b9ab7
beanstown106 <nbenis106@gmail.com>
2017-09-05 14:34:10 +0800

    base: incall vibration options [1/3]
     *allow setting vibration when call is connected
     *allow setting vibration when call is disconnected
     *allow setting vibration for call waiting
     *this works with google and aosp dialer :)
    
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Change-Id: I14a403189f37e45fa4b999ab015bde8f89ae3da4
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Signed-off-by: Jayant-Deshmukh <jayantdeshmuk008@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5184,6 +5184,24 @@ public final class Settings {
         public static final String TORCH_POWER_BUTTON_GESTURE = "torch_power_button_gesture";
 
         /**
+         * Whether the phone vibrates on call connect
+         * @hide
+         */
+        public static final String VIBRATE_ON_CONNECT = "vibrate_on_connect";
+
+         /**
+         * Whether the phone vibrates on call waiting
+         * @hide
+         */
+        public static final String VIBRATE_ON_CALLWAITING = "vibrate_on_callwaiting";
+
+         /**
+         * Whether the phone vibrates on disconnect
+         * @hide
+         */
+        public static final String VIBRATE_ON_DISCONNECT = "vibrate_on_disconnect";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
