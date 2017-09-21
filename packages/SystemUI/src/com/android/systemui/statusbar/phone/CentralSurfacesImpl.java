8c479aadb3d297b1296bf082da38f455a8fbf356
bigrushdog <randall.rushing@gmail.com>
2017-09-20 21:40:59 -0700

    Hardware key: Customization support [1/2]
    Based on DUI
    
    xyyx:
    -adapted to P
    -moved utils to utils
    -res to SystemUI
    -changed animation for lastapp
    -removed pinned action
    -commented removing from recents
    -disabled some actions for now
    
    jhenrique09 edits: make it DUI independent
    
    Original commit:
    Squashed commit of the following:
    
    Manually bring in
    
    DUI: Migrate to API/Permission model for Editor [1/5]
    bigrushdog/android_frameworks_base@20681de
    
    commit d3dd2b5df493989f3bbb479a5734df3a84d9598a
    Author: bigrushdog <randall.rushing@gmail.com>
    Date:   Fri Aug 11 18:15:22 2017 -0700
    
        DUtils: Migrate from broadcasts to API for some action handling [1/2]
    
        Protected broadcasts for ActionHandler actions will become problematic
        in the future. Currently, broadcasts are sent from SystemUI as well as
        system server (hwkeys). This may also change in the future. So provide
        a general API solution with properly secured public methods.
    
        Change-Id: I90ee0dd5fe75545d50ca77dd0fecba86ab23a09e
    
    commit 6a528e1261649a404ee70ea74dda1551f53a30e1
    Author: bigrushdog <randall.rushing@gmail.com>
    Date:   Wed Aug 2 16:10:54 2017 -0700
    
        DUI: Left in landscape navbar API [1/3]
    
        Window manager will let SystemUI know when navbar
        enters or exits the "left in landscape" position.
        We tried to avoid this using orientation listeners
        but that approach proved unreliable leaving Pulse
        in bizarre states when navbar was left in landscape
    
        Change-Id: Ie67b905755cd20219f08e76232a10fde50ec9b85
    
    commit 7e8cb8b0cfa47d0b1e1105f97905167efafac851
    Author: Randall Rushing <randall.rushing@gmail.com>
    Date:   Thu Oct 29 05:02:04 2015 -0700
    
        Pulse: forward port visualizer lock api
    
        Change-Id: I13c7bbc8f4dc79fa41f11cbab25c1cdd83aed2af
    
    commit 593d60fcea0a2edfebb1ed9ebc21ab69238cb7e7
    Author: Randall Rushing <randall.rushing@gmail.com>
    Date:   Thu Jan 7 02:41:02 2016 -0800
    
        statusbar: add screenPinningStateChanged(boolean enabled) api and pipe to navigation bar
    
        Let activity stack notify statusbar when task locking state changes. This is needed for
        alternate task managers (OmniSwitch, SlimRecents) that implement screen pinning but don't
        interact with the screen pinning pop-up. NavigationController needs to be notified of the
        event to load a secure navigation bar.
    
        Change-Id: I948667509d448bb91bea9bd560820d8bbf456f3f
    
    commit ef32637c25689fdd577a8b022991ea22a86b55c5
    Author: bigrushdog <randall.rushing@gmail.com>
    Date:   Thu Aug 25 01:44:40 2016 -0700
    
        Bring back some removed statusbar API
    
        Some of the needed statusbar API was moved to internal implementations
        for N. Expose needed methods for DUtils library
    
        Change-Id: Ie8b19b44698159e288a3ff901208a35dfac855d6
    
    commit bd0009c7da3a03f64b3c6979d6b378b159edb24f
    Author: Randall Rushing <randall.rushing@gmail.com>
    Date:   Sun Feb 14 03:51:02 2016 -0800
    
        Buttons: Bring up adapted DU hardware key action binding framework
    
    commit a030cf0ed4cc7e4bde297a63dc848e238f599440
    Author: Max <xyyx@mail.ru>
    
        HardkeyActionHandler: Adapt to Android 11
    
        Change-Id: Ib389815cc66b77ad852db4538e68b9245897aa51
    
    commit a3f606e12c761dc38e737b33a0aaf4eae421166d
    Author: Max <xyyx@mail.ru>
    
        HwKeys: Rework kill logic with LOS code
    
        Change-Id: I4065e717db1a962c2bc517aaf7cc04d51f3bbd30
    
    Stallix: Adapt to 13 and amend with proper authorship.
    
    Change-Id: I436ab30b7f44dafc89f8369ee1225ea177d29ac9
    
    Co-authored-by: jhenrique09 <jhsv09@gmail.com>
    Co-authored-by: Max <xyyx@mail.ru>
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    Change-Id: I218e878c6c87c53053aeadf96d2509ea5159dc70
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
@@ -75,6 +75,7 @@ import android.net.Uri;
 import android.os.Binder;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.Process;
 import android.os.Looper;
 import android.os.PowerManager;
 import android.os.RemoteException;
@@ -124,6 +125,11 @@ import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
 import com.android.internal.statusbar.IStatusBarService;
 import com.android.internal.statusbar.RegisterStatusBarResult;
 import com.android.keyguard.AuthKeyguardMessageArea;
+import com.android.internal.util.hwkeys.ActionConstants;
+import com.android.internal.util.hwkeys.ActionUtils;
+import com.android.internal.util.hwkeys.PackageMonitor;
+import com.android.internal.util.hwkeys.PackageMonitor.PackageChangedListener;
+import com.android.internal.util.hwkeys.PackageMonitor.PackageState;
 import com.android.keyguard.KeyguardUpdateMonitor;
 import com.android.keyguard.KeyguardUpdateMonitorCallback;
 import com.android.keyguard.ViewMediatorCallback;
@@ -283,7 +289,7 @@ import dagger.Lazy;
  * </b>
  */
 @SysUISingleton
-public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces, TunerService.Tunable {
+public class CentralSurfacesImpl implements CoreStartable, PackageChangedListener, CentralSurfaces, TunerService.Tunable {
 
     private static final String BANNER_ACTION_CANCEL =
             "com.android.systemui.statusbar.banner_action_cancel";
@@ -566,6 +572,8 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces, Tune
 
     private final DisplayMetrics mDisplayMetrics;
 
+    private PackageMonitor mPackageMonitor;
+
     // XXX: gesture research
     private final GestureRecorder mGestureRec = DEBUG_GESTURES
         ? new GestureRecorder("/sdcard/statusbar_gestures.dat")
@@ -617,6 +625,7 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces, Tune
         }
     }
 
+    private Handler mMainHandler;
     private final DelayableExecutor mMainExecutor;
 
     private int mInteractingWindows;
@@ -781,6 +790,7 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces, Tune
             LockscreenShadeTransitionController lockscreenShadeTransitionController,
             FeatureFlags featureFlags,
             KeyguardUnlockAnimationController keyguardUnlockAnimationController,
+            @Main Handler mainHandler,
             @Main DelayableExecutor delayableExecutor,
             @Main MessageRouter messageRouter,
             WallpaperManager wallpaperManager,
@@ -871,6 +881,7 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces, Tune
         mStatusBarHideIconsForBouncerManager = statusBarHideIconsForBouncerManager;
         mFeatureFlags = featureFlags;
         mKeyguardUnlockAnimationController = keyguardUnlockAnimationController;
+        mMainHandler = mainHandler;
         mMainExecutor = delayableExecutor;
         mMessageRouter = messageRouter;
         mWallpaperManager = wallpaperManager;
@@ -956,6 +967,10 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces, Tune
         mStatusBarHideIconsForBouncerManager.setDisplayId(mDisplayId);
 
         initShadeVisibilityListener();
+
+        mPackageMonitor = new PackageMonitor();
+        mPackageMonitor.register(mContext, mMainHandler);
+        mPackageMonitor.addListener(this);
 
         // start old BaseStatusBar.start().
         mWindowManagerService = WindowManagerGlobal.getWindowManagerService();
@@ -2913,6 +2928,26 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces, Tune
                                 animationController,
                                 getActivityUserHandle(intent)),
                 delay);
+    }
+
+    @Override
+    public void onPackageChanged(String pkg, PackageState state) {
+        if (state == PackageState.PACKAGE_REMOVED
+                || state == PackageState.PACKAGE_CHANGED) {
+            final Context ctx = mContext;
+            final Thread thread = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    if (!ActionUtils.hasNavbarByDefault(ctx)) {
+                        ActionUtils.resolveAndUpdateButtonActions(ctx,
+                                ActionConstants
+                                        .getDefaults(ActionConstants.HWKEYS));
+                    }
+                }
+            });
+            thread.setPriority(Process.THREAD_PRIORITY_BACKGROUND);
+            thread.start();
+        }
     }
 
     @Override
