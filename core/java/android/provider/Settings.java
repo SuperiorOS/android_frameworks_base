5969371721ff8a4a00147b2b9008da4102be2f0c
Rashed Abdel-Tawab <rashed@linux.com>
2017-10-03 17:17:37 -0400

    SystemUI: Add double tap to sleep gesture [1/2]
    Author: Roman Birg <roman@cyngn.com>
    Date:   Sun Nov 23 06:54:06 2014 -0800
    
        SystemUI: double tap to sleep improvements
    
        * Make it more reliable
        * Add it to keyguard
        * Add a content observer to not always query Settings.System on every
        touch event
    
        Change-Id: I292c4d9d9f810843590b7a9ec6e15b99ac44009d
        Signed-off-by: Roman Birg <roman@cyngn.com>
    
    Author: Adnan Begovic <adnan@cyngn.com>
    Date:   Wed Nov 11 12:05:59 2015 -0800
    
        fw: Move DOUBLE_TAP_SLEEP_GESTURE to CMSettings.
    
        Change-Id: I8274b7c241cef6835a1114a702e68c95b6d2e036
    
    Author: Zhao Wei Liew <zhaoweiliew@gmail.com>
    Date:   Fri Oct 7 08:56:25 2016 +0800
    
        SystemUI: Use Tuner API for CM settings
    
        Get rid of all the excess code by implementing TunerService.Tunable
        and observing any changes made to the settings through it.
    
        Remove UserContentObserver as the Tuner API handles user switches.
    
        Also remove some unused imports that were introduced in earlier
        CM commits, but were never removed since.
    
        Change-Id: Iecafafabdaec82b3b3c72293bea865de48f0e90a
    
    Author: Altaf-Mahdi <altaf.mahdi@gmail.com>
    Date:   Wed Nov 11 16:07:49 2015 -0500
    
        Double tap to sleep anywhere on the lock screen [1/3]
    
        Change-Id: I7dd46f3fafeb2e629974c0f32083d4d9774fb1de
        [neo: Using Tuner API.]
        Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    Author: dianlujitao <dianlujitao@lineageos.org>
    Date:   Thu Feb 27 12:57:07 2020 +0800
    
        SystemUI: Don't sleep on double tap below status bar
    
        Change-Id: Ic64c29eae63e96f34dc37cda355401b7e2cd2d39
    
    Author: Arian <arian.kulmer@web.de>
    Date:   2020-12-27 14:35:33 +0100
    
        NotificationPanelViewController: Fix DT2S gesture handling
    
        Change-Id: I9f2d3c63d397c998bea5137f747a9ad95ae2c746
    
    [Pig]: Forward port to R
    [bgcngm]: Forward port to S
    
    Co-authored-by: Michael W <baddaemon87@gmail.com>
    Change-Id: I7489204e348906dcf6e34fa04f2121974c22ddb9
    Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5628,6 +5628,12 @@ public final class Settings {
         public static final String LESS_BORING_HEADS_UP = "less_boring_heads_up";
 
         /**
+         * Enable statusbar double tap gesture to put device to sleep
+         * @hide
+         */
+        public static final String DOUBLE_TAP_SLEEP_GESTURE = "double_tap_sleep_gesture";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
