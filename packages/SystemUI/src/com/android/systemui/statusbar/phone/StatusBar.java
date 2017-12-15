db3969d09ba1ec377f9d7192cb82bd814b01228c
maxwen <max.weninger@gmail.com>
2017-12-15 19:55:46 +0100

    Configure SystemUI theme [1/2]
    arrow edits: adapt for Pie
    
    In 8.1 several elements change depending on the dominant color of the homescreen
    wallpaper. If is a light/white wallpaper these elements will turn white and if is
    a dark/black wallpaper then these elements will turn black. If is a neutral color
    then it defaults to the light/white color.
    
    This commits makes all of this configurable and so places the option in the user's
    hands. They can either leave it up to automatic selection (wallpaper api) or force
    a light or dark theme.
    
    -- Commit message was rewritten to better explain what's up :-)
    
    Original commit message:
    
    allow to force light or dark or use the wallpaper based
    automatic selection
    
    Change-Id: Id7c287ea82cf76e6d3189c5621258cefe2c4d6d2
    Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -3977,16 +3977,25 @@ public class StatusBar extends SystemUI implements DemoMode,
      * Switches theme from light to dark and vice-versa.
      */
     protected void updateTheme() {
-        final boolean inflated = mStackScroller != null && mStatusBarWindowManager != null;
-
-        // The system wallpaper defines if QS should be light or dark.
-        WallpaperColors systemColors = mColorExtractor
-                .getWallpaperColors(WallpaperManager.FLAG_SYSTEM);
-        final boolean wallpaperWantsDarkTheme = systemColors != null
-                && (systemColors.getColorHints() & WallpaperColors.HINT_SUPPORTS_DARK_THEME) != 0;
+        final boolean inflated = mStackScroller != null;
+        int userThemeSetting = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.SYSTEM_UI_THEME, 0, mCurrentUserId);
+        boolean useDarkTheme = false;
+        if (userThemeSetting == 0) {
+            // The system wallpaper defines if QS should be light or dark.
+            WallpaperColors systemColors = mColorExtractor
+                    .getWallpaperColors(WallpaperManager.FLAG_SYSTEM);
+            useDarkTheme = systemColors != null
+                    && (systemColors.getColorHints() & WallpaperColors.HINT_SUPPORTS_DARK_THEME) != 0;
             // Check for black and white accent so we don't end up
             // with white on white or black on black
             unfuckBlackWhiteAccent();
+        } else {
+            useDarkTheme = userThemeSetting == 2;
+            // Check for black and white accent so we don't end up
+            // with white on white or black on black
+            unfuckBlackWhiteAccent();
+        }
 
         if (isUsingDarkTheme() != useDarkTheme) {
             mUiOffloadThread.submit(() -> {
@@ -4920,6 +4929,9 @@ public class StatusBar extends SystemUI implements DemoMode,
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.ACCENT_PICKER),
                     false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.SYSTEM_UI_THEME),
+                    false, this, UserHandle.USER_ALL);
         }
 
         @Override
@@ -4949,6 +4961,8 @@ public class StatusBar extends SystemUI implements DemoMode,
          public void update() {
             setLockscreenDoubleTapToSleep();
             setQsRowsColumns();
+            setStatusDoubleTapToSleep();
+	    updateTheme();
         }
     }
 
