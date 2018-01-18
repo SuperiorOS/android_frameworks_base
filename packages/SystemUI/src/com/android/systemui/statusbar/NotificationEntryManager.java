ff6e147fb82fe58b5b1ac01fc7605b2123e629b7
ezio84 <brabus84@gmail.com>
2018-01-18 23:25:14 +0100

    Ambient display music ticker [1/3]
    Trigger an Ambient Display notification, even if Ambient
    Display is disabled, so we can know playing track title/artist.
    This is handy when the player starts playing a new track or even
    when we use volume rocker skip track feature when screen is off.
    A new track will force an ambient notification but with a clean
    layout without clock and other elements, but track info in the middle
    of the screen.
    
    Double tapping on track info allows to skip the track (even for
    remote Spotify/KdeConnect or other similar apps).
    
    We use provided Doze services to skip this when the proximity
    sensor is covered or system Ambient events are pending.
    
    If the feature is disabled, track info is still shown by default
    on keyguard and ambient when these modes are called by the system
    or the user.
    
    Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>
    Change-Id: Ibe8caa3a146c4ff78eed886ee8b8ce25596ed388

--- packages/SystemUI/src/com/android/systemui/statusbar/NotificationEntryManager.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/NotificationEntryManager.java
@@ -131,6 +131,7 @@ public class NotificationEntryManager implements Dumpable, NotificationInflater.
     private ExpandableNotificationRow.OnAppOpsClickListener mOnAppOpsClickListener;
 
     private boolean mLessBoringHeadsUp;
+    private NotificationData.Entry mEntryToRefresh;
 
     /**
      * Notifications with keys in this set are not actually around anymore. We kept them around
@@ -473,6 +474,21 @@ public class NotificationEntryManager implements Dumpable, NotificationInflater.
             mPresenter.updateNotificationViews();
         }
         entry.row.setLowPriorityStateUpdated(false);
+
+        if (mEntryToRefresh == entry) {
+            final Notification n = entry.notification.getNotification();
+            String notificationText = null;
+            final String title = n.extras.getString(Notification.EXTRA_TITLE);
+            final String text = n.extras.getString(Notification.EXTRA_TEXT);
+            if (!TextUtils.isEmpty(title) && !TextUtils.isEmpty(text)) {
+                notificationText = title + " - " + text;
+            }
+            mMediaManager.setMediaNotificationText(notificationText);
+        }
+    }
+
+    public void setEntryToRefresh(NotificationData.Entry entry) {
+        mEntryToRefresh = entry;
     }
 
     @Override
--- packages/SystemUI/src/com/android/systemui/statusbar/NotificationMediaManager.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/NotificationMediaManager.java
@@ -72,13 +72,7 @@ public class NotificationMediaManager implements Dumpable {
                     clearCurrentMediaNotification();
                     mPresenter.updateMediaMetaData(true, true);
                 }
-                if (mStatusBar != null) {
-                    mStatusBar.getVisualizer().setPlaying(state.getState()
-                            == PlaybackState.STATE_PLAYING);
-                }
-            }
-            if (mListener != null) {
-                mListener.onMediaUpdated(isPlaybackActive(state.getState()));
+                setMediaPlaying();
             }
         }
 
@@ -90,17 +84,13 @@ public class NotificationMediaManager implements Dumpable {
             }
             mMediaMetadata = metadata;
             mPresenter.updateMediaMetaData(true, true);
-            if (mListener != null) {
-                mListener.onMediaUpdated(isPlaybackActive());
-            }
+            setMediaPlaying();
         }
 
         @Override
         public void onSessionDestroyed() {
             super.onSessionDestroyed();
-            if (mListener != null) {
-                mListener.onMediaUpdated(isPlaybackActive());
-            }
+            setMediaPlaying();
         }
     };
 
@@ -216,6 +206,7 @@ public class NotificationMediaManager implements Dumpable {
                     mListener.onMediaUpdated(isPlaybackActive());
                 }
                 mMediaMetadata = mMediaController.getMetadata();
+                setMediaPlaying();
                 if (DEBUG_MEDIA) {
                     Log.v(TAG, "DEBUG_MEDIA: insert listener, found new controller: "
                             + mMediaController + ", receive metadata: " + mMediaMetadata);
@@ -313,10 +304,46 @@ public class NotificationMediaManager implements Dumpable {
                         + mMediaController.getPackageName());
             }
             mMediaController.unregisterCallback(mMediaListener);
-            if (mListener != null) {
-                mListener.onMediaUpdated(isPlaybackActive());
-            }
+            setMediaPlaying();
         }
         mMediaController = null;
     }
+
+  public void setMediaPlaying() {
+        if (PlaybackState.STATE_PLAYING ==
+                getMediaControllerPlaybackState(mMediaController)
+                || PlaybackState.STATE_BUFFERING ==
+                getMediaControllerPlaybackState(mMediaController)) {
+
+            ArrayList<NotificationData.Entry> activeNotifications =
+                    mEntryManager.getNotificationData().getAllNotifications();
+            int N = activeNotifications.size();
+            final String pkg = mMediaController.getPackageName();
+
+            for (int i = 0; i < N; i++) {
+                final NotificationData.Entry entry = activeNotifications.get(i);
+                if (entry.notification.getPackageName().equals(pkg)) {
+                    // NotificationEntryManager onAsyncInflationFinished will get called
+                    // when colors and album are loaded for the notification, then we can send
+                    // those info to Pulse
+                    mEntryManager.setEntryToRefresh(entry);
+                    break;
+                }
+            }
+            if (mListener != null) {
+                mListener.onMediaUpdated(true);
+            }
+        } else {
+            mEntryManager.setEntryToRefresh(null);
+            mPresenter.setAmbientMusicInfo(null, null);
+            if (mListener != null) {
+                mListener.onMediaUpdated(false);
+            }
+        }
+    }
+
+    public void setMediaNotificationText(String notificationText) {
+        mPresenter.setAmbientMusicInfo(mMediaMetadata, notificationText);
+    }
 }
+
