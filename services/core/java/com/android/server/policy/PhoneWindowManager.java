39dcb9c52d8cd578a7e52944249348b568830ae1
maxwen <max.weninger@gmail.com>
2018-01-25 15:03:47 +0100

    base: Screenrecord tile: save quality level
    save state set in qs tile into settings
    
    Change-Id: I708b324a22c3e605fa6411c1a9e1de28b584938b
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
    Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -215,6 +215,7 @@ import android.os.IBinder;
 import android.os.IDeviceIdleController;
 import android.os.Looper;
 import android.os.Message;
+import android.os.Messenger;
 import android.os.PowerManager;
 import android.os.PowerManagerInternal;
 import android.os.Process;
@@ -2133,7 +2134,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
 
     @Override
     public void screenRecordAction(int mode) {
-        mContext.enforceCallingOrSelfPermission(Manifest.permission.ACCESS_SURFACE_FLINGER,
+        mContext.enforceCallingOrSelfPermission(ACCESS_SURFACE_FLINGER,
                 TAG + "screenRecordAction permission denied");
         mHandler.removeCallbacks(mScreenrecordRunnable);
         mScreenrecordRunnable.setMode(mode);
