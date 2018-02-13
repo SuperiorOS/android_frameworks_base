8a844fd694e6f68adb1087b95d883a5f8a3e6e9f
ezio84 <brabus84@gmail.com>
2018-02-14 00:24:18 +0100

    Battery Styles: Show a bolt ⚡ when charging
    * @ neobuddy89: Only for hidden and text battery styles.
    
    Change-Id: I27f745e3458fda7c415a75d6e52f183e327cb498
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/battery/BatteryMeterView.java
+++ packages/SystemUI/src/com/android/systemui/battery/BatteryMeterView.java
@@ -221,6 +221,7 @@ public class BatteryMeterView extends LinearLayout implements DarkReceiver {
         if (mCharging != pluggedIn) {
             mCharging = pluggedIn;
             updateShowPercent();
+            updatePercentText();
         }
     }
 
@@ -304,7 +305,12 @@ public class BatteryMeterView extends LinearLayout implements DarkReceiver {
     private void setPercentTextAtCurrentLevel() {
         if (mBatteryPercentView != null) {
             mEstimateText = null;
-            String percentText = NumberFormat.getPercentInstance().format(mLevel / 100f);
+            // Use the high voltage symbol ⚡ (u26A1 unicode) but prevent the system
+            // to load its emoji colored variant with the uFE0E flag
+            String bolt = "\u26A1\uFE0E";
+            CharSequence mChargeIndicator = mCharging && (getBatteryStyle() == BATTERY_STYLE_PORTRAIT ||
+                    getBatteryStyle() == BATTERY_STYLE_TEXT) ? (bolt + " ") : "";
+            String percentText = mChargeIndicator + NumberFormat.getPercentInstance().format(mLevel / 100f);
             // Setting text actually triggers a layout pass (because the text view is set to
             // wrap_content width and TextView always relayouts for this). Avoid needless
             // relayout if the text didn't actually change.
