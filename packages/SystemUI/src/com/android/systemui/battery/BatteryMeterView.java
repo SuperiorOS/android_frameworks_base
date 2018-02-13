44af88e294302c079624ccaedd3adf1a22b2f116
ezio84 <brabus84@gmail.com>
2018-02-14 00:24:18 +0100

    Battery Styles: Show a bolt ⚡ when charging
    * @neobuddy89: Only for hidden and text battery styles.
    
    Change-Id: I27f745e3458fda7c415a75d6e52f183e327cb498
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/battery/BatteryMeterView.java
+++ packages/SystemUI/src/com/android/systemui/battery/BatteryMeterView.java
@@ -292,6 +292,7 @@ public class BatteryMeterView extends LinearLayout implements DarkReceiver {
             mAccessorizedDrawable.setBatteryLevel(mLevel);
             mCircleDrawable.setBatteryLevel(mLevel);
             mFullCircleDrawable.setBatteryLevel(mLevel);
+            updatePercentText();
         }
         if (mPluggedIn != pluggedIn) {
             mPluggedIn = pluggedIn;
@@ -299,7 +300,6 @@ public class BatteryMeterView extends LinearLayout implements DarkReceiver {
             mCircleDrawable.setCharging(isCharging());
             mFullCircleDrawable.setCharging(isCharging());
             updateShowPercent();
-        } else {
             updatePercentText();
         }
 
@@ -505,11 +505,17 @@ public class BatteryMeterView extends LinearLayout implements DarkReceiver {
                 }
             });
         } else {
+            // Use the high voltage symbol ⚡ (u26A1 unicode) but prevent the system
+            // to load its emoji colored variant with the uFE0E flag
+            String bolt = "\u26A1\uFE0E";
+            CharSequence mChargeIndicator = mCharging && (mBatteryStyle == BATTERY_STYLE_HIDDEN ||
+                    mBatteryStyle == BATTERY_STYLE_TEXT) ? (bolt + " ") : "";
+            String percentText = mChargeIndicator + text;
             // Setting text actually triggers a layout pass (because the text view is set to
             // wrap_content width and TextView always relayouts for this). Avoid needless
             // relayout if the text didn't actually change.
-            if (!TextUtils.equals(mBatteryPercentView.getText(), text)) {
-                mBatteryPercentView.setText(text);
+            if (!TextUtils.equals(mBatteryPercentView.getText(), percentText)) {
+                mBatteryPercentView.setText(percentText);
             }
         }
 
