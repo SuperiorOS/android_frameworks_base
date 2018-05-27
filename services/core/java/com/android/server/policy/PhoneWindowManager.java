ee5affea31c75612542951a627a8de775fdab5c5
Vol Zhdanov <wight554@gmail.com>
2018-05-27 14:14:25 +0000

    Screenrecorder: add support for 18:9 resolutions
    * Using higher aspect ratio bool added earlier
    
    Change-Id: Ic735a7359ba819a68ce3a626eaad718277a2d040
    Signed-off-by: Anirudh Gupta <anirudhgupta109@gmail.com>
    Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -2134,7 +2134,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
 
     @Override
     public void screenRecordAction(int mode) {
-        mContext.enforceCallingOrSelfPermission(ACCESS_SURFACE_FLINGER,
+        mContext.enforceCallingOrSelfPermission(Manifest.permission.ACCESS_SURFACE_FLINGER,
                 TAG + "screenRecordAction permission denied");
         mHandler.removeCallbacks(mScreenrecordRunnable);
         mScreenrecordRunnable.setMode(mode);
