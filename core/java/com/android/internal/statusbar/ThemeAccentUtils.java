ae7ee4924ceb2151a297fc0cb42da11220cde361
drpradsms <dr.pradsms@gmail.com>
2018-07-06 08:53:46 +0530

    Support Theming for some inbuilt apps [2/2] Themes- Dialer, Contacts, Filemanager
    Inspired by Domination theme
    
    Change-Id: Icf1c9d6ad4fc03a68f7e6a2dfaebba7c781e2db0
    Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>

--- core/java/com/android/internal/statusbar/ThemeAccentUtils.java
+++ core/java/com/android/internal/statusbar/ThemeAccentUtils.java
@@ -55,18 +55,25 @@ public class ThemeAccentUtils {
     };
 
     private static final String[] DARK_THEMES = {
-        "com.android.system.theme.dark.superior", // 0
-        "com.android.settings.theme.dark.superior", // 1
-        "com.android.systemui.theme.dark.superior", // 2
+        "com.android.system.theme.dark", // 0
+        "com.android.settings.theme.dark", // 1
+        "com.android.systemui.theme.dark", // 2
+        "com.android.dialer.theme.dark", //3
+        "com.android.contacts.theme.dark", //4
+        "com.android.documentsui.theme.dark", //5
     };
 
     private static final String[] BLACK_THEMES = {
-        "com.android.system.theme.black.superior", // 0
-        "com.android.settings.theme.black.superior", // 1
-        "com.android.systemui.theme.black.superior", // 2
+        "com.android.system.theme.black", // 0
+        "com.android.settings.theme.black", // 1
+        "com.android.systemui.theme.black", // 2
+        "com.android.dialer.theme.black", //3
+        "com.android.contacts.theme.black", //4
+        "com.android.documentsui.theme.black", //5
     };
 
-    private static final String STOCK_DARK_THEME = "com.android.systemui.theme.dark.superior";
+
+    private static final String STOCK_DARK_THEME = "com.android.systemui.theme.dark";
 
     // Switches theme accent from to another or back to stock
     public static void updateAccents(IOverlayManager om, int userId, int accentSetting) {
