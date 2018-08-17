e090a39a605d3fb6fca448991e9f236b7f44f215
maxwen <max.weninger@gmail.com>
2018-08-17 16:04:35 +0200

    base: SystemUI: enable tuner and show in Settings
    Change-Id: I4d3ff3ca989e6a6952419d147208318ee269b9d7
    Signed-off-by: xyyx <xyyx@mail.ru>
    Signed-off-by: S I P U N <sipunkumar85@gmail.com>

--- packages/SystemUI/src/com/android/systemui/tuner/TunerServiceImpl.java
+++ packages/SystemUI/src/com/android/systemui/tuner/TunerServiceImpl.java
@@ -131,6 +131,7 @@ public class TunerServiceImpl extends TunerService {
         };
         mUserTracker.addCallback(mCurrentUserTracker,
                 new HandlerExecutor(mainHandler));
+        setTunerEnabled(true);
     }
 
     @Override
