cafbaabe39f908bde07bd8dba1df73ed53655da0
Alex Cruz <du.alexcruz@gmail.com>
2018-09-12 20:31:45 +0530

    Lockscreen Clock Widget Fonts [1/2]
    SuperDroidBond-:
    Forward Port to Pie.
    Add all new fonts for LS.
    Now we got 30+ options to choose from
    
    This is @beanstown106's idea, we got talking while messing around with some layouts
    and decided that this should be a mod and so here it is :-)
    
    Currently there are 12 fonts to include default font, will add more as time goes by
    
    http://i.imgur.com/adKTfsM.png
    
    http://i.imgur.com/XyaACLw.jpg
    
    BenzoEdit:
    Fixed the fonts being tiny

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4611,6 +4611,13 @@ public final class Settings {
         public static final String SCREENSHOT_DEFAULT_MODE = "screenshot_default_mode";
 
         /**
+	 ** Change fonts for the system lockscreen clock widget
+	 **
+	 ** @hide
+	 **/
+        public static final String LOCK_CLOCK_FONTS = "lock_clock_fonts";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
