1711e685f98ad2911ec287b7e52c6616b9d40028
davidevinavil <davidevinavil@gmail.com>
2018-09-21 02:10:35 +0200

    Home wake up: Make home button wake up opt-in [2/2]
    Change-Id: I5fa3c4ee62da681913f307907458a014d7567604
    Signed-off-by: mhkjahromi <m.h.k.jahromi@gmail.com>
    Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -572,6 +572,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     DisplayRotation mDefaultDisplayRotation;
     DisplayPolicy mDefaultDisplayPolicy;
 
+    // The home button wake
+    boolean mHomeWakeButton;
+
     // What we do when the user long presses on home
     private int mLongPressOnHomeBehavior;
 
@@ -882,6 +885,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                     UserHandle.USER_ALL);
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.VOLUME_ANSWER_CALL), false, this,
+                    UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.HOME_WAKE_BUTTON), false, this,
                     UserHandle.USER_ALL);
             updateSettings();
         }
@@ -2278,6 +2284,10 @@ public class PhoneWindowManager implements WindowManagerPolicy {
 
             mVolumeAnswerCall = (Settings.System.getIntForUser(resolver,
                     Settings.System.VOLUME_ANSWER_CALL, 0, UserHandle.USER_CURRENT) == 1);
+
+	    // home wake button
+            mHomeWakeButton = Settings.System.getIntForUser(resolver,
+                    Settings.System.HOME_WAKE_BUTTON, 0, UserHandle.USER_CURRENT) != 0;
 
             // Configure wake gesture.
             boolean wakeGestureEnabledSetting = Settings.Secure.getIntForUser(resolver,
@@ -4298,7 +4308,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             }
 
             case KeyEvent.KEYCODE_HOME:
-                if (down && !interactive) {
+                if (down && !interactive && mHomeWakeButton) {
                     isWakeKey = true;
                 }
                 break;
