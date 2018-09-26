7cde35a9e965ac2d4982c3d5f73679ad9640659c
Pranav Vashi <neobuddy89@gmail.com>
2018-09-26 23:14:47 +0530

    SystemUI: CompassTile: Set default state inactive
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: I259c251ff4b9740a09c4d31d770f82549bff1eae
    Signed-off-by: GhostMaster69-dev <rathore6375@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tiles/CompassTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/CompassTile.java
@@ -145,6 +145,7 @@ public class CompassTile extends QSTileImpl<BooleanState> implements SensorEvent
         state.value = mActive;
 
         if (state.value) {
+            state.state = Tile.STATE_ACTIVE;
             state.icon = ResourceIcon.get(R.drawable.ic_qs_compass_on);
             if (arg != null) {
                 state.label = formatValueWithCardinalDirection(degrees);
@@ -166,6 +167,7 @@ public class CompassTile extends QSTileImpl<BooleanState> implements SensorEvent
             state.label = mContext.getString(R.string.quick_settings_compass_label);
             state.contentDescription = mContext.getString(
                     R.string.accessibility_quick_settings_compass_off);
+            state.state = Tile.STATE_INACTIVE;
             if (mImage != null) {
                 mImage.setRotation(0);
             }
