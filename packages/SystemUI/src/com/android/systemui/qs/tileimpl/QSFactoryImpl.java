46c4b2e907a09cb2cc571a547c7ffa080e240d0d
ZeNiXxX <vitjahermann@gmail.com>
2018-11-07 22:51:41 +0530

    SystemUI: Add Camera tile
    Added to pie by @ZeNiXxX
    
    Change-Id: Ib3761dbbd607482b5fa750f6b81e97bdca4b930e
    Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -29,6 +29,7 @@ import com.android.systemui.qs.tiles.BluetoothTile;
 import com.android.systemui.qs.tiles.CastTile;
 import com.android.systemui.qs.tiles.CellularTile;
 import com.android.systemui.qs.tiles.CaffeineTile;
+import com.android.systemui.qs.tiles.CameraTile;
 import com.android.systemui.qs.tiles.ColorInversionTile;
 import com.android.systemui.qs.tiles.DataSaverTile;
 import com.android.systemui.qs.tiles.DndTile;
@@ -134,6 +135,8 @@ public class QSFactoryImpl implements QSFactory {
                 return new CalendarTile(mHost);
             case "call":
                 return new CallTile(mHost);
+            case "camera":
+                return new CameraTile(mHost);
         }
 
         // Intent tiles.
