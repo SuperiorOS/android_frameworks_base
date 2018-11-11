bd0ace6d27a1f29e299f6ca09071aed1c6333cc3
Nicolas Dhouailly <ndhouailly@gmail.com>
2018-11-11 18:04:32 +0100

    Battery styles : Fix icon portrait level
    * The icon portrait mode was always displaying full level. Lets fix it.
    * Clean up code a little bit.
    
    Change-Id: Ibb6b6814d288c3e3e449009c5741a926adef1372
    Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>

--- packages/SettingsLib/src/com/android/settingslib/graph/BatteryMeterDrawableBase.java
+++ packages/SettingsLib/src/com/android/settingslib/graph/BatteryMeterDrawableBase.java
@@ -105,6 +105,7 @@ public class BatteryMeterDrawableBase extends Drawable {
 
     private final Path mShapePath = new Path();
     private final Path mOutlinePath = new Path();
+    private final Path mClipPath = new Path();
     private final Path mTextPath = new Path();
 
     private DashPathEffect mPathEffect;
@@ -363,6 +364,9 @@ public class BatteryMeterDrawableBase extends Drawable {
         final int left = mPadding.left + bounds.left;
         final int top = bounds.bottom - mPadding.bottom - height;
 
+        mFrame.set(left, top, width + left, height + top);
+        mFrame.offset(px, 0);
+
         mFramePaint.setStrokeWidth(0);
         mFramePaint.setStyle(Paint.Style.FILL_AND_STROKE);
         mBatteryPaint.setStrokeWidth(0);
@@ -390,12 +394,8 @@ public class BatteryMeterDrawableBase extends Drawable {
             drawFrac = 0f;
         }
 
-        final float levelTop;
-        if (drawFrac == 1f) {
-            levelTop = mButtonFrame.top;
-        } else {
-            levelTop = (mFrame.top + (mFrame.height() * (1f - drawFrac)));
-        }
+        final float levelTop = drawFrac == 1f ? mButtonFrame.top
+                : (mFrame.top + (mFrame.height() * (1f - drawFrac)));
 
         // define the battery shape
         mShapePath.reset();
@@ -412,10 +412,11 @@ public class BatteryMeterDrawableBase extends Drawable {
         if (mCharging) {
             // define the bolt shape
             // Shift right by 1px for maximal bolt-goodness
-            final float bl = mFrame.left + mFrame.width() / (4f + 1);
+            final float bl = mFrame.left + mFrame.width() / 4f + 1;
             final float bt = mFrame.top + mFrame.height() / 6f;
-            final float br = mFrame.right - mFrame.width() / (4f + 1);
+            final float br = mFrame.right - mFrame.width() / 4f + 1;
             final float bb = mFrame.bottom - mFrame.height() / 10f;
+
             if (mBoltFrame.left != bl || mBoltFrame.top != bt
                     || mBoltFrame.right != br || mBoltFrame.bottom != bb) {
                 mBoltFrame.set(bl, bt, br, bb);
@@ -507,6 +508,10 @@ public class BatteryMeterDrawableBase extends Drawable {
         c.clipRect(mFrame);
         c.drawPath(mShapePath, mBatteryPaint);
         c.restore();
+
+        mClipPath.reset();
+        mClipPath.addRect(mFrame, Path.Direction.CCW);
+        mShapePath.op(mClipPath, Path.Op.INTERSECT);
 
         if (!mCharging && !mPowerSaveEnabled) {
             if (level <= mCriticalLevel) {
