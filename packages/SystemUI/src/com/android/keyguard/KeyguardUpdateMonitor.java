0eb404eb306eb33af7389cceb34594296a70c8f7
Scott Warner <Tortel1210@gmail.com>
2018-11-18 18:48:26 -0500

    SystemUI: Keyguard: Check for a null errString
    Change-Id: I9ae365d83176267002edf8c7eacaeac077a05c66

--- packages/SystemUI/src/com/android/keyguard/KeyguardUpdateMonitor.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardUpdateMonitor.java
@@ -1752,7 +1752,8 @@ public class KeyguardUpdateMonitor implements TrustManager.TrustListener, Dumpab
                 @Override
                 public void onAuthenticationError(int errMsgId, CharSequence errString) {
                     Trace.beginSection("KeyguardUpdateMonitor#onAuthenticationError");
-                    handleFingerprintError(errMsgId, errString.toString());
+                    handleFingerprintError(errMsgId, errString != null
+                            ? errString.toString() : "");
                     Trace.endSection();
                 }
 
