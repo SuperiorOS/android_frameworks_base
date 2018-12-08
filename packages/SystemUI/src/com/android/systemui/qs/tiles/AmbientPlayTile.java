syberia-project/platform_frameworks_base

QS tiles: add ambient play tile
icons from materialdesignicons.com

Change-Id: I2ac074d80bba18dd3882a2740ca69c85e4c58602
Signed-off-by: DennySPB <dennyspb@gmail.com>

packages/SystemUI/src/com/android/systemui/qs/tiles/AmbientPlayTile.java
@@ -1,0 +1,125 @@
+/*
+ * Copyright (C) 2018 Syberia Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.systemui.qs.tiles;
+
+import android.content.Intent;
+import android.content.ComponentName;
+import android.os.Build;
+import android.os.SystemProperties;
+import android.provider.Settings;
+import android.provider.Settings.System;
+import android.service.quicksettings.Tile;
+import android.text.TextUtils;
+
+import com.android.systemui.plugins.qs.QSTile.BooleanState;
+import com.android.systemui.qs.QSHost;
+import com.android.systemui.qs.tileimpl.QSTileImpl;
+import com.android.systemui.qs.SystemSetting;
+import com.android.systemui.R;
+
+import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
+
+public class AmbientPlayTile extends QSTileImpl<BooleanState> {
+
+    private static final Intent AMBIENT_PLAY_SETTINGS = new Intent().setComponent(new ComponentName(
+            "com.android.settings", "com.android.settings.Settings$AmbientPlaySettingsActivity"));
+
+    private final SystemSetting mSetting;
+
+    public AmbientPlayTile(QSHost host) {
+        super(host);
+
+        mSetting = new SystemSetting(mContext, mHandler, System.AMBIENT_RECOGNITION) {
+            @Override
+            protected void handleValueChanged(int value, boolean observedChange) {
+                handleRefreshState(value);
+            }
+        };
+    }
+
+    @Override
+    public boolean isAvailable() {
+        return true;
+    }
+
+    @Override
+    public BooleanState newTileState() {
+        return new BooleanState();
+    }
+
+    @Override
+    protected void handleClick() {
+        setEnabled(!mState.value);
+        refreshState();
+    }
+
+    @Override
+    public Intent getLongClickIntent() {
+        return AMBIENT_PLAY_SETTINGS;
+    }
+
+    private void setEnabled(boolean enabled) {
+        Settings.System.putInt(mContext.getContentResolver(),
+                Settings.System.AMBIENT_RECOGNITION,
+                enabled ? 1 : 0);
+    }
+
+    @Override
+    protected void handleUpdateState(BooleanState state, Object arg) {
+        final int value = arg instanceof Integer ? (Integer)arg : mSetting.getValue();
+        final boolean enable = value != 0;
+        state.value = enable;
+        state.label = mContext.getString(R.string.quick_settings_ambient_play);
+        if (enable) {
+            state.icon = ResourceIcon.get(R.drawable.ic_qs_ambient_play_on);
+            state.contentDescription =  mContext.getString(
+                    R.string.quick_settings_ambient_play);
+            state.state = Tile.STATE_ACTIVE;
+        } else {
+            state.icon = ResourceIcon.get(R.drawable.ic_qs_ambient_play_off);
+            state.contentDescription =  mContext.getString(
+                    R.string.quick_settings_ambient_play);
+            state.state = Tile.STATE_INACTIVE;
+        }
+    }
+
+    @Override
+    public CharSequence getTileLabel() {
+        return mContext.getString(R.string.quick_settings_ambient_play);
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsEvent.SYBERIA;
+    }
+
+    @Override
+    protected String composeChangeAnnouncement() {
+        if (mState.value) {
+            return mContext.getString(
+                    R.string.quick_settings_ambient_play);
+        } else {
+            return mContext.getString(
+                    R.string.quick_settings_ambient_play);
+        }
+    }
+
+    @Override
+    public void handleSetListening(boolean listening) {
+        // Do nothing
+    }
+}

SuperiorOS/android_frameworks_base

Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>

packages/SystemUI/src/com/android/systemui/qs/tiles/AmbientPlayTile.java
@@ -1,0 +1,125 @@
+/*
+ * Copyright (C) 2018 Syberia Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.systemui.qs.tiles;
+
+import android.content.Intent;
+import android.content.ComponentName;
+import android.os.Build;
+import android.os.SystemProperties;
+import android.provider.Settings;
+import android.provider.Settings.System;
+import android.service.quicksettings.Tile;
+import android.text.TextUtils;
+
+import com.android.systemui.plugins.qs.QSTile.BooleanState;
+import com.android.systemui.qs.QSHost;
+import com.android.systemui.qs.tileimpl.QSTileImpl;
+import com.android.systemui.qs.SystemSetting;
+import com.android.systemui.R;
+
+import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
+
+public class AmbientPlayTile extends QSTileImpl<BooleanState> {
+
+    private static final Intent AMBIENT_PLAY_SETTINGS = new Intent().setComponent(new ComponentName(
+            "com.android.settings", "com.android.settings.Settings$AmbientPlaySettingsActivity"));
+
+    private final SystemSetting mSetting;
+
+    public AmbientPlayTile(QSHost host) {
+        super(host);
+
+        mSetting = new SystemSetting(mContext, mHandler, System.AMBIENT_RECOGNITION) {
+            @Override
+            protected void handleValueChanged(int value, boolean observedChange) {
+                handleRefreshState(value);
+            }
+        };
+    }
+
+    @Override
+    public boolean isAvailable() {
+        return true;
+    }
+
+    @Override
+    public BooleanState newTileState() {
+        return new BooleanState();
+    }
+
+    @Override
+    protected void handleClick() {
+        setEnabled(!mState.value);
+        refreshState();
+    }
+
+    @Override
+    public Intent getLongClickIntent() {
+        return AMBIENT_PLAY_SETTINGS;
+    }
+
+    private void setEnabled(boolean enabled) {
+        Settings.System.putInt(mContext.getContentResolver(),
+                Settings.System.AMBIENT_RECOGNITION,
+                enabled ? 1 : 0);
+    }
+
+    @Override
+    protected void handleUpdateState(BooleanState state, Object arg) {
+        final int value = arg instanceof Integer ? (Integer)arg : mSetting.getValue();
+        final boolean enable = value != 0;
+        state.value = enable;
+        state.label = mContext.getString(R.string.quick_settings_ambient_play);
+        if (enable) {
+            state.icon = ResourceIcon.get(R.drawable.ic_qs_ambient_play_on);
+            state.contentDescription =  mContext.getString(
+                    R.string.quick_settings_ambient_play);
+            state.state = Tile.STATE_ACTIVE;
+        } else {
+            state.icon = ResourceIcon.get(R.drawable.ic_qs_ambient_play_off);
+            state.contentDescription =  mContext.getString(
+                    R.string.quick_settings_ambient_play);
+            state.state = Tile.STATE_INACTIVE;
+        }
+    }
+
+    @Override
+    public CharSequence getTileLabel() {
+        return mContext.getString(R.string.quick_settings_ambient_play);
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsEvent.SUPERIOR;
+    }
+
+    @Override
+    protected String composeChangeAnnouncement() {
+        if (mState.value) {
+            return mContext.getString(
+                    R.string.quick_settings_ambient_play);
+        } else {
+            return mContext.getString(
+                    R.string.quick_settings_ambient_play);
+        }
+    }
+
+    @Override
+    public void handleSetListening(boolean listening) {
+        // Do nothing
+    }
+}