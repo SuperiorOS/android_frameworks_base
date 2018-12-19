168703e641ecde11bca99e5950d13cd3f0273beb
a.derendyaev <a.derendyaev@magdv.com>
2018-12-19 11:53:23 +0700

    Switching audio panel position [1/2]
    Change-Id: I6ebb5c6201a8f1e8e90dde005c23e505d874db8f
    Signed-off-by: a.derendyaev <a.derendyaev@magdv.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4727,6 +4727,9 @@ public final class Settings {
         /** @hide */
         public static final String AUDIO_PANEL_VIEW_BT_SCO = "audio_panel_view_bt_sco";
 
+        /** @hide */
+        public static final String AUDIO_PANEL_VIEW_POSITION = "audio_panel_view_position";
+
         /**
          * 0 - wallpaper based
          * 1 - force light
