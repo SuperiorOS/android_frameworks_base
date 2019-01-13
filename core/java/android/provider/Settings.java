00f3e28a3803f55ae57b11b879c4f317cf8b40d2
Alex Cruz <du.alexcruz@gmail.com>
2019-01-13 10:59:49 -0600

    Bring back traffic indicator for all!
    arrow-edits:
     - adapt to Q
    
    Change-Id: Ia41474a262b258f8a2d4d7b10a15891729ffd17c
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
    Signed-off-by: Arghya Chanda <arghyac35@gmail.com>
    Signed-off-by: Jayant-Deshmukh <jayantdeshmuk008@gmail.com>
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    Signed-off-by: Jayant-Deshmukh <jayantdeshmuk008@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5159,6 +5159,8 @@ public final class Settings {
          */
         public static final String QS_BRIGHTNESS_POSITION_BOTTOM = "qs_brightness_position_bottom";
 
+
+
         /**
          * Whether to show QS brightness slider above qs footer
          * Only applies when {@link QS_BRIGHTNESS_POSITION_BOTTOM} is enabled
@@ -5203,6 +5205,18 @@ public final class Settings {
          * @hide
          */
         public static final String FORCE_SHOW_NAVBAR = "navigation_bar_show_new";
+
+         /**
+         * Wheter to show network traffic indicator in statusbar
+         * @hide
+         */
+        public static final String NETWORK_TRAFFIC_STATE = "network_traffic_state";
+
+        /**
+         * Network traffic inactivity threshold (default is 1 kBs)
+         * @hide
+         */
+        public static final String NETWORK_TRAFFIC_AUTOHIDE_THRESHOLD = "network_traffic_autohide_threshold";
 
         /**
          * Keys we no longer back up under the current schema, but want to continue to
