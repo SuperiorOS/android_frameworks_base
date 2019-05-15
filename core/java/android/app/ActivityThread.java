e706b9307a62c80b3fbdc2dc711eef8d04bce887
mydongistiny <jaysonedson@gmail.com>
2019-05-15 06:20:53 -0700

    ActivityThread: Remove Failed to find provider info logspam
    Example:
    E ActivityThread: Failed to find provider info for com.google.android.gsf.gservices
    
    Change-Id: I25448aae1b62d6ab4d86f3ef78a36fefb4b94267
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- core/java/android/app/ActivityThread.java
+++ core/java/android/app/ActivityThread.java
@@ -6565,7 +6565,6 @@ public final class ActivityThread extends ClientTransactionHandler {
             throw ex.rethrowFromSystemServer();
         }
         if (holder == null) {
-            Slog.e(TAG, "Failed to find provider info for " + auth);
             return null;
         }
 
