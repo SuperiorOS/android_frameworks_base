16e094d745b42e16fba07accfa692504a7330556
Danny Baumann <dannybaumann@web.de>
2019-07-03 15:12:22 +0200

    SystemUI: Add VPN tile
    Allows quickly connecting/disconnecting to/from VPNs.
    
    Change-Id: Iaa9413bf1071805be8764d92bc5e2dd3131fdaed
    Signed-off-by: S I P U N <sipunkumar85@gmail.com>

--- packages/SystemUI/src/com/android/systemui/superior/SuperiorOSModule.kt
+++ packages/SystemUI/src/com/android/systemui/superior/SuperiorOSModule.kt
@@ -23,6 +23,7 @@ import com.android.systemui.qs.tiles.CellularTile
 import com.android.systemui.qs.tiles.HeadsUpTile
 import com.android.systemui.qs.tiles.SyncTile
 import com.android.systemui.qs.tiles.UsbTetherTile
+import com.android.systemui.qs.tiles.VpnTile
 import com.android.systemui.qs.tiles.WifiTile
 
 import dagger.Binds
@@ -62,6 +63,12 @@ interface SuperiorOSModule {
     @IntoMap
     @StringKey(UsbTetherTile.TILE_SPEC)
     fun bindUsbTetherTile(usbTetherTile: UsbTetherTile): QSTileImpl<*>
+
+    /** Inject VpnTile into tileMap in QSModule */
+    @Binds
+    @IntoMap
+    @StringKey(VpnTile.TILE_SPEC)
+    fun bindVpnTile(vpnTile: VpnTile): QSTileImpl<*>
 
     /** Inject WifiTile into tileMap in QSModule */
     @Binds
