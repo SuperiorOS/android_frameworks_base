4921787965cf20dc213efd268e1df3544bef4b9f
maxwen <max.weninger@gmail.com>
2019-09-25 16:19:49 +0200

    [1/2] base: navigation mode settings
    to configure the height of the back gesture
    
    Change-Id: I6ebe0039e250f3b8f0b91b232df5941b58026499
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5370,6 +5370,9 @@ public final class Settings {
         /** @hide */
         public static final String BACK_GESTURE_HAPTIC = "back_gesture_haptic";
 
+        /** @hide */
+        public static final String BACK_GESTURE_HEIGHT = "back_gesture_height";
+
         /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
