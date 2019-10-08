05fb847a04760af38e8f58b2d34d9bdd25292ded
Marko Man <darkobas@gmail.com>
2019-10-08 11:03:31 +0200

    base: Add qs status bar header image [1/2]
    This is a squashed commit with fixes and improvements from
    maxwen, SKULSHADY, eldainosor
    
    neobuddy89: Clean up unnecessary bits and adapted for Android 11.
    Stallix: Remove unnecessary gradient background
    Sweeto143: Adpat for Superior OS
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Joey Huab <joey@evolution-x.org>
    Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>
    Change-Id: I318b03bd493ecddef0bb3aeae69960979a0b3271

--- packages/SystemUI/src/com/android/systemui/qs/customize/QSCustomizer.java
+++ packages/SystemUI/src/com/android/systemui/qs/customize/QSCustomizer.java
@@ -96,6 +96,8 @@ public class QSCustomizer extends LinearLayout implements OnMenuItemClickListene
     private int mY;
     private boolean mOpening;
     private boolean mIsShowingNavBackdrop;
+    private boolean mHeaderImageEnabled;
+
     private UiEventLogger mUiEventLogger = new UiEventLoggerImpl();
 
     private GridLayoutManager mLayout;
@@ -193,6 +195,10 @@ public class QSCustomizer extends LinearLayout implements OnMenuItemClickListene
         LayoutParams lp = (LayoutParams) mTransparentView.getLayoutParams();
         lp.height = mContext.getResources().getDimensionPixelSize(
                 com.android.internal.R.dimen.quick_qs_offset_height);
+        if (mHeaderImageEnabled) {
+            lp.height += mContext.getResources().getDimensionPixelSize(
+                    R.dimen.qs_header_image_offset);
+        }
         mTransparentView.setLayoutParams(lp);
     }
 
@@ -478,6 +484,9 @@ public class QSCustomizer extends LinearLayout implements OnMenuItemClickListene
 
     public void updateSettings() {
         final Resources res = mContext.getResources();
+        mHeaderImageEnabled = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.STATUS_BAR_CUSTOM_HEADER, 0,
+                UserHandle.USER_CURRENT) == 1;
         boolean isPortrait = res.getConfiguration().orientation
                 == Configuration.ORIENTATION_PORTRAIT;
         int defaultColumns = Math.max(1, mContext.getResources().getInteger(R.integer.quick_settings_num_columns));
