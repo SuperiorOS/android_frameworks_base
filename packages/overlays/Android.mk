85821fcf669a71eaf7ecb1d97d32f30bb465eeae
SuperDroidBond <superdroidbond@yahoo.com>
2019-10-13 17:38:04 +0530

    Add our favourite fonts in AOSP FontEngine [2/2]
    Signed-off-by: Jayant-Deshmukh <jayantdeshmuk008@gmail.com>
    Change-Id: Ib6e678ef1697d25933b9bda72d64bd54228730b5

--- packages/overlays/Android.mk
+++ packages/overlays/Android.mk
@@ -23,6 +23,16 @@ LOCAL_REQUIRED_MODULES := \
 	DisplayCutoutEmulationTallOverlay \
 	DisplayCutoutEmulationWaterfallOverlay \
 	FontNotoSerifSourceOverlay \
+        FontAclonicaSourceOverlay \
+        FontAmaranteSourceOverlay \
+        FontBariolSourceOverlay \
+        FontCagliostroSourceOverlay \
+        FontCoolstorySourceOverlay \
+        FontLGSmartGothicSourceOverlay \
+        FontNotoSerifSourceOverlay \
+        FontRosemarySourceOverlay \
+        FontSonySketchSourceOverlay \
+        FontSurferSourceOverlay \
 	IconPackCircularAndroidOverlay \
 	IconPackCircularLauncherOverlay \
 	IconPackCircularSettingsOverlay \
