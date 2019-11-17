790ea86ad905ebf14104e3cbc3872699ec3d5290
ezio84 <brabus84@gmail.com>
2019-11-17 09:24:15 +0000

    d2w: fix double tap to wake on ambient

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
@@ -1208,7 +1208,7 @@ public class NotificationPanelView extends PanelView implements
                 return false;
             }
         }
-        if (mIsLockscreenDoubleTapEnabled
+        if (mIsLockscreenDoubleTapEnabled && !mPulsing && !mDozing
                 && mBarState == StatusBarState.KEYGUARD) {
             mLockscreenDoubleTapToSleep.onTouchEvent(event);
         }
