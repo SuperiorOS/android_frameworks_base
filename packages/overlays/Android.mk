8f99ddebd51e222a31c99c1b20c2df67aa8bad62
Marko Man <darkobas@gmail.com>
2019-11-27 23:19:08 +0100

    overlay: Introduce Primary color overlay
    Signed-off-by: Jayant-Deshmukh <jayantdeshmuk008@gmail.com>
    Change-Id: Ibd932113df64ee88dbe1ac9444e33603a294b64e

--- packages/overlays/Android.mk
+++ packages/overlays/Android.mk
@@ -119,7 +119,8 @@ LOCAL_REQUIRED_MODULES := \
 	NavigationBarModeGesturalOverlayNarrowBack \
 	NavigationBarModeGesturalOverlayWideBack \
 	NavigationBarModeGesturalOverlayExtraWideBack \
-	preinstalled-packages-platform-overlays.xml
+	preinstalled-packages-platform-overlays.xml \
+	PrimaryColorBlackOverlay
 
 include $(BUILD_PHONY_PACKAGE)
 include $(CLEAR_VARS)
