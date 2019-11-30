404b8987bff768781a08d397dabc659518040cd6
Luca Stefani <luca.stefani.ge1@gmail.com>
2019-11-30 13:41:05 +0100

    Battery Styles: Bring back good ol' circle battery style
    * This code is based on the old Java implementation and converted to Kotlin
    
    * Instead of creating different style within the same drawable
      create one on its own, this makes the code more readable and future proof
    
    Anushek Prasal <anushekprasal@gmail.com>
    - Convert to SettingsObserver
    - Cleanup tuner code
    
    CarbonROM:
    - Remove broken battery entry from tuner list
    - Add nullcheck for percentage
    
    Dirty Unicorns:
    - Adapt 'updateShowPercent' method to existing modifications
    - Clean up code
    
    Change-Id: I23798426e603b277408fb63f4898b385027e7eeb
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Reviewed-on: https://review.statixos.com/c/android_frameworks_base/+/6741
    Reviewed-by: Sourajit Karmakar <sourajit@live.com>
    Tested-by: Sourajit Karmakar <sourajit@live.com>
    Reviewed-on: https://review.statixos.com/c/android_frameworks_base/+/7373
    Reviewed-by: Anay Wadhera <anay1018@gmail.com>
    Tested-by: Anay Wadhera <anay1018@gmail.com>
    Signed-off-by: Jayant-Deshmukh <jayantdeshmuk008@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5196,7 +5196,7 @@ public final class Settings {
          * @hide
          */
         @Readable
-        public static final String SHOW_BATTERY_PERCENT = "status_bar_show_battery_percent";
+        public static final String SHOW_BATTERY_PERCENT = "dummy_show_battery_percent";
 
         /**
          * Whether or not to enable multiple audio focus.
@@ -5237,6 +5237,21 @@ public final class Settings {
          * @hide
          */
         public static final String AMBIENT_WAKE_GESTURES = "ambient_wake_gestures";
+
+        /**
+         * Battery style
+         * @hide
+         */
+        public static final String STATUS_BAR_BATTERY_STYLE = "status_bar_battery_style";
+
+        /**
+          * Statusbar Battery %
+          * 0: Hide the battery percentage
+          * 1: Display the battery percentage inside the icon
+          * 2: Display the battery percentage next to Icon
+          * @hide
+          */
+        public static final String STATUS_BAR_SHOW_BATTERY_PERCENT = "status_bar_show_battery_percent";
 
         /**
          * Double tap on lockscreen to sleep
@@ -5410,7 +5425,6 @@ public final class Settings {
             PRIVATE_SETTINGS.add(POINTER_SPEED);
             PRIVATE_SETTINGS.add(LOCK_TO_APP_ENABLED);
             PRIVATE_SETTINGS.add(EGG_MODE);
-            PRIVATE_SETTINGS.add(SHOW_BATTERY_PERCENT);
             PRIVATE_SETTINGS.add(DISPLAY_COLOR_MODE);
             PRIVATE_SETTINGS.add(DISPLAY_COLOR_MODE_VENDOR_HINT);
             PRIVATE_SETTINGS.add(DOUBLE_TAP_SLEEP_LOCKSCREEN);
