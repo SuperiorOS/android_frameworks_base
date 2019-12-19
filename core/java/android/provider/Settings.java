6e32631c82cdc14309feb2dc2a3930cc6ee9ae46
00day0 <therandomuser11@gmail.com>
2019-12-19 23:29:50 +0100

    [SQUASH] base: Tint QS Tile with accent color [1/2]
    * Tinted QS Tile stuffs (icon, background, label)
    * Using bolder font
    
    Adjust QS tile background and icon color
    
    + make qs tile icon smaller by 1dp
    + reduce qs tile vertical margin
    
    SystemUI: Add a toggle for the new QS tint [1/2]
    
    Co-authored-by: Stallix <joey@evolution-x.org>
    Signed-off-by: Joey Huab <joey@evolution-x.org>
    Signed-off-by: Jayant-Deshmukh <jayantdeshmuk008@gmail.com>
    Change-Id: I46cb59adfacce35e7942da85af58d91e93afb305

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5621,6 +5621,12 @@ public final class Settings {
         public static final String COLOR_BUCKET_OVERLAY = "color_bucket_overlay";
 
         /**
+         * Whether to use new QS panel bg tint or not
+         * @hide
+         */
+        public static final String QS_PANEL_BG_USE_NEW_TINT = "qs_panel_bg_use_new_tint";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
@@ -5791,6 +5797,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(VOLUME_BUTTON_MUSIC_CONTROL);
             PRIVATE_SETTINGS.add(FOD_GESTURE);
             PRIVATE_SETTINGS.add(COLOR_BUCKET_OVERLAY);
+            PRIVATE_SETTINGS.add(QS_PANEL_BG_USE_NEW_TINT);
         }
 
         /**
--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -708,6 +708,9 @@ public class StatusBar extends SystemUI implements DemoMode,
             mContext.getContentResolver().registerContentObserver(Settings.System.getUriFor(
                     Settings.System.LOCKSCREEN_CHARGING_ANIMATION_STYLE),
                     false, this, UserHandle.USER_ALL);
+            mContext.getContentResolver().registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.QS_PANEL_BG_USE_NEW_TINT),
+                    false, this, UserHandle.USER_ALL);
         }
 
         @Override
@@ -730,6 +733,9 @@ public class StatusBar extends SystemUI implements DemoMode,
             } else if (uri.equals(Settings.System.getUriFor(
                     Settings.System.LOCKSCREEN_CHARGING_ANIMATION_STYLE))) {
                 updateChargingAnimation();
+            } else if (uri.equals(Settings.System.getUriFor(
+                    Settings.System.QS_PANEL_BG_USE_NEW_TINT))) {
+                mQSPanel.getHost().reloadAllTiles();
             }
         }
 
