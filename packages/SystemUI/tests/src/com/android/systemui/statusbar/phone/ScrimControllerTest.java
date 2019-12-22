079b18bc7fda1a86e53d9b37d10da970fdd42d83
Dyneteve <dyneteve@pixelexperience.org>
2019-12-22 19:37:14 +0200

    Revert all the scrim commits
    Change-Id: I41a90ffafb94f546ff0b101b622a660781a16867
    Signed-off-by: LordShenron <shen.priyanshu@gmail.com>

--- packages/SystemUI/tests/src/com/android/systemui/statusbar/phone/ScrimControllerTest.java
+++ packages/SystemUI/tests/src/com/android/systemui/statusbar/phone/ScrimControllerTest.java
@@ -232,21 +232,10 @@ public class ScrimControllerTest extends SysuiTestCase {
         // Pulse callback should have been invoked
         assertScrimVisibility(VISIBILITY_FULLY_TRANSPARENT, VISIBILITY_FULLY_OPAQUE);
         assertScrimTint(mScrimBehind, true /* tinted */);
-
-        // ... and when ambient goes dark, front scrim should be semi-transparent
-        mScrimController.setAodFrontScrimAlpha(0.5f);
-        mScrimController.finishAnimationsImmediately();
-        // Front scrim should be semi-transparent
-        assertScrimVisibility(VISIBILITY_SEMI_TRANSPARENT /* front */,
-                VISIBILITY_FULLY_OPAQUE /* back */);
-
         mScrimController.setWakeLockScreenSensorActive(true);
         mScrimController.finishAnimationsImmediately();
         assertScrimVisibility(VISIBILITY_SEMI_TRANSPARENT /* front */,
                 VISIBILITY_SEMI_TRANSPARENT /* back */);
-
-        // Reset value since enums are static.
-        mScrimController.setAodFrontScrimAlpha(0f);
     }
 
     @Test
