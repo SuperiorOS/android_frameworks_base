55dadd193f681a8277565885a36d21ae4089c990
Pranav Vashi <neobuddy89@gmail.com>
2019-12-29 22:39:19 +0530

    Add Google Sans and Comic Sans font styles [1/2]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Jayant-Deshmukh <jayantdeshmuk008@gmail.com>
    Change-Id: I2be9a5702e8b9acd771e01f628ec22bef6154554

--- packages/overlays/Android.mk
+++ packages/overlays/Android.mk
@@ -52,8 +52,9 @@ LOCAL_REQUIRED_MODULES := \
 	FontEvolveSansSourceOverlay \
 	FontFucekSourceOverlay \
 	FontGoogleSansMediumSourceOverlay \
-	FontGoogleSansOverlay \
 	FontLemonMilkSourceOverlay \
+	FontComicSansSourceOverlay \
+	FontGoogleSansSourceOverlay \
 	FontNotoSerifSourceOverlay \
 	FontRubikRubikOverlay \
 	FontOdudaSourceOverlay \
