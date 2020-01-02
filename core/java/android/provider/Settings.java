9fb1cf9be770c99cfaec486d21c3814d61434363
Anushek Prasal <anushekprasal@gmail.com>
2020-01-03 00:18:59 +0530

    base: Add Touch HIDL support
    All credits to LineageOS and related authors on:
    https://github.com/LineageOS/android_lineage-sdk
    
    Ported to AOSP from lineage-sdk by @SKULSHADY
    
    Signed-off-by: Pranav Temkar <pranavtemkar@gmail.com>
    Change-Id: Iac8dba8b2d509fe1fa707efc4da9c661f1321d9d

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6232,6 +6232,12 @@ public final class Settings {
         public static final String LOCKSCREEN_WEATHER_TEXT = "lockscreen_weather_text";
 
         /**
+         * Whether or not to vibrate when a touchscreen gesture is detected
+         * @hide
+         */
+        public static final String TOUCHSCREEN_GESTURE_HAPTIC_FEEDBACK = "touchscreen_gesture_haptic_feedback";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
@@ -6364,6 +6370,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(SCREEN_FLASH_NOTIFICATION);
             PRIVATE_SETTINGS.add(SCREEN_FLASH_NOTIFICATION_COLOR);
             PRIVATE_SETTINGS.add(DEFAULT_DEVICE_FONT_SCALE);
+            PRIVATE_SETTINGS.add(TOUCHSCREEN_GESTURE_HAPTIC_FEEDBACK);
         }
 
         /**
