5b836886419a6542be74fd4cc4c6c7b73e187d11
Lup Gabriel <gwolf2u.website@gmail.com>
2020-01-17 19:11:13 +0200

    Add Exotwo, Storopia and Ubuntu fonts [1/2]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Jayant-Deshmukh <jayantdeshmuk008@gmail.com>
    Change-Id: I108ceba627944520ad400df45f86af2f9acc6088

--- packages/overlays/Android.mk
+++ packages/overlays/Android.mk
@@ -48,6 +48,9 @@ LOCAL_REQUIRED_MODULES := \
 	FontGoogleSansMediumSourceOverlay \
 	FontLemonMilkSourceOverlay \
 	FontComfortaaSourceOverlay \
+	FontExotwoSourceOverlay \
+	FontStoropiaSourceOverlay \
+	FontUbuntuSourceOverlay \
 	FontComicSansSourceOverlay \
 	FontGoogleSansSourceOverlay \
 	FontNotoSerifSourceOverlay \
