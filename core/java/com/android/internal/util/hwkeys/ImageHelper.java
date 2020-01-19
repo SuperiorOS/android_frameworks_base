d50b4ffd3521f8d2af08bae779562c9fa8a197fa
El Dainosor <eldainosor@gmail.com>
2020-01-19 07:44:18 +0100

    base: Let's shrink file headers bigger than the device size
    Custom headers and new cameras with bigger resolution sizes aren't a good match.
    This might put a stop to those who put a 4K image as their header and complains about SysUI struggling :)
    
    Change-Id: I4a9ade0008f532d84160f44c57efdaa3e1fdeb30
    Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>
    Signed-off-by: Jayant-Deshmukh <jayantdeshmuk008@gmail.com>

--- core/java/com/android/internal/util/hwkeys/ImageHelper.java
+++ core/java/com/android/internal/util/hwkeys/ImageHelper.java
@@ -51,9 +51,11 @@ import android.graphics.drawable.VectorDrawable;
 import android.net.Uri;
 import android.os.ParcelFileDescriptor;
 import android.util.AttributeSet;
+import android.util.DisplayMetrics;
 import android.util.Log;
 import android.util.TypedValue;
 import android.util.Xml;
+import android.view.WindowManager;
 
 public class ImageHelper {
     private static final int VECTOR_WIDTH = 512;
@@ -158,6 +160,42 @@ public class ImageHelper {
                     middleY - bitmap.getHeight() / 2, paint);
             return new BitmapDrawable(context.getResources(), scaledBitmap);
         }
+    }
+
+    public static Bitmap resizeMaxDeviceSize(Context context, Drawable image) {
+        Bitmap i2b = ((BitmapDrawable) image).getBitmap();
+        return resizeMaxDeviceSize(context, i2b);
+    }
+
+    public static Bitmap resizeMaxDeviceSize(Context context, Bitmap image) {
+        Bitmap imageToBitmap;
+        DisplayMetrics metrics = new DisplayMetrics();
+        WindowManager wm = context.getSystemService(WindowManager.class);
+        wm.getDefaultDisplay().getRealMetrics(metrics);
+        int maxHeight = metrics.heightPixels;
+        int maxWidth = metrics.widthPixels;
+        try {
+            imageToBitmap = RGB565toARGB888(image);
+            if (maxHeight > 0 && maxWidth > 0) {
+                int width = imageToBitmap.getWidth();
+                int height = imageToBitmap.getHeight();
+                float ratioBitmap = (float) width / (float) height;
+                float ratioMax = (float) maxWidth / (float) maxHeight;
+
+                int finalWidth = maxWidth;
+                int finalHeight = maxHeight;
+                if (ratioMax > ratioBitmap) {
+                    finalWidth = (int) ((float)maxHeight * ratioBitmap);
+                } else {
+                    finalHeight = (int) ((float)maxWidth / ratioBitmap);
+                }
+                imageToBitmap = Bitmap.createScaledBitmap(imageToBitmap, finalWidth, finalHeight, true);
+                return imageToBitmap;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return image;
     }
 
     public static Bitmap getRoundedCornerBitmap(Bitmap bitmap) {
@@ -273,6 +311,20 @@ public class ImageHelper {
         Bitmap image = BitmapFactory.decodeFileDescriptor(fileDescriptor);
         parcelFileDescriptor.close();
         return image;
+    }
+    private static Bitmap RGB565toARGB888(Bitmap img) throws Exception {
+        int numPixels = img.getWidth() * img.getHeight();
+        int[] pixels = new int[numPixels];
+
+        //Get JPEG pixels.  Each int is the color values for one pixel.
+        img.getPixels(pixels, 0, img.getWidth(), 0, 0, img.getWidth(), img.getHeight());
+
+        //Create a Bitmap of the appropriate format.
+        Bitmap result = Bitmap.createBitmap(img.getWidth(), img.getHeight(), Bitmap.Config.ARGB_8888);
+
+        //Set RGB pixels.
+        result.setPixels(pixels, 0, result.getWidth(), 0, 0, result.getWidth(), result.getHeight());
+        return result;
     }
 
     /**
