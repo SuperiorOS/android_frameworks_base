1d1a77a4080d1d950f86d352e495f6068a4da4e5
Demon000 <demonsingur@gmail.com>
2020-02-01 18:24:34 +0000

    GlobalScreenshot: do not capture multiple screenshots simultaneously
    Change-Id: I4c0c73759b8b0483f0d28b4c6c39e9a616c26b57
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- packages/SystemUI/src/com/android/systemui/screenshot/GlobalScreenshot.java
+++ packages/SystemUI/src/com/android/systemui/screenshot/GlobalScreenshot.java
@@ -674,7 +674,11 @@ class GlobalScreenshot {
     }
 
     void takeScreenshot(Runnable finisher, boolean statusBarVisible, boolean navBarVisible) {
-        mPartialShot = false;
+        if (mScreenshotLayout.getParent() != null) {
+            finisher.run();
+            return;
+        }
+
         mDisplay.getRealMetrics(mDisplayMetrics);
         takeScreenshot(finisher, statusBarVisible, navBarVisible,
                 new Rect(0, 0, mDisplayMetrics.widthPixels, mDisplayMetrics.heightPixels));
@@ -697,6 +701,11 @@ class GlobalScreenshot {
      */
     void takeScreenshotPartial(final Runnable finisher, final boolean statusBarVisible,
             final boolean navBarVisible) {
+        if (mScreenshotLayout.getParent() != null) {
+            finisher.run();
+            return;
+        }
+
         setBlockedGesturalNavigation(true);
         mWindowManager.addView(mScreenshotLayout, mWindowLayoutParams);
         mScreenshotSelectorView.setSelectionListener(
