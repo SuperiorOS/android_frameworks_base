4d99d74202ee5428857be1333b6f063633f6f5a5
Alessandro Astone <ales.astone@gmail.com>
2020-02-24 21:32:02 +0100

    Start BiometricService even if no hardware is present
    * BiometricService already has support for authenticate() to fallback to CDC.
      This allows BiometricPrompt to ask for credentials when no biometric
      hardware is present, rather then doing nothing.
    
    Change-Id: Ib69b913a9e9a6d4d76de5d2ee9e618a3dfd6e5bd

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -1790,12 +1790,10 @@ public final class SystemServer {
                 traceEnd();
             }
 
-            if (hasFeatureFace || hasFeatureIris || hasFeatureFingerprint) {
-                // Start this service after all biometric services.
-                traceBeginAndSlog("StartBiometricService");
-                mSystemServiceManager.startService(BiometricService.class);
-                traceEnd();
-            }
+            // Start this service after all biometric services.
+            traceBeginAndSlog("StartBiometricService");
+            mSystemServiceManager.startService(BiometricService.class);
+            traceEnd();
 
             traceBeginAndSlog("StartBackgroundDexOptService");
             try {
