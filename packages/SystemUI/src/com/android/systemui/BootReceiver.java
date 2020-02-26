31cc3383936db2e975ff07e52b950a1efe9959d5
Marko Man <darkobas@gmail.com>
2020-02-26 22:00:02 +0530

    fwb: FPS Info Overlay & Tile
    ** SDB - This commit is amalgam of ~
    1) CPU Info - c93c75f
    2) FPS Device Info - DerpFest-Devices/device_oneplus_msm8998-common@e6ec51c#diff-195d2c91fafb0c372a1d6dc13d2bba4e
    
    Co-authored-by: SuperDroidBond <superdroidbond@yahoo.com>
    Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>
    Change-Id: Icdd931394525d1e999a2d18a74e7bf7f7e58070d

--- packages/SystemUI/src/com/android/systemui/BootReceiver.java
+++ packages/SystemUI/src/com/android/systemui/BootReceiver.java
@@ -44,6 +44,9 @@ public class BootReceiver extends BroadcastReceiver {
             mContext.getContentResolver().registerContentObserver(Settings.Global.getUriFor(
                     Settings.Global.SHOW_CPU_OVERLAY),
                     false, this);
+            mContext.getContentResolver().registerContentObserver(Settings.Global.getUriFor(
+                    Settings.Global.SHOW_FPS_OVERLAY),
+                    false, this);
             update();
         }
 
@@ -54,10 +57,16 @@ public class BootReceiver extends BroadcastReceiver {
 
         public void update() {
             Intent cpuinfo = new Intent(mContext, com.android.systemui.CPUInfoService.class);
+            Intent fpsinfo = new Intent(mContext, com.android.systemui.FPSInfoService.class);
             if (Settings.Global.getInt(mContext.getContentResolver(), Settings.Global.SHOW_CPU_OVERLAY, 0) != 0) {
                 mContext.startService(cpuinfo);
             } else {
                 mContext.stopService(cpuinfo);
+            }
+            if (Settings.Global.getInt(mContext.getContentResolver(), Settings.Global.SHOW_FPS_OVERLAY, 0) != 0) {
+                mContext.startService(fpsinfo);
+            } else {
+                mContext.stopService(fpsinfo);
             }
         }
     }
@@ -75,6 +84,11 @@ public class BootReceiver extends BroadcastReceiver {
             if (Settings.Global.getInt(mContext.getContentResolver(), Settings.Global.SHOW_CPU_OVERLAY, 0) != 0) {
                 Intent cpuinfo = new Intent(mContext, com.android.systemui.CPUInfoService.class);
                 mContext.startService(cpuinfo);
+            }
+            // Start the fps info overlay, if activated
+            if (Settings.Global.getInt(mContext.getContentResolver(), Settings.Global.SHOW_FPS_OVERLAY, 0) != 0) {
+                Intent fpsinfo = new Intent(mContext, com.android.systemui.FPSInfoService.class);
+                mContext.startService(fpsinfo);
             }
 
         } catch (Exception e) {
--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -39,6 +39,7 @@ import com.android.systemui.qs.tiles.DataSwitchTile;
 import com.android.systemui.qs.tiles.DndTile;
 import com.android.systemui.qs.tiles.FlashlightTile;
 import com.android.systemui.qs.tiles.GamingModeTile;
+import com.android.systemui.qs.tiles.FPSInfoTile;
 import com.android.systemui.qs.tiles.HeadsUpTile;
 import com.android.systemui.qs.tiles.HotspotTile;
 import com.android.systemui.qs.tiles.HWKeysTile;
@@ -101,6 +102,7 @@ public class QSFactoryImpl implements QSFactory {
     private final Provider<DataSwitchTile> mDataSwitchTileProvider;
     private final Provider<GamingModeTile> mGamingModeTileProvider;
     private final Provider<ImmersiveTile> mImmersiveTileProvider;
+    private final Provider<FPSInfoTile> mFPSInfoTileProvider;
 
     private QSTileHost mHost;
 
@@ -136,7 +138,8 @@ public class QSFactoryImpl implements QSFactory {
             Provider<SleepScreenTile> sleepScreenTileProvider,
             Provider<DataSwitchTile> dataSwitchTileProvider,
             Provider<GamingModeTile> gamingModeTileProvider,
-            Provider<ImmersiveTile> immersiveTileProvider) {
+            Provider<ImmersiveTile> immersiveTileProvider,
+            Provider<FPSInfoTile> fpsInfoTileProvider) {
         mWifiTileProvider = wifiTileProvider;
         mBluetoothTileProvider = bluetoothTileProvider;
         mCellularTileProvider = cellularTileProvider;
@@ -169,6 +172,7 @@ public class QSFactoryImpl implements QSFactory {
         mDataSwitchTileProvider = dataSwitchTileProvider;
         mGamingModeTileProvider = gamingModeTileProvider;
         mImmersiveTileProvider = immersiveTileProvider;
+        mFPSInfoTileProvider = fpsInfoTileProvider;
     }
 
     public void setHost(QSTileHost host) {
@@ -248,6 +252,8 @@ public class QSFactoryImpl implements QSFactory {
                 return mGamingModeTileProvider.get();
             case "immersive":
                 return mImmersiveTileProvider.get();
+            case "fpsinfo":
+                return mFPSInfoTileProvider.get();
         }
 
         // Intent tiles.
