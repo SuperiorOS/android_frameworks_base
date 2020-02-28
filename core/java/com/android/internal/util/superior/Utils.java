6a60b8f10ba1c5e15e78bca3d000700e013f9a72
Alex Cruz <du.alexcruz@gmail.com>
2020-02-29 00:06:17 +0100

    Custom actions Utils: add clear all notifications action
    Change-Id: I0ccb7d8adfd62ca83934f87cdd04f4a74ffcbb94
    Signed-off-by: Jayant-Deshmukh <jayantdeshmuk008@gmail.com>

--- core/java/com/android/internal/util/superior/Utils.java
+++ core/java/com/android/internal/util/superior/Utils.java
@@ -17,6 +17,7 @@
 package com.android.internal.util.superior;
 
 import android.app.NotificationManager;
+import android.app.ActivityManager;
 import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
@@ -266,6 +267,20 @@ public class Utils {
                 } catch (RemoteException e) {}
             }
         }
+
+        // Clear notifications
+        public static void clearAllNotifications() {
+            IStatusBarService service = getStatusBarService();
+            if (service != null) {
+                try {
+                    service.onClearAllNotifications(ActivityManager.getCurrentUser());
+                } catch (RemoteException e) {}
+            }
+        }
+    }
+
+    public static void clearAllNotifications() {
+        FireActions.clearAllNotifications();
     }
 
     public static void toggleQsPanel() {
