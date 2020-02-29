bacd211cfecc009fbf2261ea6b55be15bc3a5022
ezio84 <brabus84@gmail.com>
2020-02-29 18:11:32 +0100

    Move launchCamera and launchVoiceSearch actions to custom Utils [1/2]
    Change-Id: I1ee720ece187509b77db1de9f671d75f95bec17a
    Signed-off-by: Jayant-Deshmukh <jayantdeshmuk008@gmail.com>

--- core/java/com/android/internal/util/superior/Utils.java
+++ core/java/com/android/internal/util/superior/Utils.java
@@ -47,6 +47,7 @@ import android.os.SystemProperties;
 import android.text.format.Time;
 import android.os.UserHandle;
 import android.provider.Settings;
+import android.provider.MediaStore;
 import android.view.IWindowManager;
 import android.view.WindowManagerGlobal;
 import android.telephony.TelephonyManager;
@@ -312,6 +313,18 @@ public class Utils {
     public static void toggleVolumePanel(Context context) {
         AudioManager am = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
         am.adjustVolume(AudioManager.ADJUST_SAME, AudioManager.FLAG_SHOW_UI);
+    }
+
+    public static void launchCamera(Context context) {
+        Intent intent = new Intent(MediaStore.INTENT_ACTION_STILL_IMAGE_CAMERA_SECURE);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        context.startActivity(intent);
+    }
+
+    public static void launchVoiceSearch(Context context) {
+        Intent intent = new Intent(Intent.ACTION_SEARCH_LONG_PRESS);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK).addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        context.startActivity(intent);
     }
 
     /**
