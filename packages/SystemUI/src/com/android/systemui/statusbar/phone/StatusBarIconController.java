d70550f015ea5f8e2ebd1d985aea9a2b6224efc3
rajadeja <rajsjadeja4@gmail.com>
2020-03-21 14:18:40 +0000

    StatusBarIconController: Don't glitch traffic indicator on QS panel
    Change-Id: I43d751284b48989010bb6b89851e223754a2b8da
    Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarIconController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarIconController.java
@@ -317,7 +317,7 @@ public interface StatusBarIconController {
 
         private StatusBarNetworkTraffic onCreateNetworkTraffic(String slot) {
             StatusBarNetworkTraffic view = new StatusBarNetworkTraffic(mContext);
-            view.setPadding(4, 0, 4, 0);
+            view.setPadding(0,0,0,0);
             return view;
         }
 
