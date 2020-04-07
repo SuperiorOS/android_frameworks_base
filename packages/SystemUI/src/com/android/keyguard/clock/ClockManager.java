6c722ad6c928c4fd6dc24d61d2a7f5f0a5065c2c
El Dainosor <eldainosor@gmail.com>
2020-04-07 22:59:36 +0000

    clock: Add Oroño clock
    Change-Id: I8eeec442bf54a806fd71f2fdd517106eca6f6c1d

--- packages/SystemUI/src/com/android/keyguard/clock/ClockManager.java
+++ packages/SystemUI/src/com/android/keyguard/clock/ClockManager.java
@@ -164,6 +164,7 @@ public final class ClockManager {
         addBuiltinClock(() -> new FluidClockController(res, layoutInflater, colorExtractor));
         addBuiltinClock(() -> new ShapeShiftClockController(res, layoutInflater, colorExtractor));
         addBuiltinClock(() -> new IDEClockController(res, layoutInflater, colorExtractor));
+        addBuiltinClock(() -> new OronosClockController(res, layoutInflater, colorExtractor));
 
         // Store the size of the display for generation of clock preview.
         DisplayMetrics dm = res.getDisplayMetrics();
