f9651b769af8b5f7f380abf9b6d74091c6513c96
Jayant-Deshmukh <jayantdeshmuk008@gmail.com>
2020-04-16 21:04:20 +0530

    Traffic indicator: Add some padding to the right
    Signed-off-by: Jayant-Deshmukh <jayantdeshmuk008@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarIconController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarIconController.java
@@ -317,7 +317,7 @@ public interface StatusBarIconController {
 
         private StatusBarNetworkTraffic onCreateNetworkTraffic(String slot) {
             StatusBarNetworkTraffic view = new StatusBarNetworkTraffic(mContext);
-            view.setPadding(0,0,0,0);
+            view.setPadding(2,0,2,0);
             return view;
         }
 
