f868f982eee29aa6b3bf5c155cec5e9bccb48fc1
Linux4 <tim@linux4.de>
2020-04-20 17:57:43 +0200

    SystemUI: Add powershare QS tile
    Author: Linux4 <tim@linux4.de>
    Date:   Mon Apr 20 17:57:43 2020 +0200
    
        base: SystemUI: add qs powershare tile
    
        Change-Id: Ibcf5cc3e766d971d9dca9242bf2770093d4d3099
    
    Author: Linux4 <tim@linux4.de>
    Date:   Sat May 2 12:54:49 2020 +0200
    
        base: SystemUI: fix NPE in PowerShareTile
    
        Change-Id: If69846f3e3d4f3a12c0f78f47b5adc1008888de1
    
    Author: LuK1337 <priv.luk@gmail.com>
    Date:   Tue Jul 7 13:27:12 2020 +0200
    
        SystemUI: Call setOnlyAlertOnce(true) for PowerShare notification
    
        * Otherwise we end up getting notification sound
          every 20 seconds when using it...
    
        Change-Id: Ia56b01f332860b64f0d2c532884e6bcaa2acfb17
    
    Change-Id: Ia09a674745adb433f3aaeebfeb79899906350793
    
    [someone5678] Adapt to QPR3
    
    Change-Id: I6597cbdf0c3c09fe74583c8d8e78aa17fcae178e
    Signed-off-by: someone5678 <nemui3353@gmail.com>
    Signed-off-by: Jayant-Deshmukh <jayantdeshmuk008@gmail.com>

--- packages/SystemUI/src/com/android/systemui/superior/SuperiorOSModule.kt
+++ packages/SystemUI/src/com/android/systemui/superior/SuperiorOSModule.kt
@@ -23,6 +23,7 @@ import com.android.systemui.qs.tiles.CellularTile
 import com.android.systemui.qs.tiles.CompassTile
 import com.android.systemui.qs.tiles.DataSwitchTile
 import com.android.systemui.qs.tiles.HeadsUpTile
+import com.android.systemui.qs.tiles.PowerShareTile
 import com.android.systemui.qs.tiles.SoundTile
 import com.android.systemui.qs.tiles.SyncTile
 import com.android.systemui.qs.tiles.UsbTetherTile
@@ -66,6 +67,12 @@ interface SuperiorOSModule {
     @IntoMap
     @StringKey(HeadsUpTile.TILE_SPEC)
     fun bindHeadsUpTile(headsUpTile: HeadsUpTile): QSTileImpl<*>
+
+    /** Inject PowerShareTile into tileMap in QSModule */
+    @Binds
+    @IntoMap
+    @StringKey(PowerShareTile.TILE_SPEC)
+    fun bindPowerShareTile(powerShareTile: PowerShareTile): QSTileImpl<*>
 
     /** Inject SoundTile into tileMap in QSModule */
     @Binds
