591b1d70391a688d741f7a60e520453fb958afc8
bigrushdog <randall.rushing@gmail.com>
2020-05-17 09:17:32 -0700

    Pulse navbar audio gfx visualizer for Android 10 [1/4]
    Change-Id: I046f37e0a50fd8569d309d5bb48c6c5ebfbafd8f
    Signed-off-by: Jayant-Deshmukh <jayantdeshmuk008@gmail.com>
    Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>

--- services/core/java/com/android/server/pm/permission/DefaultPermissionGrantPolicy.java
+++ services/core/java/com/android/server/pm/permission/DefaultPermissionGrantPolicy.java
@@ -199,6 +199,12 @@ public final class DefaultPermissionGrantPolicy {
         WALLPAPER_PERMISSIONS.add(Manifest.permission.SET_WALLPAPER_COMPONENT);
     }
 
+    private static final Set<String> PULSE_EQ_PERMISSIONS = new ArraySet<>();
+    static {
+        PULSE_EQ_PERMISSIONS.add(Manifest.permission.MODIFY_AUDIO_SETTINGS);
+        PULSE_EQ_PERMISSIONS.add(Manifest.permission.RECORD_AUDIO);
+    }
+
     private static final int MSG_READ_DEFAULT_PERMISSION_EXCEPTIONS = 1;
 
     private static final String ACTION_TRACK = "com.android.fitness.TRACK";
@@ -848,6 +854,9 @@ public final class DefaultPermissionGrantPolicy {
         // Lawnchair
         grantSystemFixedPermissionsToSystemPackage("ch.deletescape.lawnchair.ci", userId, CALENDAR_PERMISSIONS,
                 ALWAYS_LOCATION_PERMISSIONS, PHONE_PERMISSIONS, STORAGE_PERMISSIONS);
+
+        // SystemUI Pulse EQ
+        grantSystemFixedPermissionsToSystemPackage("com.android.systemui", userId, PULSE_EQ_PERMISSIONS);
     }
 
     private String getDefaultSystemHandlerActivityPackageForCategory(String category, int userId) {
