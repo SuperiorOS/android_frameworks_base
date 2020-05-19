25ea879807a168fbac691fb9351c1ee8f4fd5e28
Jesse Chan <jc@lineageos.org>
2020-05-19 16:05:23 +0800

    SystemUI: Implement hide gestural navigation hint bar [1/2]
    Author: Jesse Chan <jc@lineageos.org>
    Date:   Tue Jun 2 13:59:52 2020 +0800
    
        SystemUI: Set no hint overlay to highest priority
    
        As overlays are also used to apply navigation mode, it is needed to set
        our customization overlay to highest priority to ensure it is applied.
    
        Change-Id: I41ca4b7ea99fea9f62d2e8ece5114f682e8e7fe9
        Signed-off-by: Jesse Chan <jc@lineageos.org>
    
    Change-Id: Ic6695dfe929db443f169d59f153e14a6de249590
    Signed-off-by: Jesse Chan <jc@lineageos.org>
    Signed-off-by: Joey Huab <joey@evolution-x.org>
    Signed-off-by: Jayant-Deshmukh <jayantdeshmuk008@gmail.com>

--- packages/overlays/Android.mk
+++ packages/overlays/Android.mk
@@ -83,6 +83,7 @@ LOCAL_REQUIRED_MODULES := \
 	NavigationBarModeGesturalOverlayNarrowBack \
 	NavigationBarModeGesturalOverlayWideBack \
 	NavigationBarModeGesturalOverlayExtraWideBack \
+	NavigationBarNoHintOverlay \
 	preinstalled-packages-platform-overlays.xml
 
 include $(BUILD_PHONY_PACKAGE)
