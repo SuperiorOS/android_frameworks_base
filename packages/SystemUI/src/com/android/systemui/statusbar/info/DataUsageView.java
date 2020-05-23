aaf6de9f369928d71e95c07d95d81ca6b3cb2260
AnkitGourav <Ankitgourav035@gmail.com>
2020-05-23 07:31:36 +0530

    SystemUI: QS Header data usage info: improve layout and view
    Signed-off-by: Jayant-Deshmukh <jayantdeshmuk008@gmail.com>
    Change-Id: Ie71696ca73391e053a9ba129a3cfd30d482f8a18

--- packages/SystemUI/src/com/android/systemui/statusbar/info/DataUsageView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/info/DataUsageView.java
@@ -67,7 +67,7 @@ public class DataUsageView extends TextView {
         shouldUpdateDataTextView = true;
     }
 
-    private boolean isDataUsageEnabled() {
+    public boolean isDataUsageEnabled() {
         return Settings.System.getInt(mContext.getContentResolver(),
                 Settings.System.QS_DATAUSAGE, 0) != 0;
     }
