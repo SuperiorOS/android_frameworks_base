2508fe41be85cbabec02c52ff314043ce19ed416
AnkitGourav <Ankitgourav035@gmail.com>
2020-06-15 06:55:22 +0530

    Partially Revert "QSHeaderDataUsage: hide usage if device is not connected to internet"
     partially revert commit 62081100a2c75f260712689c4a0ed2100241c906
    
    - fix data usage not updating on qs toggle
    
    Signed-off-by: AnkitGourav <Ankitgourav035@gmail.com>
    Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>
    Change-Id: I43d04ba55e29d3a03a48a454bb78ac5a58e2d9b5

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -4608,6 +4608,10 @@ public class StatusBar extends SystemUI implements DemoMode,
         }
     }
 
+    public void updateQSDataUsageInfo() {
+        DataUsageView.updateUsage();
+    }
+
     public int getWakefulnessState() {
         return mWakefulnessLifecycle.getWakefulness();
     }
