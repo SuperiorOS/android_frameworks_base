ac0cd04168113bc2ead4ebba1f5c0ea52f6030e0
DennySPb <dennyspb@gmail.com>
2020-06-23 14:34:56 +0300

    SystemUI: Add four more selectable VoWiFi icons [1/2]
    - OnePlus
    - Asus
    - Huawei
    - Motorola
    
    Change-Id: I20c777891f0bf0ed8e65e50758012bd876bd41d5
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    Signed-off-by: Jayant-Deshmukh <jayantdeshmuk008@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/MobileSignalController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/MobileSignalController.java
@@ -114,6 +114,8 @@ public class MobileSignalController extends SignalController<
 
     // VoWiFi Icon
     private int mVoWiFiIcon;
+    // VoWiFi Icon Style
+    private int mVoWiFistyle;
 
     // TODO: Reduce number of vars passed in, if we have the NetworkController, probably don't
     // need listener lists anymore.
@@ -200,6 +202,9 @@ public class MobileSignalController extends SignalController<
            resolver.registerContentObserver(Settings.System.getUriFor(
                   Settings.System.VOWIFI_ICON),
                   false,this, UserHandle.USER_ALL);
+           resolver.registerContentObserver(Settings.System.getUriFor(
+                  Settings.System.VOWIFI_ICON_STYLE),
+                  false,this, UserHandle.USER_ALL);
            updateSettings();
         }
 
@@ -227,6 +232,10 @@ public class MobileSignalController extends SignalController<
         mVoWiFiIcon = Settings.System.getIntForUser(resolver,
                 Settings.System.VOWIFI_ICON, 0,
                 UserHandle.USER_CURRENT);
+        mVoWiFistyle = Settings.System.getIntForUser(resolver,
+                Settings.System.VOWIFI_ICON_STYLE, 0,
+                UserHandle.USER_CURRENT);
+
         mapIconSets();
         updateTelephony();
         notifyListeners();
@@ -849,7 +858,22 @@ public class MobileSignalController extends SignalController<
         if ( isVowifiAvailable() && !isCallIdle() ) {
             return TelephonyIcons.VOWIFI_CALLING;
         }else if (isVowifiAvailable()) {
-            return TelephonyIcons.VOWIFI;
+            switch(mVoWiFistyle) {
+                // OOS
+                case 1:
+                    return TelephonyIcons.VOWIFI_ONEPLUS;
+                // Motorola
+                case 2:
+                    return TelephonyIcons.VOWIFI_MOTO;
+                // ASUS
+                case 3:
+                    return TelephonyIcons.VOWIFI_ASUS;
+                // EMUI (Huawei P10)
+                case 4:
+                    return TelephonyIcons.VOWIFI_EMUI;
+                default:
+                    return TelephonyIcons.VOWIFI;
+            }
         }else {
             return null;
         }
