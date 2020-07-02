cfd77f4500f90e0960f716423515b8e21e52e19e
SKULSHADY <anushekprasal@gmail.com>
2020-07-02 20:18:41 +0200

    base: Switch Styles [1/3]
    @ZeNiXxX :Add stripped version of theme engine (based on DU Themes implementation)
    @idoybh edits: Add setting to backup and document
    
    Signed-off-by: Aman Singh <amankumarsingh1412@gmail.com>
    Signed-off-by: Jayant-Deshmukh <jayantdeshmuk008@gmail.com>
    Change-Id: I5e1b845093740cc182f0d7c8c213feaf843be9db

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6434,7 +6434,6 @@ public final class Settings {
         /** @hide */
         private static final Validator ASPECT_RATIO_APPS_ENABLED_VALIDATOR = BOOLEAN_VALIDATOR;
 
-
         /**
          * SBC HD priority - whether to use dual channel for SBC by default
          * @hide
@@ -6465,6 +6464,22 @@ public final class Settings {
         * @hide
         */
         public static final String AUDIO_PANEL_VIEW_THEME = "audio_panel_view_theme";
+
+        /**
+         * System-wide switch style
+         * 0 = Default
+         * 1 = OnePlus
+         * 2 = Narrow
+         * 3 = Contained
+         * 4 = Telegram
+         * 5 = Md2
+         * 6 = Retro
+         * @hide
+         */
+        public static final String SWITCH_STYLE = "switch_style";
+        /** @hide */
+        private static final Validator SWITCH_STYLE_VALIDATOR =
+                new SettingsValidators.InclusiveIntegerRangeValidator(0, 6);
 
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
@@ -6611,6 +6626,7 @@ public final class Settings {
             NAVIGATION_BAR_ARROW_KEYS,
             QS_DATAUSAGE,
             DATA_ACTIVITY_ARROW,
+            SWITCH_STYLE,
         };
 
         /**
@@ -7085,6 +7101,7 @@ public final class Settings {
                     NAVIGATION_BAR_ARROW_KEYS_VALIDATOR);
             VALIDATORS.put(QS_DATAUSAGE, QS_DATAUSAGE_VALIDATOR);
             VALIDATORS.put(DATA_ACTIVITY_ARROW, DATA_ACTIVITY_ARROW_VALIDATOR);
+            VALIDATORS.put(SWITCH_STYLE, SWITCH_STYLE_VALIDATOR);
         }
 
         /**
--- core/java/com/android/internal/util/superior/ThemesUtils.java
+++ core/java/com/android/internal/util/superior/ThemesUtils.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2014 The Android Open Source Project
+ * Copyright (C) 2020 ZenX-OS
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,7 +17,21 @@
 
 package com.android.internal.util.superior;
 
+import static android.os.UserHandle.USER_SYSTEM;
+
+import android.app.UiModeManager;
+import android.content.Context;
+import android.content.om.IOverlayManager;
+import android.content.om.OverlayInfo;
+import android.os.RemoteException;
+import android.os.ServiceManager;
+import android.provider.Settings;
+import android.os.RemoteException;
+import android.util.Log;
+
 public class ThemesUtils {
+
+    public static final String TAG = "ThemesUtils";
 
     public static final String[] SOLARIZED_DARK = {
             "com.android.theme.solarizeddark.system",
@@ -46,6 +61,41 @@ public class ThemesUtils {
             "com.android.theme.materialocean.system",
             "com.android.theme.materialocean.systemui",
     };
+
+    // Switch themes
+    private static final String[] SWITCH_THEMES = {
+        "com.android.system.switch.stock", // 0
+        "com.android.system.switch.oneplus", // 1
+        "com.android.system.switch.narrow", // 2
+        "com.android.system.switch.contained", // 3
+        "com.android.system.switch.telegram", // 4
+        "com.android.system.switch.md2", // 5
+        "com.android.system.switch.retro", // 6
+    };
+
+    public static void updateSwitchStyle(IOverlayManager om, int userId, int switchStyle) {
+        if (switchStyle == 0) {
+            stockSwitchStyle(om, userId);
+        } else {
+            try {
+                om.setEnabled(SWITCH_THEMES[switchStyle],
+                        true, userId);
+            } catch (RemoteException e) {
+                Log.w(TAG, "Can't change switch theme", e);
+            }
+        }
+    }
+
+    public static void stockSwitchStyle(IOverlayManager om, int userId) {
+        for (int i = 0; i < SWITCH_THEMES.length; i++) {
+            String switchtheme = SWITCH_THEMES[i];
+            try {
+                om.setEnabled(switchtheme,
+                        false /*disable*/, userId);
+            } catch (RemoteException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
 }
-
-
