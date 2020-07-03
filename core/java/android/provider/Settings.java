98f366855a5fdcec0526c381d36a8808266ac306
Hikari-no-Tenshi <kyryljan.serhij@gmail.com>
2020-07-03 18:03:39 +0300

    A2DP codec priority [1/3]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: Ic789805eacb87f8fccccacbd2b4c06bb6899099f
    Signed-off-by: Jayant-Deshmukh <jayantdeshmuk008@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6339,6 +6339,12 @@ public final class Settings {
         public static final String QS_RUNNING_SERVICES_TOGGLE = "qs_running_services_toggle";
 
         /**
+         * A2DP codec priority
+         * @hide
+         */
+        public static final String A2DP_SOURCE_CODEC_PRIORITY = "a2dp_source_codec_priority";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
