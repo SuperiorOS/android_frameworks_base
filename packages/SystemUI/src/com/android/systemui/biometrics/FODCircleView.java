1a27528c713b6ac372ddc75a588e4a4d1cf3f602
Hikari-no-Tenshi <kyryljan.serhij@gmail.com>
2020-07-05 22:58:01 +0300

    FODCircleView: Post calls of main hal functions on background thread
    Change-Id: Ie3175fd143a83a87f985e3cb3a668b9d589c5903
    Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>

--- packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
@@ -52,6 +52,7 @@ import com.android.internal.widget.LockPatternUtils;
 import com.android.keyguard.KeyguardSecurityModel.SecurityMode;
 import com.android.keyguard.KeyguardUpdateMonitor;
 import com.android.keyguard.KeyguardUpdateMonitorCallback;
+import com.android.settingslib.utils.ThreadUtils;
 import com.android.systemui.Dependency;
 import com.android.systemui.R;
 import com.android.systemui.tuner.TunerService;
@@ -491,7 +492,9 @@ public class FODCircleView extends ImageView implements TunerService.Tunable {
         setKeepScreenOn(true);
 
         setDim(true);
-        dispatchPress();
+        ThreadUtils.postOnBackgroundThread(() -> {
+            dispatchPress();
+        });
 
         setImageDrawable(null);
         updateIconDim(false);
@@ -504,7 +507,9 @@ public class FODCircleView extends ImageView implements TunerService.Tunable {
         setImageResource(ICON_STYLES[mSelectedIcon]);
         invalidate();
 
-        dispatchRelease();
+        ThreadUtils.postOnBackgroundThread(() -> {
+            dispatchRelease();
+        });
         setDim(false);
 
         setKeepScreenOn(false);
@@ -521,7 +526,9 @@ public class FODCircleView extends ImageView implements TunerService.Tunable {
         updatePosition();
 
         Dependency.get(TunerService.class).addTunable(this, SCREEN_BRIGHTNESS);
-        dispatchShow();
+        ThreadUtils.postOnBackgroundThread(() -> {
+            dispatchShow();
+        });
         setVisibility(View.VISIBLE);
     }
 
@@ -529,7 +536,9 @@ public class FODCircleView extends ImageView implements TunerService.Tunable {
         setVisibility(View.GONE);
         Dependency.get(TunerService.class).removeTunable(this);
         hideCircle();
-        dispatchHide();
+        ThreadUtils.postOnBackgroundThread(() -> {
+            dispatchHide();
+        });
     }
 
     private void updateAlpha() {
