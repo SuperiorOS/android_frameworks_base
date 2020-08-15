67d1567d46f9757c91d799534fe042483abc5124
Sipun Ku Mahanta <sipunkumar85@gmail.com>
2020-08-15 11:02:41 +0530

    [SQUASH] Drop Boostframework stuff
    Revert "BoostFramework: Add debug to hide logcat spam on devices without QPerformance"
    
    This reverts commit 6911ce3494fba515270c050c23f0a506ddea4931.
    
    Revert "perf: enable gesture fling boost by default"
    
    This reverts commit 71cc9cf224be284276220512fcd8e7fb59813453.
    
    Revert "BoostFramework: To Enhance performance."
    
    This reverts commit 9c6df844144e7387d11435592aa37c305402780f.
    
    Change-Id: I0abf06cb6b5d2bc5ae17b20441e11733a7a3a959
    
    Revert "cgroup follow for procs in the same cgroup.procs"
    
    This reverts commit dc6a1b48748adf0e3fe00176b84dc756ffd7a5ff.
    
    Revert "Ensure cgroup.procs in the same cgroup"
    
    This reverts commit 03ef7a6f00f9f7d86a71bc068d8c2bd4acd679ce.
    
    Change-Id: Ic9e7fcf4360ddba06fb21a179f6b7e266fc5a88c
    Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PanelView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PanelView.java
@@ -50,7 +50,6 @@ import com.android.systemui.statusbar.StatusBarState;
 import com.android.systemui.statusbar.SysuiStatusBarStateController;
 import com.android.systemui.statusbar.VibratorHelper;
 import com.android.systemui.statusbar.policy.KeyguardMonitor;
-import android.util.BoostFramework;
 import com.android.systemui.statusbar.notification.stack.NotificationStackScrollLayout;
 import com.android.systemui.statusbar.phone.StatusBar;
 
@@ -115,11 +114,6 @@ public abstract class PanelView extends FrameLayout {
     private FlingAnimationUtils mFlingAnimationUtilsDismissing;
     private final FalsingManager mFalsingManager;
     private final VibratorHelper mVibratorHelper;
-
-    /**
-     * For PanelView fling perflock call
-     */
-    private BoostFramework mPerf = null;
 
     /**
      * Whether an instant expand request is currently pending and we are just waiting for layout.
@@ -229,8 +223,6 @@ public abstract class PanelView extends FrameLayout {
         mVibratorHelper = Dependency.get(VibratorHelper.class);
         mVibrateOnOpening = mContext.getResources().getBoolean(
                 R.bool.config_vibrateOnIconAnimation);
-
-        mPerf = new BoostFramework();
     }
 
     protected void loadDimens() {
@@ -792,10 +784,6 @@ public abstract class PanelView extends FrameLayout {
             if (mFixedDuration != NO_FIXED_DURATION) {
                 animator.setDuration(mFixedDuration);
             }
-        }
-        if (mPerf != null) {
-            String currentPackage = mContext.getPackageName();
-            mPerf.perfHint(BoostFramework.VENDOR_HINT_SCROLL_BOOST, currentPackage, -1, BoostFramework.Scroll.PANEL_VIEW);
         }
         animator.addListener(new AnimatorListenerAdapter() {
             private boolean mCancelled;
