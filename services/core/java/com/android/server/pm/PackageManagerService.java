44886caa9141b53d24995914ae21112a82a18485
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-09-03 10:23:31 -0300

    PackageManagerService: Protect forced enabled components at runtime
    Change-Id: I1dc15786d08cde1879df8b7c6f9b73c4443ff168
    (cherry picked from commit d0886dc6ea3bb9f077848a0166a55007398da7cd)
    Signed-off-by: S I P U N <sipunkumar85@gmail.com>

--- services/core/java/com/android/server/pm/PackageManagerService.java
+++ services/core/java/com/android/server/pm/PackageManagerService.java
@@ -908,6 +908,7 @@ public class PackageManagerService implements PackageSender, TestUtilityService 
     private final PackageProperty mPackageProperty = new PackageProperty();
 
     ArrayList<ComponentName> mDisabledComponentsList;
+    ArrayList<ComponentName> mForceEnabledComponentsList;
 
     final PendingPackageBroadcasts mPendingBroadcasts;
 
@@ -1674,6 +1675,15 @@ public class PackageManagerService implements PackageSender, TestUtilityService 
         }
     }
 
+    private void loadForceEnabledComponents(){
+        String[] components = mContext.getResources().getStringArray(
+                    com.android.internal.R.array.config_forceEnabledComponents);
+        for (String name : components) {
+            ComponentName cn = ComponentName.unflattenFromString(name);
+            mForceEnabledComponentsList.add(cn);
+        }
+    }
+
     private void enableComponents(String[] components, boolean enable) {
         // Disable or enable components marked at build-time
         for (String name : components) {
@@ -2270,8 +2280,10 @@ public class PackageManagerService implements PackageSender, TestUtilityService 
                     com.android.internal.R.array.config_globallyDisabledComponents), false);
 
             // Enable components marked for forced-enable at build-time
+            mForceEnabledComponentsList = new ArrayList<ComponentName>();
             enableComponents(mContext.getResources().getStringArray(
                     com.android.internal.R.array.config_forceEnabledComponents), true);
+            loadForceEnabledComponents();
 
             // If this is first boot after an OTA, then we need to clear code cache directories.
             // Note that we do *not* clear the application profiles. These remain valid
@@ -5900,6 +5912,13 @@ public class PackageManagerService implements PackageSender, TestUtilityService 
             // Don't allow to enable components marked for disabling at build-time
             if (mDisabledComponentsList.contains(componentName)) {
                 Slog.d(TAG, "Ignoring attempt to set enabled state of disabled component "
+                        + componentName.flattenToString());
+                return;
+            }
+
+            // Don't allow to control components forced enabled at build-time
+            if (mForceEnabledComponentsList.contains(componentName)) {
+                Slog.d(TAG, "Ignoring attempt to control forced enabled component "
                         + componentName.flattenToString());
                 return;
             }
