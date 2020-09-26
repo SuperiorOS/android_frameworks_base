3e962e2de2a08d6c71fc9b5b9485d126d11faf5a
Adithya R <gh0strider.2k18.reborn@gmail.com>
2020-09-26 13:50:48 +0530

    base: Allow devices to specify their own pickup sensor type
     * Most xiaomi and oneplus devices have a different pickup sensor type such as
       xiaomi.sensor.pickup or com.oneplus.sensor.pickup, while some devices may want
       to use their tilt sensor as pickup sensor. Let's allow these devices to leverage
       AOSP's lift-to-wake function by making the pickup sensor type configurable via
       overlay.
    
    Change-Id: I454f43e7acf81226d3e4c56111990d26beda05a8

--- core/java/android/hardware/display/AmbientDisplayConfiguration.java
+++ core/java/android/hardware/display/AmbientDisplayConfiguration.java
@@ -126,6 +126,11 @@ public class AmbientDisplayConfiguration {
     }
 
     /** {@hide} */
+    public String pickupSensorType() {
+        return mContext.getResources().getString(R.string.config_dozePickupSensorType);
+    }
+
+    /** {@hide} */
     public String doubleTapSensorType() {
         return mContext.getResources().getString(R.string.config_dozeDoubleTapSensorType);
     }
