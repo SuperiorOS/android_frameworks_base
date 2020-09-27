6f706105167d2d5803d47e23e570dde2876eb89f
Kshitij Gupta <kshitijgm@gmail.com>
2020-09-27 17:52:21 +0530

    SystemUI: add reloadAllTiles
    only method picked from QS Themes Commit of Q
    
    Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/QSTileHost.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSTileHost.java
@@ -212,8 +212,7 @@ public class QSTileHost implements QSHost, Tunable, PluginListener<QSFactory>, D
         mQsFactories.remove(plugin);
         // Force remove and recreate of all tiles.
         String value = mTunerService.getValue(TILES_SETTING);
-        onTuningChanged(TILES_SETTING, "");
-        onTuningChanged(TILES_SETTING, value);
+        reloadAllTiles();
     }
 
     @Override
@@ -273,6 +272,13 @@ public class QSTileHost implements QSHost, Tunable, PluginListener<QSFactory>, D
 
     public int indexOf(String spec) {
         return mTileSpecs.indexOf(spec);
+    }
+
+    public void reloadAllTiles() {
+        // Force remove and recreate of all tiles.
+        String value = mTunerService.getValue(TILES_SETTING);
+        onTuningChanged(TILES_SETTING, "");
+        onTuningChanged(TILES_SETTING, value);
     }
 
     /**
