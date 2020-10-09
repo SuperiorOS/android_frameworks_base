12d17d0dc0dd35380f85c95645908d33d239dfc0
Ido Ben-Hur <idoybh2@gmail.com>
2020-10-09 15:16:28 +0300

    base: Allow hiding notification headers [1/2]
    Gimme back my space, thank you
    
    Change-Id: I5f63a384d2bd2052d4a8a9d49aff1456d743367f

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5678,6 +5678,13 @@ public final class Settings {
         public static final String STATUSBAR_ICONS_STYLE = "statusbar_icons_style";
 
         /**
+         * Controls whether to show notification headers
+         * @hide
+         */
+        @Readable
+        public static final String NOTIFICATION_HEADERS = "notification_headers";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
