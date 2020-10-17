25dddd9bfa5a22823ceef09df33777e42ae1e76d
Marko Man <darkobas@gmail.com>
2020-10-17 21:42:41 +0200

    SystemUI: add timeout for ambient notification pulse
    Change-Id: Ifb2a7f4e14711b522fbfe8b9c546ea78c48af268
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    Signed-off-by: Jayant-Deshmukh <jayantdeshmuk008@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -1356,6 +1356,7 @@ public class StatusBar extends SystemUI implements DemoMode,
         filter.addAction(Intent.ACTION_SCREEN_OFF);
         filter.addAction(DevicePolicyManager.ACTION_SHOW_DEVICE_MONITORING_DIALOG);
         filter.addAction(Utils.ACTION_DISMISS_KEYGUARD);
+        filter.addAction(NotificationPanelViewController.CANCEL_NOTIFICATION_PULSE_ACTION);
         mBroadcastDispatcher.registerReceiver(mBroadcastReceiver, filter, null, UserHandle.ALL);
     }
 
@@ -2867,6 +2868,9 @@ public class StatusBar extends SystemUI implements DemoMode,
             }
             else if (DevicePolicyManager.ACTION_SHOW_DEVICE_MONITORING_DIALOG.equals(action)) {
                 mQSPanel.showDeviceMonitoringDialog();
+            }
+            else if (NotificationPanelViewController.CANCEL_NOTIFICATION_PULSE_ACTION.equals(action)) {
+                mNotificationPanelViewController.stopNotificationPulse();
             }
             else if (Utils.ACTION_DISMISS_KEYGUARD.equals(action)) {
                 if (intent.hasExtra(Utils.DISMISS_KEYGUARD_EXTRA_INTENT)) {
