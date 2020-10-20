058b5af36a9f0c9000fb54fdb5ed4287ba248c95
Hikari-no-Tenshi <kyryljan.serhij@gmail.com>
2020-10-20 16:40:11 +0300

    FODCircleView: Call show from onStartedWakingUp
    * On OnePlus 7 aka guacamoleb when turning screen on by power button
      in some cases short flash of brightness are observed.
      Method onStartedWakingUp are called little earlier than onScreenTurnedOn
      so lets call show from onStartedWakingUp so dim layer will have more time
      to hide hbm before screen will be turned on.
    
    * Do not show when already shown.
    
    Change-Id: I9421d29f29f1f49e95401a10500ac14b80627caf
    Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>

--- packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
@@ -109,6 +109,7 @@ public class FODCircleView extends ImageView {
 
     private boolean mIsBouncer;
     private boolean mIsDreaming;
+    private boolean mIsScreenTurnedOn;
     private boolean mIsCircleShowing;
     private boolean mIsAnimating = false;
 
@@ -165,11 +166,13 @@ public class FODCircleView extends ImageView {
 
         @Override
         public void onScreenTurnedOff() {
+            mIsScreenTurnedOn = false;
             hide();
         }
 
         @Override
         public void onScreenTurnedOn() {
+            mIsScreenTurnedOn = true;
             if (mUpdateMonitor.isFingerprintDetectionRunning()) {
                 show();
             }
