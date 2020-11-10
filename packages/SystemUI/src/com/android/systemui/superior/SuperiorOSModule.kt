e8ab7427c5fd3d4e10fb9f28ddb37b483efcbe80
Keith Mok <kmok@cyngn.com>
2020-11-10 10:47:09 -0300

    SystemUI: Add caffeine QS tile
    * Caffeine Tile icon designed by Andrew Fluck
    
    [Adin Kwok] Add long press to infinity to caffeine qs tile
    [ghostrider-reborn] Updated for android 12
    [Stallix] Adapt for A13 QPR3
    
    Co-authored-by: Adin Kwok <adin@adin.dev>
    Change-Id: If54ea27ddf179c2de39b61850163e14173727698
    Signed-off-by: Adithya <gh0strider.2k18.reborn@gmail.com>
    Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>

--- packages/SystemUI/src/com/android/systemui/superior/SuperiorOSModule.kt
+++ packages/SystemUI/src/com/android/systemui/superior/SuperiorOSModule.kt
@@ -1,4 +1,3 @@
-
 /*
  * Copyright (C) 2023 The SuperiorOS Project
  *
@@ -18,6 +17,7 @@
 package com.android.systemui.superior
 
 import com.android.systemui.qs.tileimpl.QSTileImpl
+import com.android.systemui.qs.tiles.CaffeineTile
 
 import dagger.Binds
 import dagger.Module
@@ -26,5 +26,11 @@ import dagger.multibindings.StringKey
 
 @Module
 interface SuperiorOSModule {
+
+    /** Inject CaffeineTile into tileMap in QSModule */
+    @Binds
+    @IntoMap
+    @StringKey(CaffeineTile.TILE_SPEC)
+    fun bindCaffeineTile(caffeineTile: CaffeineTile): QSTileImpl<*>
 
 }
