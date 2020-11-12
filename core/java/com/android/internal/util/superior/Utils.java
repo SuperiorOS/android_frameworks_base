6055a4c0fe7a52eeb3478bbf41a06aea91c1f458
ezio84 <brabus84@gmail.com>
2020-11-12 22:06:29 +0100

    ActionUtils: Reverse actions for QS and notification panel
    This just makes sense, so lets do it!
    
    - Update method names in PWM
    
    Change-Id: Ic08bee12c90dc55265dea3c3972f4087b93d2826
    Signed-off-by: Jayant-Deshmukh <jayantdeshmuk008@gmail.com>

--- core/java/com/android/internal/util/superior/Utils.java
+++ core/java/com/android/internal/util/superior/Utils.java
@@ -262,7 +262,7 @@ public class Utils {
             IStatusBarService service = getStatusBarService();
             if (service != null) {
                 try {
-                    service.expandSettingsPanel(null);
+                    service.toggleSettingsPanel();
                 } catch (RemoteException e) {}
             }
         }
