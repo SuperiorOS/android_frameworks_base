448243e843e5b5e56b0ee7da36a56dbedf8739de
Jayant-Deshmukh <jayantdeshmuk008@gmail.com>
2020-11-29 08:10:32 +0000

    Revert "SystemUI: assist: disable old Assistant animations"
    This reverts commit f6d616f805b387c679ccda09c8b77c3c1d3f9985.
    
    Change-Id: Ie0f95f7da11ce92113e0fe6dff1adb96b6bb3d22

--- packages/SystemUI/src/com/android/systemui/assist/AssistManager.java
+++ packages/SystemUI/src/com/android/systemui/assist/AssistManager.java
@@ -396,6 +396,10 @@ public class AssistManager {
         intent.setComponent(assistComponent);
         intent.putExtras(args);
 
+        if (structureEnabled && AssistUtils.isDisclosureEnabled(mContext)) {
+            showDisclosure();
+        }
+
         try {
             final ActivityOptions opts = ActivityOptions.makeCustomAnimation(mContext,
                     R.anim.search_launch_enter, R.anim.search_launch_exit);
@@ -486,7 +490,9 @@ public class AssistManager {
         return getAssistInfoForUser(KeyguardUpdateMonitor.getCurrentUser());
     }
 
-    public void showDisclosure() {}
+    public void showDisclosure() {
+        mAssistDisclosure.postShow();
+    }
 
     public void onLockscreenShown() {
         AsyncTask.execute(new Runnable() {
