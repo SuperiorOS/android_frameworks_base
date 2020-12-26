9ae002822fb1a41edb5127eda4c130f715d6de1a
Sipun Ku Mahanta <sipunkumar85@gmail.com>
2020-12-26 21:22:17 +0530

    [SQUASH]Revert FOD Implementation
    Revert "FODCircleView: Bring back the background"
    
    This reverts commit b9ed534194ad037d39608f901d7833f2ff2c6c91.
    
    Revert "FODCircleView: Improve position update"
    
    This reverts commit eb9ffe21ee13af49772d4e48906905456887aafc.
    
    Revert "SystemUI: Fod: Aod content should move only in Y"
    
    This reverts commit 2a6a3df01433571d1c3135f6feabc3e2cb764425.
    
    Revert "FODCircleView: Update FOD icon position on DreamingStateChanged"
    
    This reverts commit eeebdfefa15c904857e93456e24b3c1d2cea2877.
    
    Revert "FODCircleView: Call show from onStartedWakingUp"
    
    This reverts commit 058b5af36a9f0c9000fb54fdb5ed4287ba248c95.
    
    Revert "FODCircleView: Use a ContentObserver to check screen brightness"
    
    This reverts commit 8adec6bba10167662cf54a74a65e57f064864c96.
    
    Revert "Don't start fingerprint for FOD devices early"
    
    This reverts commit 8a1e9d42513b86e5c4f7553ac04f44aa0c84d978.
    
    Revert "FODCircleView: Apply color filter to FOD background"
    
    This reverts commit 34d588f0e7d97180b2df9931eab4f8365042fea0.
    
    Revert "FODCircleView: Enable hardware acceleration"
    
    This reverts commit 94b272170a35d0a946805e95573d39b712edb2aa.
    
    Revert "FODCircleView: Post calls of main hal functions on background thread"
    
    This reverts commit 76e8d23c3b1413ffa78d6e1cf81e63817afa6dbb.
    
    Revert "FODCircleView: Use Handler for Icon dim color filter"
    
    This reverts commit d7f9e896137898de2347ed11fe792afb16bc66b2.
    
    Revert "SystemUI: FODCircleView: Animate the icon dim"
    
    This reverts commit 4da175afee3c6eabee0795fd1a456aea514a9824.
    
    Revert "FODCircleView: Dim FOD icon"
    
    This reverts commit c5895c6a79245fbfbf65d916fedf45f883446b20.
    
    Revert "FODCircleView: Only reset brightness if brightness boost is enabled"
    
    This reverts commit 260ed5bf6a24abd8f3cc1b1d2c28403ea6242af1.
    
    Revert "FODCircleView: Add support for custom FP pressed icon"
    
    This reverts commit 01ff478ba055936b1fd30f0d67739f69c248e568.
    
    Revert "FODCircleView: Add support for pressed fod layer"
    
    This reverts commit 08205b64819cbe588c191e6029e0abe0c277f446.
    
    Revert "Revert "FODCircleView: dispatch onPress to HAL only after dimming is applied""
    
    This reverts commit d80cb98a9ac0b41f45ed2d58990d946ca09f650a.
    
    Revert "FODCircleView: Let us call show() when mIsBouncer() && !isPinOrPattern()"
    
    This reverts commit e43b4ce40772189de8dad859a152f145e5eed4d1.
    
    Revert "FODCircleView: Show while bouncer is requesting Pattern or PIN"
    
    This reverts commit 8b27deb4030caaec6461264d91eafdfed5c9c8be.
    
    Revert "FODCircleView: update position of icon before show"
    
    This reverts commit 405b850ecdb49c9123477ab6cdc697910f3f38df.
    
    Revert "FODCircleView: disable touch detection on screen off"
    
    This reverts commit 47cdca58244fb864985e1dcbf400337625616c15.
    
    Revert "FODCircleView: dispatch onPress to HAL only after dimming is applied"
    
    This reverts commit 1d36de54b5d42f425bbf66b961f326c0b92ee665.
    
    Revert "FODCircleView: invert default icon path and add #20000000 bg"
    
    This reverts commit 738b105770d58691b8db787002c7435471161418.
    
    Revert "KeyguardView: report transiting bouncer as shown"
    
    This reverts commit 62c0ad04cd070f77c68be1306c35f21e0cb7b481.
    
    Revert "FODCircleView: rewrite and simplify implementation"
    
    This reverts commit a00a95f43272542c4d03fed8e00f19a07c825e62.
    
    Revert "FODCircleView: place above other UI elements"
    
    This reverts commit 27ae24e2d44d9e83076ecac43f73a29a537ece6a.
    
    Revert "FODCircleView: defer removal to next re-layout"
    
    This reverts commit 90efad8f50c3a56f070c8d78dd16f48ae8e4b2b2.
    
    Revert "Initial support for in-display fingerprint sensors"
    
    This reverts commit 0ab9f2a73616b50e10467bfcb6e2dc3fe99894f6.
    
    Change-Id: I66abc5594dbaefba5e9fd5c23efbd8e48f639593
    Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>

--- services/core/java/com/android/server/biometrics/fingerprint/FingerprintService.java
+++ services/core/java/com/android/server/biometrics/fingerprint/FingerprintService.java
@@ -80,16 +80,12 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-
-import vendor.lineage.biometrics.fingerprint.inscreen.V1_0.IFingerprintInscreen;
-
 import java.io.File;
 import java.io.FileDescriptor;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.NoSuchElementException;
 import java.util.concurrent.CopyOnWriteArrayList;
 
 /**
@@ -110,9 +106,6 @@ public class FingerprintService extends BiometricServiceBase {
     private static final int MAX_FAILED_ATTEMPTS_LOCKOUT_PERMANENT = 20;
     private static final long FAIL_LOCKOUT_TIMEOUT_MS = 30 * 1000;
     private static final String KEY_LOCKOUT_RESET_USER = "lockout_reset_user";
-
-    private final boolean mHasFod;
-    private boolean mIsKeyguard;
 
     private final class ResetFailedAttemptsForUserRunnable implements Runnable {
         @Override
@@ -648,7 +641,6 @@ public class FingerprintService extends BiometricServiceBase {
 
     @GuardedBy("this")
     private IBiometricsFingerprint mDaemon;
-    private IFingerprintInscreen mFingerprintInscreenDaemon;
     private final SparseBooleanArray mTimedLockoutCleared;
     private final SparseIntArray mFailedAttempts;
     private final AlarmManager mAlarmManager;
@@ -670,21 +662,6 @@ public class FingerprintService extends BiometricServiceBase {
                         new Fingerprint(getBiometricUtils().getUniqueName(getContext(), groupId),
                                 groupId, fingerId, deviceId);
                 FingerprintService.super.handleEnrollResult(fingerprint, remaining);
-                if (remaining == 0 && mHasFod) {
-                    IFingerprintInscreen fodDaemon = getFingerprintInScreenDaemon();
-                    if (fodDaemon != null) {
-                        try {
-                            fodDaemon.onFinishEnroll();
-                        } catch (RemoteException e) {
-                            Slog.e(TAG, "onFinishEnroll failed", e);
-                        }
-                    }
-                    try {
-                        mStatusBarService.hideInDisplayFingerprintView();
-                    } catch (RemoteException e) {
-                        Slog.e(TAG, "hideInDisplayFingerprintView failed", e);
-                    }
-                }
             });
         }
 
@@ -696,16 +673,6 @@ public class FingerprintService extends BiometricServiceBase {
         @Override
         public void onAcquired_2_2(long deviceId, int acquiredInfo, int vendorCode) {
             mHandler.post(() -> {
-                IFingerprintInscreen daemon = getFingerprintInScreenDaemon();
-                if (daemon != null) {
-                    try {
-                        if (daemon.handleAcquired(acquiredInfo, vendorCode)) {
-                            return;
-                        }
-                    } catch (RemoteException e) {
-                        Slog.e(TAG, "handleError failed", e);
-                    }
-                }
                 FingerprintService.super.handleAcquired(deviceId, acquiredInfo, vendorCode);
             });
         }
@@ -722,32 +689,14 @@ public class FingerprintService extends BiometricServiceBase {
                         authenticated = true;
                     }
                 }
-
-                final Fingerprint fp = new Fingerprint("", groupId, fingerId, deviceId);
-                FingerprintService.super.handleAuthenticated(authenticated, fp, token);
-                if (mHasFod && fp.getBiometricId() != 0) {
-                    try {
-                        mStatusBarService.hideInDisplayFingerprintView();
-                    } catch (RemoteException e) {
-                        Slog.e(TAG, "hideInDisplayFingerprintView failed", e);
-                    }
-                }
+                Fingerprint fp = new Fingerprint("", groupId, fingerId, deviceId);
+                FingerprintService.super.handleAuthenticated(fp, token);
             });
         }
 
         @Override
         public void onError(final long deviceId, final int error, final int vendorCode) {
             mHandler.post(() -> {
-                IFingerprintInscreen daemon = getFingerprintInScreenDaemon();
-                if (daemon != null) {
-                    try {
-                        if (daemon.handleError(error, vendorCode)) {
-                            return;
-                        }
-                    } catch (RemoteException e) {
-                        Slog.e(TAG, "handleError failed", e);
-                    }
-                }
                 FingerprintService.super.handleError(deviceId, error, vendorCode);
                 // TODO: this chunk of code should be common to all biometric services
                 if (error == BiometricConstants.BIOMETRIC_ERROR_HW_UNAVAILABLE) {
@@ -795,21 +744,6 @@ public class FingerprintService extends BiometricServiceBase {
                 Slog.w(TAG, "authenticate(): no fingerprint HAL!");
                 return ERROR_ESRCH;
             }
-            if (mHasFod) {
-                IFingerprintInscreen fodDaemon = getFingerprintInScreenDaemon();
-                if (fodDaemon != null) {
-                    try {
-                        fodDaemon.setLongPressEnabled(mIsKeyguard);
-                    } catch (RemoteException e) {
-                        Slog.e(TAG, "setLongPressEnabled failed", e);
-                    }
-                }
-                try {
-                    mStatusBarService.showInDisplayFingerprintView();
-                } catch (RemoteException e) {
-                    Slog.e(TAG, "showInDisplayFingerprintView failed", e);
-                }
-            }
             return daemon.authenticate(operationId, groupId);
         }
 
@@ -819,13 +753,6 @@ public class FingerprintService extends BiometricServiceBase {
             if (daemon == null) {
                 Slog.w(TAG, "cancel(): no fingerprint HAL!");
                 return ERROR_ESRCH;
-            }
-            if (mHasFod) {
-                try {
-                    mStatusBarService.hideInDisplayFingerprintView();
-                } catch (RemoteException e) {
-                    Slog.e(TAG, "hideInDisplayFingerprintView failed", e);
-                }
             }
             return daemon.cancel();
         }
@@ -858,21 +785,6 @@ public class FingerprintService extends BiometricServiceBase {
                 Slog.w(TAG, "enroll(): no fingerprint HAL!");
                 return ERROR_ESRCH;
             }
-            if (mHasFod) {
-                IFingerprintInscreen fodDaemon = getFingerprintInScreenDaemon();
-                if (fodDaemon != null) {
-                    try {
-                        fodDaemon.onStartEnroll();
-                    } catch (RemoteException e) {
-                        Slog.e(TAG, "onStartEnroll failed", e);
-                    }
-                }
-                try {
-                    mStatusBarService.showInDisplayFingerprintView();
-                } catch (RemoteException e) {
-                    Slog.e(TAG, "showInDisplayFingerprintView failed", e);
-                }
-            }
             return daemon.enroll(cryptoToken, groupId, timeout);
         }
 
@@ -891,7 +803,6 @@ public class FingerprintService extends BiometricServiceBase {
         mAlarmManager = context.getSystemService(AlarmManager.class);
         context.registerReceiver(mLockoutReceiver, new IntentFilter(getLockoutResetIntent()),
                 getLockoutBroadcastPermission(), null /* handler */);
-        mHasFod = context.getResources().getBoolean(com.android.internal.R.bool.config_needCustomFODView);
         mLockPatternUtils = new LockPatternUtils(context);
     }
 
@@ -977,7 +888,6 @@ public class FingerprintService extends BiometricServiceBase {
 
                     daemon.setActiveGroup(userId, fpDir.getAbsolutePath());
                     mCurrentUserId = userId;
-                    mIsKeyguard = isKeyguard(clientPackage);
                 }
                 mAuthenticatorIds.put(userId,
                         hasEnrolledBiometrics(userId) ? daemon.getAuthenticatorId() : 0L);
@@ -1113,26 +1023,6 @@ public class FingerprintService extends BiometricServiceBase {
             }
         }
         return mDaemon;
-    }
-
-    private synchronized IFingerprintInscreen getFingerprintInScreenDaemon() {
-        if (!mHasFod) {
-            return null;
-        }
-
-        if (mFingerprintInscreenDaemon == null) {
-            try {
-                mFingerprintInscreenDaemon = IFingerprintInscreen.getService();
-                if (mFingerprintInscreenDaemon != null) {
-                    mFingerprintInscreenDaemon.asBinder().linkToDeath((cookie) -> {
-                        mFingerprintInscreenDaemon = null;
-                    }, 0);
-                }
-            } catch (NoSuchElementException | RemoteException e) {
-                Slog.e(TAG, "Failed to get IFingerprintInscreen interface", e);
-            }
-        }
-        return mFingerprintInscreenDaemon;
     }
 
     private long startPreEnroll(IBinder token) {
