9424df336186d1547eb5660502efb546a8fd00a9
Sipun Ku Mahanta <sipunkumar85@gmail.com>
2020-12-26 21:22:17 +0530

    [SQUASH]Revert FOD Implementation
    Revert "FODCircleView: Bring back the background"
    
    This reverts commit b9ed534194ad037d39608f901d7833f2ff2c6c91.
    
    Revert "FODCircleView: Improve position update"
    
    This reverts commit eb9ffe21ee13af49772d4e48906905456887aafc.
    
    Revert "SystemUI: Fod: Aod content should move only in Y"
    
    This reverts commit 2a6a3df01433571d1c3135f6feabc3e2cb764425.
    
    Revert "FODCircleView: Update FOD icon position on DreamingStateChanged"
    
    This reverts commit eeebdfefa15c904857e93456e24b3c1d2cea2877.
    
    Revert "FODCircleView: Call show from onStartedWakingUp"
    
    This reverts commit 058b5af36a9f0c9000fb54fdb5ed4287ba248c95.
    
    Revert "FODCircleView: Use a ContentObserver to check screen brightness"
    
    This reverts commit 8adec6bba10167662cf54a74a65e57f064864c96.
    
    Revert "Don't start fingerprint for FOD devices early"
    
    This reverts commit 8a1e9d42513b86e5c4f7553ac04f44aa0c84d978.
    
    Revert "FODCircleView: Apply color filter to FOD background"
    
    This reverts commit 34d588f0e7d97180b2df9931eab4f8365042fea0.
    
    Revert "FODCircleView: Enable hardware acceleration"
    
    This reverts commit 94b272170a35d0a946805e95573d39b712edb2aa.
    
    Revert "FODCircleView: Post calls of main hal functions on background thread"
    
    This reverts commit 76e8d23c3b1413ffa78d6e1cf81e63817afa6dbb.
    
    Revert "FODCircleView: Use Handler for Icon dim color filter"
    
    This reverts commit d7f9e896137898de2347ed11fe792afb16bc66b2.
    
    Revert "SystemUI: FODCircleView: Animate the icon dim"
    
    This reverts commit 4da175afee3c6eabee0795fd1a456aea514a9824.
    
    Revert "FODCircleView: Dim FOD icon"
    
    This reverts commit c5895c6a79245fbfbf65d916fedf45f883446b20.
    
    Revert "FODCircleView: Only reset brightness if brightness boost is enabled"
    
    This reverts commit 260ed5bf6a24abd8f3cc1b1d2c28403ea6242af1.
    
    Revert "FODCircleView: Add support for custom FP pressed icon"
    
    This reverts commit 01ff478ba055936b1fd30f0d67739f69c248e568.
    
    Revert "FODCircleView: Add support for pressed fod layer"
    
    This reverts commit 08205b64819cbe588c191e6029e0abe0c277f446.
    
    Revert "Revert "FODCircleView: dispatch onPress to HAL only after dimming is applied""
    
    This reverts commit d80cb98a9ac0b41f45ed2d58990d946ca09f650a.
    
    Revert "FODCircleView: Let us call show() when mIsBouncer() && !isPinOrPattern()"
    
    This reverts commit e43b4ce40772189de8dad859a152f145e5eed4d1.
    
    Revert "FODCircleView: Show while bouncer is requesting Pattern or PIN"
    
    This reverts commit 8b27deb4030caaec6461264d91eafdfed5c9c8be.
    
    Revert "FODCircleView: update position of icon before show"
    
    This reverts commit 405b850ecdb49c9123477ab6cdc697910f3f38df.
    
    Revert "FODCircleView: disable touch detection on screen off"
    
    This reverts commit 47cdca58244fb864985e1dcbf400337625616c15.
    
    Revert "FODCircleView: dispatch onPress to HAL only after dimming is applied"
    
    This reverts commit 1d36de54b5d42f425bbf66b961f326c0b92ee665.
    
    Revert "FODCircleView: invert default icon path and add #20000000 bg"
    
    This reverts commit 738b105770d58691b8db787002c7435471161418.
    
    Revert "KeyguardView: report transiting bouncer as shown"
    
    This reverts commit 62c0ad04cd070f77c68be1306c35f21e0cb7b481.
    
    Revert "FODCircleView: rewrite and simplify implementation"
    
    This reverts commit a00a95f43272542c4d03fed8e00f19a07c825e62.
    
    Revert "FODCircleView: place above other UI elements"
    
    This reverts commit 27ae24e2d44d9e83076ecac43f73a29a537ece6a.
    
    Revert "FODCircleView: defer removal to next re-layout"
    
    This reverts commit 90efad8f50c3a56f070c8d78dd16f48ae8e4b2b2.
    
    Revert "Initial support for in-display fingerprint sensors"
    
    This reverts commit 0ab9f2a73616b50e10467bfcb6e2dc3fe99894f6.
    
    Change-Id: I66abc5594dbaefba5e9fd5c23efbd8e48f639593
    Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/CommandQueue.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/CommandQueue.java
@@ -128,8 +128,6 @@ public class CommandQueue extends IStatusBar.Stub implements CallbackController<
     private static final int MSG_SUPPRESS_AMBIENT_DISPLAY          = 56 << MSG_SHIFT;
     private static final int MSG_TOGGLE_CAMERA_FLASH               = 57 << MSG_SHIFT;
     private static final int MSG_SET_BLOCKED_GESTURAL_NAVIGATION   = 58 << MSG_SHIFT;
-    private static final int MSG_SHOW_IN_DISPLAY_FINGERPRINT_VIEW  = 59 << MSG_SHIFT;
-    private static final int MSG_HIDE_IN_DISPLAY_FINGERPRINT_VIEW  = 60 << MSG_SHIFT;
     private static final int MSG_TOGGLE_SETTINGS_PANEL             = 61 << MSG_SHIFT;
     private static final int MSG_KILL_FOREGROUND_APP               = 62 << MSG_SHIFT;
     private static final int MSG_SCREEN_PINNING_STATE_CHANGED      = 63 << MSG_SHIFT;
@@ -278,8 +276,6 @@ public class CommandQueue extends IStatusBar.Stub implements CallbackController<
         default void onBiometricError(int modality, int error, int vendorCode) { }
         default void hideAuthenticationDialog() { }
         default void setBlockedGesturalNavigation(boolean blocked) {}
-        default void showInDisplayFingerprintView() { }
-        default void hideInDisplayFingerprintView() { }
 
         /**
          * @see IStatusBar#onDisplayReady(int)
@@ -892,20 +888,6 @@ public class CommandQueue extends IStatusBar.Stub implements CallbackController<
     }
 
     @Override
-    public void showInDisplayFingerprintView() {
-        synchronized (mLock) {
-            mHandler.obtainMessage(MSG_SHOW_IN_DISPLAY_FINGERPRINT_VIEW).sendToTarget();
-        }
-    }
-
-    @Override
-    public void hideInDisplayFingerprintView() {
-        synchronized (mLock) {
-            mHandler.obtainMessage(MSG_HIDE_IN_DISPLAY_FINGERPRINT_VIEW).sendToTarget();
-        }
-    }
-
-    @Override
     public void onDisplayReady(int displayId) {
         synchronized (mLock) {
             mHandler.obtainMessage(MSG_DISPLAY_READY, displayId, 0).sendToTarget();
@@ -1393,7 +1375,6 @@ public class CommandQueue extends IStatusBar.Stub implements CallbackController<
                         callbacks.suppressAmbientDisplay((boolean) msg.obj);
                     }
                     break;
-
                 case MSG_TOGGLE_CAMERA_FLASH:
                     for (int i = 0; i < mCallbacks.size(); i++) {
                         mCallbacks.get(i).toggleCameraFlash();
@@ -1402,16 +1383,6 @@ public class CommandQueue extends IStatusBar.Stub implements CallbackController<
                 case MSG_SET_BLOCKED_GESTURAL_NAVIGATION:
                     for (int i = 0; i < mCallbacks.size(); i++) {
                         mCallbacks.get(i).setBlockedGesturalNavigation((Boolean) msg.obj);
-                    }
-                    break;
-                case MSG_SHOW_IN_DISPLAY_FINGERPRINT_VIEW:
-                    for (int i = 0; i < mCallbacks.size(); i++) {
-                        mCallbacks.get(i).showInDisplayFingerprintView();
-                    }
-                    break;
-                case MSG_HIDE_IN_DISPLAY_FINGERPRINT_VIEW:
-                    for (int i = 0; i < mCallbacks.size(); i++) {
-                        mCallbacks.get(i).hideInDisplayFingerprintView();
                     }
                     break;
                 case MSG_KILL_FOREGROUND_APP:
