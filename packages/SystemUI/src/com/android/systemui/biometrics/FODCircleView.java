3a5f17753c67aed3143fbdb8dff8ccd8417145b2
Sipun Ku Mahanta <sipunkumar85@gmail.com>
2020-12-31 20:49:19 +0530

    FOD: Remove some stuffs
    - We r not evolution X.
    
    Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>
    Change-Id: I2c00d71c3eb50d8d8c6a16c73776ee47f08753a6

--- packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
@@ -154,7 +154,6 @@ public class FODCircleView extends ImageView {
         R.drawable.fod_icon_cpt_america_flat,
         R.drawable.fod_icon_cpt_america_flat_gray,
         R.drawable.fod_icon_dragon_black_flat,
-        R.drawable.fod_icon_evo1,
         R.drawable.fod_icon_glow_circle,
         R.drawable.fod_icon_neon_arc,
         R.drawable.fod_icon_neon_arc_gray,
