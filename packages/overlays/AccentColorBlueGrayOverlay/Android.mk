109b46ac85aa037e52d803b6762bce1cf0c3b32a
harsh-23 <harsh.v23labs@gmail.com>
2021-01-16 16:55:59 +0000

    overlays: Add some accent colors from Q
    * Drop deep red and update red accents
    
    Signed-off-by: Joey Huab <joey@evolution-x.org>
    Signed-off-by: Joker-V2 <nipinna0@gmail.com>
    Change-Id: I2cf9a929d44045416a1a09030ae7734cda503eb1

--- packages/overlays/AccentColorBlueGrayOverlay/Android.mk
+++ packages/overlays/AccentColorBlueGrayOverlay/Android.mk
@@ -17,13 +17,15 @@
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
-LOCAL_RRO_THEME := AccentColorDeepRed
+LOCAL_RRO_THEME := AccentColorBlueGray
 
 LOCAL_PRODUCT_MODULE := true
 
+LOCAL_SRC_FILES := $(call all-subdir-java-files)
+
 LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
 
-LOCAL_PACKAGE_NAME := AccentColorDeepRedOverlay
+LOCAL_PACKAGE_NAME := AccentColorBlueGrayOverlay
 LOCAL_SDK_VERSION := current
 
 include $(BUILD_RRO_PACKAGE)
