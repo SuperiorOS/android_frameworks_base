06c11f4c5a82d18e51e24abdc0de48b65716e21e
Michael W <baddaemon87@gmail.com>
2021-02-04 17:49:58 +0100

    SystemUI: Screenshots: Refactor shutter sound logic
    * Move it to an own method to reduce the footprint in the AOSP code parts
      and to reduce duplication
    
    @ Stallix  - Forward port to A13.
    
    Change-Id: I57eaaee4db401d16cc6ef65c68604cdb4053ca01

--- packages/SystemUI/src/com/android/systemui/screenshot/ScreenshotController.java
+++ packages/SystemUI/src/com/android/systemui/screenshot/ScreenshotController.java
@@ -918,24 +918,8 @@ public class ScreenshotController {
      * failure).
      */
     private void saveScreenshotAndToast(UserHandle owner, Consumer<Uri> finisher) {
-        switch (mAudioManager.getRingerMode()) {
-            case AudioManager.RINGER_MODE_SILENT:
-                 // do nothing
-                 break;
-            case AudioManager.RINGER_MODE_VIBRATE:
-                 if (mVibrator != null && mVibrator.hasVibrator()) {
-                     mVibrator.vibrate(VibrationEffect.createOneShot(50,
-                            VibrationEffect.DEFAULT_AMPLITUDE));
-                 }
-                 break;
-            case AudioManager.RINGER_MODE_NORMAL:
-                 if (Settings.System.getIntForUser(mContext.getContentResolver(),
-                     Settings.System.SCREENSHOT_SHUTTER_SOUND, 1, UserHandle.USER_CURRENT) == 1) {
-                     // Play the shutter sound to notify that we've taken a screenshot
-                     playCameraSound();
-                 }
-                 break;
-        }
+        // Play the shutter sound to notify that we've taken a screenshot
+        playShutterSound();
 
         saveScreenshotInWorkerThread(
                 owner,
@@ -969,24 +953,8 @@ public class ScreenshotController {
         mScreenshotAnimation =
                 mScreenshotView.createScreenshotDropInAnimation(screenRect, showFlash);
 
-        switch (mAudioManager.getRingerMode()) {
-            case AudioManager.RINGER_MODE_SILENT:
-                 // do nothing
-                 break;
-            case AudioManager.RINGER_MODE_VIBRATE:
-                 if (mVibrator != null && mVibrator.hasVibrator()) {
-                     mVibrator.vibrate(VibrationEffect.createOneShot(50,
-                            VibrationEffect.DEFAULT_AMPLITUDE));
-                 }
-                 break;
-            case AudioManager.RINGER_MODE_NORMAL:
-                 if (Settings.System.getIntForUser(mContext.getContentResolver(),
-                     Settings.System.SCREENSHOT_SHUTTER_SOUND, 1, UserHandle.USER_CURRENT) == 1) {
-                     // Play the shutter sound to notify that we've taken a screenshot
-                     playCameraSound();
-                 }
-                 break;
-        }
+        // Play the shutter sound to notify that we've taken a screenshot
+        playShutterSound();
 
         if (DEBUG_ANIM) {
             Log.d(TAG, "starting post-screenshot animation");
@@ -1249,6 +1217,27 @@ public class ScreenshotController {
                 public void onFinish() {
                 }
             };
+        }
+    }
+
+    private void playShutterSound() {
+        switch (mAudioManager.getRingerMode()) {
+            case AudioManager.RINGER_MODE_SILENT:
+                // do nothing
+                break;
+            case AudioManager.RINGER_MODE_VIBRATE:
+                if (mVibrator != null && mVibrator.hasVibrator()) {
+                    mVibrator.vibrate(VibrationEffect.createOneShot(50,
+                            VibrationEffect.DEFAULT_AMPLITUDE));
+                }
+                break;
+            case AudioManager.RINGER_MODE_NORMAL:
+                if (Settings.System.getIntForUser(mContext.getContentResolver(),
+                    Settings.System.SCREENSHOT_SHUTTER_SOUND, 1, UserHandle.USER_CURRENT) == 1) {
+                    // Play the shutter sound to notify that we've taken a screenshot
+                    playCameraSound();
+                }
+                break;
         }
     }
 }
