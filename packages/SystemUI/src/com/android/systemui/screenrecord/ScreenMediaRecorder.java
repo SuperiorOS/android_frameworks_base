70bf0896bbf3c30351270661cb495c078dc03363
Kuba Wojciechowski <nullbytepl@gmail.com>
2021-02-06 21:54:25 +0300

    SystemUI: allow to limit the max framerate of built-in screen recorder
    Some devices seem to have problems when running the screen recorder at
    native resolution @ max fps (I'm looking at you surya). The symptoms may
    include severe lagging in the recording or stuff like crashes when
    stopping in a landscape orientation. Allow limiting the recorder fps to
    fix these issues.
    
    Test: record screen on surya with config_screenRecorderMaxFramerate==90
    while screen is running @ 120Hz, observe a smooth video. Stop the
    recording while in full screen landscape youtube video, observe no crash
    
    Signed-off-by: Kuba Wojciechowski <nullbytepl@gmail.com>
    Change-Id: I361c7ae4bf74f2dd67b86e960f8d2d6ef63f5b8f
    Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>

--- packages/SystemUI/src/com/android/systemui/screenrecord/ScreenMediaRecorder.java
+++ packages/SystemUI/src/com/android/systemui/screenrecord/ScreenMediaRecorder.java
@@ -53,6 +53,8 @@ import android.view.Surface;
 import android.view.WindowManager;
 
 import com.android.systemui.media.MediaProjectionCaptureTarget;
+import com.android.systemui.R;
+
 import java.io.File;
 import java.io.Closeable;
 import java.io.IOException;
@@ -89,6 +91,7 @@ public class ScreenMediaRecorder extends MediaProjection.Callback {
     private ScreenRecordingAudioSource mAudioSource;
     private final MediaProjectionCaptureTarget mCaptureRegion;
     private final Handler mHandler;
+    private int mMaxRefreshRate;
 
     private Context mContext;
     ScreenMediaRecorderListener mListener;
@@ -103,6 +106,8 @@ public class ScreenMediaRecorder extends MediaProjection.Callback {
         mCaptureRegion = captureRegion;
         mListener = listener;
         mAudioSource = audioSource;
+        mMaxRefreshRate = mContext.getResources().getInteger(
+                R.integer.config_screenRecorderMaxFramerate);
     }
 
     private void prepare() throws IOException, RemoteException, RuntimeException {
@@ -141,6 +146,7 @@ public class ScreenMediaRecorder extends MediaProjection.Callback {
         WindowManager wm = (WindowManager) mContext.getSystemService(Context.WINDOW_SERVICE);
         wm.getDefaultDisplay().getRealMetrics(metrics);
         int refreshRate = (int) wm.getDefaultDisplay().getRefreshRate();
+        if (mMaxRefreshRate != 0 && refreshRate > mMaxRefreshRate) refreshRate = mMaxRefreshRate;
         int[] dimens = getSupportedSize(metrics.widthPixels, metrics.heightPixels, refreshRate);
         int width = dimens[0];
         int height = dimens[1];
