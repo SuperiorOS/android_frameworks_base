98be24be52af1c3d037ff85d93c57e902c1247c5
Sipun Ku Mahanta <sipunkumar85@gmail.com>
2021-02-28 16:50:28 +0100

    SystemUI: Adapat more ls-clocks to our layout
    This fixes blanks- https://imgur.com/a/ANRs7fS
    
    - SamsungHighlight
    - Fluid
    
    Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>
    Change-Id: I5ba878375a9c5c7409521bb0c76730e519d7d730

--- packages/SystemUI/src/com/android/keyguard/clock/SamsungHighlightClockController.java
+++ packages/SystemUI/src/com/android/keyguard/clock/SamsungHighlightClockController.java
@@ -34,9 +34,6 @@ import com.android.systemui.plugins.ClockPlugin;
 
 import java.util.TimeZone;
 
-import static com.android.systemui.statusbar.phone
-        .KeyguardClockPositionAlgorithm.CLOCK_USE_DEFAULT_Y;
-
 /**
  * Plugin for the default clock face used only to provide a preview.
  */
@@ -65,7 +62,7 @@ public class SamsungHighlightClockController implements ClockPlugin {
     /**
      * Root view of clock.
      */
-    private ClockLayout mView;
+    private ClockLayout mBigClockView;
 
     /**
      * Text clock in preview view hierarchy.
@@ -92,9 +89,9 @@ public class SamsungHighlightClockController implements ClockPlugin {
     }
 
     private void createViews() {
-        mView = (ClockLayout) mLayoutInflater
+        mBigClockView = (ClockLayout) mLayoutInflater
                 .inflate(R.layout.digital_clock_custom, null);
-        mClock = mView.findViewById(R.id.clock);
+        mClock = mBigClockView.findViewById(R.id.clock);
         ColorExtractor.GradientColors colors = mColorExtractor.getColors(
                 WallpaperManager.FLAG_LOCK);
         setColorPalette(colors.supportsDarkText(), colors.getColorPalette());
@@ -106,7 +103,7 @@ public class SamsungHighlightClockController implements ClockPlugin {
 
     @Override
     public void onDestroyView() {
-        mView = null;
+        mBigClockView = null;
         mClock = null;
     }
 
@@ -148,20 +145,20 @@ public class SamsungHighlightClockController implements ClockPlugin {
 
     @Override
     public View getView() {
-        if (mView == null) {
-            createViews();
-        }
-        return mView;
-    }
-
-    @Override
-    public View getBigClockView() {
         return null;
     }
 
     @Override
+    public View getBigClockView() {
+        if (mBigClockView  == null) {
+            createViews();
+        }
+        return mBigClockView;
+    }
+
+    @Override
     public int getPreferredY(int totalHeight) {
-        return CLOCK_USE_DEFAULT_Y;
+        return totalHeight / 2;
     }
 
     @Override
@@ -183,15 +180,15 @@ public class SamsungHighlightClockController implements ClockPlugin {
 
     @Override
     public void onTimeTick() {
-        if (mView != null && mClock != null) {
-            mView.onTimeChanged();
+        if (mBigClockView != null && mClock != null) {
+            mBigClockView.onTimeChanged();
             mClock.refreshTime();
         }
     }
 
     @Override
     public void setDarkAmount(float darkAmount) {
-        mView.setDarkAmount(darkAmount);
+        mBigClockView.setDarkAmount(darkAmount);
     }
 
     @Override
