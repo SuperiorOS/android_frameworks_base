bb2506369afd4f65f911197711019d41d06729d1
Sipun Ku Mahanta <sipunkumar85@gmail.com>
2021-03-21 14:58:44 +0100

    [SQUASH]Revert Face unlock stuffs [1/4]
    Revert "base: Swipe up on keyguard to use Face Unlock [1/2]"
    
    This reverts commit e938fa00097a46df5cb064d27ab2aef2df03cb2b.
    
    Revert "SystemUI: Face Unlock animation"
    
    This reverts commit 49a2ac65ea6cedf350dfcf3e3a010e8922904643.
    
    Revert "FaceService: save ourselves from NPE"
    
    This reverts commit f53f2085054d123ba395304ce2bcf4c922d4ab8b.
    
    Revert "base: do not use new lockscreen layout for bypass"
    
    This reverts commit fab03d48c9f88573f47111f2906a8b9f74ab5a9b.
    
    Revert "biometrics: Allow posting reset runnable for all clients"
    
    This reverts commit 190a7dc15954415349110f10d31983b2c4ad0c48.
    
    Revert "FaceService: Fix FaceUnlock after r21 merge."
    
    This reverts commit 5e552760f66fefd8dd4d7ce4d9807397d23a3afd.
    
    Revert "SystemUI: Allow listening for face only on pin/pass view"
    
    This reverts commit 66092fd3c93981b80e595a5f2a66d010e9a0462d.
    
    Revert "Base: fix facelock crash when lock screen is disabled"
    
    This reverts commit 4b4f8aaff0c535659c44a8acdbc98075da9bd40d.
    
    Revert "base: Guard some functions against face unlock"
    
    This reverts commit c266cec72ee6378ea0c34f11b59be0d8340b3c2c.
    
    Revert "fixup! fixup! lockscreen: Add support for showing unlock screen directly"
    
    This reverts commit f277c67dbe1a4192578a6bdf34f151450b8c920c.
    
    Revert "fixup! lockscreen: Add support for showing unlock screen directly"
    
    This reverts commit d73a990406232060306d4d62561546fff02126f1.
    
    Revert "FaceUnlock: drop lockouts completely"
    
    This reverts commit 23539873d353dd6c53088c926fb4ad12a77b9d6d.
    
    Revert "Revert "Persist face error message for permanent lockout""
    
    This reverts commit 27328269940b4c8ce76736996ab4788a2a282b60.
    
    Revert "base: Port face unlock feature [1/3]"
    
    This reverts commit 650e82ef24710c7b2bb88bc6f35c67073837f245.
    
    Change-Id: Iff67f08a85efb6cd602e14e8bb86291a8f783f1c
    Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardBypassController.kt
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardBypassController.kt
@@ -31,7 +31,6 @@ import java.io.FileDescriptor
 import java.io.PrintWriter
 import javax.inject.Inject
 import javax.inject.Singleton
-import android.util.Log
 
 @Singleton
 open class KeyguardBypassController : Dumpable {
@@ -61,8 +60,6 @@ open class KeyguardBypassController : Dumpable {
      * If face unlock dismisses the lock screen or keeps user on keyguard for the current user.
      */
     var bypassEnabled: Boolean = false
-
-    var bypassEnabledBiometric: Boolean = false
         get() = field && mKeyguardStateController.isFaceAuthEnabled
         private set
 
@@ -107,7 +104,7 @@ open class KeyguardBypassController : Dumpable {
                         com.android.internal.R.bool.config_faceAuthDismissesKeyguard)) 1 else 0
         tunerService.addTunable(object : TunerService.Tunable {
             override fun onTuningChanged(key: String?, newValue: String?) {
-                bypassEnabledBiometric = tunerService.getValue(key, dismissByDefault) != 0
+                bypassEnabled = tunerService.getValue(key, dismissByDefault) != 0
             }
         }, Settings.Secure.FACE_UNLOCK_DISMISSES_KEYGUARD)
         lockscreenUserManager.addUserChangedListener(
@@ -127,8 +124,8 @@ open class KeyguardBypassController : Dumpable {
         biometricSourceType: BiometricSourceType,
         isStrongBiometric: Boolean
     ): Boolean {
-        if (bypassEnabledBiometric) {
-            val can = biometricSourceType != BiometricSourceType.FACE || canBypass()
+        if (bypassEnabled) {
+            val can = canBypass()
             if (!can && (isPulseExpanding || qSExpanded)) {
                 pendingUnlock = PendingUnlock(biometricSourceType, isStrongBiometric)
             }
@@ -152,7 +149,7 @@ open class KeyguardBypassController : Dumpable {
      * If keyguard can be dismissed because of bypass.
      */
     fun canBypass(): Boolean {
-        if (bypassEnabledBiometric) {
+        if (bypassEnabled) {
             return when {
                 bouncerShowing -> true
                 statusBarStateController.state != StatusBarState.KEYGUARD -> false
@@ -168,7 +165,7 @@ open class KeyguardBypassController : Dumpable {
      * If shorter animations should be played when unlocking.
      */
     fun canPlaySubtleWindowAnimations(): Boolean {
-        if (bypassEnabledBiometric) {
+        if (bypassEnabled) {
             return when {
                 statusBarStateController.state != StatusBarState.KEYGUARD -> false
                 qSExpanded -> false
@@ -190,7 +187,7 @@ open class KeyguardBypassController : Dumpable {
         } else {
             pw.println("  mPendingUnlock: $pendingUnlock")
         }
-        pw.println("  bypassEnabledBiometric: $bypassEnabledBiometric")
+        pw.println("  bypassEnabled: $bypassEnabled")
         pw.println("  canBypass: ${canBypass()}")
         pw.println("  bouncerShowing: $bouncerShowing")
         pw.println("  isPulseExpanding: $isPulseExpanding")
