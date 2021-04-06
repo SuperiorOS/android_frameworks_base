e83595071aa94e55cfb6dfdcb3811ed10ac6d7b5
Danny Lin <danny@kdrag0n.dev>
2021-04-05 21:45:18 -0700

    EventLog: Disable event writing in production builds
    When opening and closing activities in Settings, a measurably
    significant amount of CPU time is spent writing to the EventLog, as
    reported by simpleperf:
    
    0.01%     /system/lib64/libandroid_runtime.so                                                                                                       android::EventLogHelper<(log_id)2, &(android::kEventLogEventClass)>::writeEventArray(_JNIEnv*, _jobject*, int, _jobjectArray*)
    
    We have no use for EventLog events in production builds, so disable
    event writing entirely on non-debuggable builds.
    
    To avoid having to check ro.debuggable in native code and pay the
    overhead of a JNI call for every event, perform checks on the Java side
    and make the native method implementations private for delegation.
    
    Test: simpleperf record -a; verify that EventLogHelper no longer
          appears in sample hits
    Change-Id: I3505c460f234d9a5038bd1f9d41ce911697ca76e
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: S I P U N <sipunkumar85@gmail.com>

--- core/java/android/util/EventLog.java
+++ core/java/android/util/EventLog.java
@@ -331,7 +331,19 @@ public class EventLog {
         }
     }
 
-    // We assume that the native methods deal with any concurrency issues.
+    /**
+     * Record an event log message.
+     * @param tag The event type tag code
+     * @param value A value to log
+     * @return The number of bytes written
+     */
+    public static int writeEvent(int tag, int value) {
+        if (!Build.IS_DEBUGGABLE) {
+            return 0;
+        }
+
+        return nativeWriteEvent(tag, value);
+    }
 
     /**
      * Record an event log message.
@@ -339,7 +351,13 @@ public class EventLog {
      * @param value A value to log
      * @return The number of bytes written
      */
-    public static native int writeEvent(int tag, int value);
+    public static int writeEvent(int tag, long value) {
+        if (!Build.IS_DEBUGGABLE) {
+            return 0;
+        }
+
+        return nativeWriteEvent(tag, value);
+    }
 
     /**
      * Record an event log message.
@@ -347,15 +365,13 @@ public class EventLog {
      * @param value A value to log
      * @return The number of bytes written
      */
-    public static native int writeEvent(int tag, long value);
+    public static int writeEvent(int tag, float value) {
+        if (!Build.IS_DEBUGGABLE) {
+            return 0;
+        }
 
-    /**
-     * Record an event log message.
-     * @param tag The event type tag code
-     * @param value A value to log
-     * @return The number of bytes written
-     */
-    public static native int writeEvent(int tag, float value);
+        return nativeWriteEvent(tag, value);
+    }
 
     /**
      * Record an event log message.
@@ -363,7 +379,13 @@ public class EventLog {
      * @param str A value to log
      * @return The number of bytes written
      */
-    public static native int writeEvent(int tag, String str);
+    public static int writeEvent(int tag, String str) {
+        if (!Build.IS_DEBUGGABLE) {
+            return 0;
+        }
+
+        return nativeWriteEvent(tag, str);
+    }
 
     /**
      * Record an event log message.
@@ -371,7 +393,13 @@ public class EventLog {
      * @param list A list of values to log
      * @return The number of bytes written
      */
-    public static native int writeEvent(int tag, Object... list);
+    public static int writeEvent(int tag, Object... list) {
+        if (!Build.IS_DEBUGGABLE) {
+            return 0;
+        }
+
+        return nativeWriteEvent(tag, list);
+    }
 
     /**
      * Read events from the log, filtered by type.
@@ -379,8 +407,14 @@ public class EventLog {
      * @param output container to add events into
      * @throws IOException if something goes wrong reading events
      */
-    public static native void readEvents(int[] tags, Collection<Event> output)
-            throws IOException;
+    public static void readEvents(int[] tags, Collection<Event> output)
+            throws IOException {
+        if (!Build.IS_DEBUGGABLE) {
+            return;
+        }
+
+        nativeReadEvents(tags, output);
+    }
 
     /**
      * Read events from the log, filtered by type, blocking until logs are about to be overwritten.
@@ -391,7 +425,27 @@ public class EventLog {
      * @hide
      */
     @SystemApi
-    public static native void readEventsOnWrapping(int[] tags, long timestamp,
+    public static void readEventsOnWrapping(int[] tags, long timestamp,
+            Collection<Event> output)
+            throws IOException {
+        if (!Build.IS_DEBUGGABLE) {
+            return;
+        }
+
+        nativeReadEventsOnWrapping(tags, timestamp, output);
+    }
+
+    // We assume that the native methods deal with any concurrency issues.
+
+    private static native int nativeWriteEvent(int tag, int value);
+    private static native int nativeWriteEvent(int tag, long value);
+    private static native int nativeWriteEvent(int tag, float value);
+    private static native int nativeWriteEvent(int tag, String str);
+    private static native int nativeWriteEvent(int tag, Object... list);
+
+    private static native void nativeReadEvents(int[] tags, Collection<Event> output)
+            throws IOException;
+    private static native void nativeReadEventsOnWrapping(int[] tags, long timestamp,
             Collection<Event> output)
             throws IOException;
 
