a763f7111857215a4b701fe6e5a592cd83c4438e
TikkiTikki <radstylemusic@gmail.com>
2021-04-18 19:37:06 -0300

    Pulse: Vertical mirror [1/2]
    Change-Id: I4251534e88a8a88c0af418116c30e86bcda405ab
    Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -11482,6 +11482,12 @@ public final class Settings {
         public static final String VISUALIZER_CENTER_MIRRORED = "visualizer_center_mirrored";
 
         /**
+         * Pulse vertical mirror
+         * @hide
+         */
+        public static final String PULSE_VERTICAL_MIRROR = "pulse_vertical_mirror";
+
+        /**
          * These entries are considered common between the personal and the managed profile,
          * since the managed profile doesn't get to change them.
          */
