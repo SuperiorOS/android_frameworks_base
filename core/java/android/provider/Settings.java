94615f15a52af9e58977f92ecf42a0898e8a753f
jhenrique09 <jhsv09@gmail.com>
2021-06-18 11:54:32 -0300

    [1/2] Allow changing face unlock method when locked
    Change-Id: I9a8e812bbe869dead94d631fa0a2f53f3bf67cc6

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -10497,6 +10497,13 @@ public final class Settings {
         public static final String DEVICE_STATE_ROTATION_LOCK =
                 "device_state_rotation_lock";
 
+
+        /**
+         * Face Unlock Method
+         * @hide
+         */
+        public static final String FACE_UNLOCK_METHOD = "face_unlock_method";
+
         /**
          * These entries are considered common between the personal and the managed profile,
          * since the managed profile doesn't get to change them.
