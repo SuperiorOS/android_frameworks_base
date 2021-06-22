9701c41bc2e54b9b385be341f0fdcfeb85556ac0
Pranav Vashi <neobuddy89@gmail.com>
2021-06-22 22:42:19 +0530

    DisplayModeDirector: Use default refresh rate as min refresh rate
    * We are already linking this in Settings. Might as well do it here.
    
    Change-Id: Ib8a29b5ec18986217910479705244a7f186c0a13
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: S I P U N <sipunkumar85@gmail.com>

--- services/core/java/com/android/server/display/mode/DisplayModeDirector.java
+++ services/core/java/com/android/server/display/mode/DisplayModeDirector.java
@@ -1010,7 +1010,7 @@ public class DisplayModeDirector {
             float highestRefreshRate = getMaxRefreshRateLocked(displayId);
 
             float minRefreshRate = Settings.System.getFloatForUser(cr,
-                    Settings.System.MIN_REFRESH_RATE, 0f, cr.getUserId());
+                    Settings.System.MIN_REFRESH_RATE, mDefaultRefreshRate, cr.getUserId());
             if (Float.isInfinite(minRefreshRate)) {
                 // Infinity means that we want the highest possible refresh rate
                 minRefreshRate = highestRefreshRate;
