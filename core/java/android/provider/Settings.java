2ed6064faacf0663f06d18889f6c29756af09d1b
Ramii Ahmed <ramy@ahmedramy.com>
2021-07-05 16:30:48 +0800

    Introduce high touch polling rate feature control
    Change-Id: I82c96d696e65d7c9cb345d37b071905a5d2f48de

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5685,6 +5685,14 @@ public final class Settings {
                 "high_touch_sensitivity_enable";
 
         /**
+         * Whether the HighTouchPollingRate is activated or not.
+         * 0 = off, 1 = on
+         * @hide
+         */
+        public static final String HIGH_TOUCH_POLLING_RATE_ENABLE =
+                "high_touch_polling_rate_enable";
+
+        /**
          * Double tap on lockscreen to sleep
          * @hide
          */
@@ -5901,6 +5909,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(DOUBLE_TAP_SLEEP_GESTURE);
             PRIVATE_SETTINGS.add(BACK_GESTURE_HAPTIC);
             PRIVATE_SETTINGS.add(HIGH_TOUCH_SENSITIVITY_ENABLE);
+            PRIVATE_SETTINGS.add(HIGH_TOUCH_POLLING_RATE_ENABLE);
         }
 
         /**
