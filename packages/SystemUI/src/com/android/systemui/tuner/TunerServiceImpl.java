ed0c2c4ab7f04275cd02b9ff197d37b5ab027194
Joe Maples <joe@frap129.org>
2021-10-19 19:23:01 +0000

    Tuner: Don't clear out prefs, it's not disabled
    Change-Id: I7bea54fa19c61f3c769c0bafd9ea359ad1bd3356
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Signed-off-by: Joe Maples <joe@frap129.org>
    Signed-off-by: S I P U N <sipunkumar85@gmail.com>

--- packages/SystemUI/src/com/android/systemui/tuner/TunerServiceImpl.java
+++ packages/SystemUI/src/com/android/systemui/tuner/TunerServiceImpl.java
@@ -154,15 +154,8 @@ public class TunerServiceImpl extends TunerService {
                         TextUtils.join(",", iconHideList), mCurrentUser);
             }
         }
-        if (oldVersion < 2) {
+        if (oldVersion < 5) {
             setTunerEnabled(false);
-        }
-        // 3 Removed because of a revert.
-        if (oldVersion < 4) {
-            // Delay this so that we can wait for everything to be registered first.
-            final int user = mCurrentUser;
-            mainHandler.postDelayed(
-                    () -> clearAllFromUser(user), 5000);
         }
         setValue(TUNER_VERSION, newVersion);
     }
@@ -359,6 +352,9 @@ public class TunerServiceImpl extends TunerService {
 
     private void reloadAll() {
         for (String key : mTunableLookup.keySet()) {
+            if (ArrayUtils.contains(RESET_EXCEPTION_LIST, key) || isCustomSetting(key)) {
+                continue;
+            }
             String value = getValue(key);
             for (Tunable tunable : mTunableLookup.get(key)) {
                 if (tunable != null) {
@@ -380,9 +376,6 @@ public class TunerServiceImpl extends TunerService {
 
         // A couple special cases.
         for (String key : mTunableLookup.keySet()) {
-            if (ArrayUtils.contains(RESET_EXCEPTION_LIST, key) || isCustomSetting(key)) {
-                continue;
-            }
             setValue(key, null);
         }
     }
