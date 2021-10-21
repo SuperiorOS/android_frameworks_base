905fcc2e22ad115607c8b191de576e6c589fcbca
SagarMakhar <sagarmakhar@gmail.com>
2021-10-21 11:10:09 +0000

    Use flow layout for advanced power menu aswell
    Change-Id: I87d9650f5297689620a08c6ea6b230efb2728d11
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Signed-off-by: S I P U N <sipunkumar85@gmail.com>

--- packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsPowerDialog.java
+++ packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsPowerDialog.java
@@ -29,6 +29,7 @@ import android.widget.ListAdapter;
 
 import com.android.systemui.statusbar.BlurUtils;
 import com.android.systemui.dump.DumpManager;
+import androidx.constraintlayout.helper.widget.Flow;
 
 /**
  * Creates a customized Dialog for displaying the Shut Down and Restart actions.
@@ -40,11 +41,15 @@ public class GlobalActionsPowerDialog {
      */
     public static Dialog create(@NonNull Context context, ListAdapter adapter) {
         ViewGroup listView = (ViewGroup) LayoutInflater.from(context).inflate(
-                com.android.systemui.res.R.layout.global_actions_power_dialog, null);
+                com.android.systemui.res.R.layout.global_actions_power_dialog_flow, null);
+
+        Flow flow = listView.findViewById(com.android.systemui.R.id.power_flow);
 
         for (int i = 0; i < adapter.getCount(); i++) {
             View action = adapter.getView(i, null, listView);
+            action.setId(View.generateViewId());
             listView.addView(action);
+            flow.addView(action);
         }
 
         Resources res = context.getResources();
@@ -61,7 +66,7 @@ public class GlobalActionsPowerDialog {
         window.setType(WindowManager.LayoutParams.TYPE_VOLUME_OVERLAY);
         window.setTitle(""); // prevent Talkback from speaking first item name twice
         window.setBackgroundDrawable(res.getDrawable(
-                com.android.systemui.res.R.drawable.control_background, context.getTheme()));
+                com.android.systemui.res.R.drawable.global_actions_lite_background, context.getTheme()));
         window.addFlags(WindowManager.LayoutParams.FLAG_ALT_FOCUSABLE_IM);
         window.addFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND);
         if (blurUtils.supportsBlursOnWindows()) {
