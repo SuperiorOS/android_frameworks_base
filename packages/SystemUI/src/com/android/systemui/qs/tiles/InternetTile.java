bfb2a5f20f76f4a908638de1f4f01fd17663f3a8
Danny Lin <danny@kdrag0n.dev>
2021-10-25 19:32:33 -0700

    SystemUI: Allow Wi-Fi/cell tiles to co-exist with provider model
    The dedicated Wi-Fi/cellular data QS tiles are partially working now
    that we've exposed them in the list of tile options, but they're very
    buggy and somewhat broken when the provider model (unified internet) is
    enabled.
    
    Allow the tiles to co-exist with InternetTile and the provider model
    (including both settings_provider_model and combined signal icons) by
    always including QS icons in Wi-Fi/data indicator callbacks, and add
    a new flag to indicate whether they should be shown in the internet
    tile.
    
    (cdesai, mkbestas: Port to 12L)
    
    Change-Id: I10af134b35dfabdb9275f1aca8ca8512e1db6d27
    Signed-off-by: aswin7469 <aswinas@pixysos.com>
    Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tiles/InternetTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/InternetTile.java
@@ -313,7 +313,7 @@ public class InternetTile extends QSTileImpl<SignalState> {
                 mWifiInfo.mSsid = indicators.description;
                 mWifiInfo.mIsTransient = indicators.isTransient;
                 mWifiInfo.mStatusLabel = indicators.statusLabel;
-                if (indicators.qsIcon != null) {
+                if (indicators.qsIcon != null|| !indicators.isDefault) {
                     mWifiInfo.mConnected = indicators.qsIcon.visible;
                     mWifiInfo.mWifiSignalIconId = indicators.qsIcon.icon;
                     mWifiInfo.mWifiSignalContentDescription = indicators.qsIcon.contentDescription;
@@ -323,7 +323,7 @@ public class InternetTile extends QSTileImpl<SignalState> {
                     mWifiInfo.mWifiSignalContentDescription = null;
                 }
             }
-            if (indicators.qsIcon != null) {
+            if (indicators.qsIcon != null|| !indicators.isDefault) {
                 refreshState(mWifiInfo);
             }
         }
@@ -333,7 +333,7 @@ public class InternetTile extends QSTileImpl<SignalState> {
             if (DEBUG) {
                 Log.d(TAG, "setMobileDataIndicators: " + indicators);
             }
-            if (indicators.qsIcon == null) {
+            if (indicators.qsIcon == null || !indicators.isDefault) {
                 // Not data sim, don't display.
                 return;
             }
