e88ab53c4959ee26a83f0cdce5dbe08af57ccfab
neobuddy89 <neobuddy89@gmail.com>
2021-11-11 12:25:27 +0530

    PixelPropsUtils: Invite streaming apps to the party
    * Some streaming apps (Prime Video in particular) provides better high quality (1.82 GB/hr vs 0.46 GB/hr)
      on devices with Pixel props.
    
    Change-Id: Ied79de03c308540b0adb3d99b01f4feb6f4d575c

--- core/java/com/android/internal/util/custom/PixelPropsUtils.java
+++ core/java/com/android/internal/util/custom/PixelPropsUtils.java
@@ -44,7 +44,11 @@ public class PixelPropsUtils {
     private static final String[] extraPackagesToChange = {
         "com.android.chrome",
         "com.android.vending",
-        "com.breel.wallpapers20"
+        "com.breel.wallpapers20",
+        "com.amazon.avod.thirdpartyclient",
+        "com.disney.disneyplus",
+        "com.netflix.mediaclient",
+        "in.startv.hotstar"
     };
 
     private static final String[] packagesToChangeCOD = {
