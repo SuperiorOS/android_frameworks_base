c591c6899a8f0bfe8b7f83025ccdd88cdcb50d1a
acras01 <acras1@gmail.com>
2021-11-12 08:14:16 +0200

    SystemUI: Add a toggle for combined signal icons in status bar (1/2)
    * @ jhonboy121: Use the new feature flag APIs and the flag state as a default value, otherwise there is no point in having a "toggle" if the feature flag is not enabled
    
    Change-Id: If869fe31e43f198c4ced8e872c5c060a524f6462

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -10930,6 +10930,11 @@ public final class Settings {
         public static final String WINDOW_IGNORE_SECURE = "window_ignore_secure";
 
         /**
+         * @hide
+         */
+        public static final String SHOW_COMBINED_STATUS_BAR_SIGNAL_ICONS = "show_combined_status_bar_signal_icons";
+
+        /**
          * These entries are considered common between the personal and the managed profile,
          * since the managed profile doesn't get to change them.
          */
