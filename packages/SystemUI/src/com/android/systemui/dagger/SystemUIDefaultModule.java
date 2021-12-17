7f6e7092545c3ce4531b6733322a7305c583b4a2
Jayant-Deshmukh <jayantdeshmuk008@gmail.com>
2021-12-17 11:50:38 +0000

    Revert "SystemUI: Implement Smart space"
    This reverts commit 7ed5f14acaba8841909e2b0a608101a5fc12c82e.
    
    Change-Id: I647e5d07c544f0157664083d85b002b4132efbf1

--- packages/SystemUI/src/com/android/systemui/dagger/SystemUIDefaultModule.java
+++ packages/SystemUI/src/com/android/systemui/dagger/SystemUIDefaultModule.java
@@ -97,6 +97,10 @@ public abstract class SystemUIDefaultModule {
     @Binds
     abstract EnhancedEstimates bindEnhancedEstimates(EnhancedEstimatesImpl enhancedEstimates);
 
+    @Binds
+    abstract NotificationLockscreenUserManager bindNotificationLockscreenUserManager(
+            NotificationLockscreenUserManagerImpl notificationLockscreenUserManager);
+
     @Provides
     @SysUISingleton
     static BatteryController provideBatteryController(
