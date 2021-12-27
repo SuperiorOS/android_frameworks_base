AOSP-Krypton/frameworks_base

[SQUASH]SystemUI: Bring back lockscreen media art [1/2]
commit 5adce36c57d0623cc95374498d4a93d931f23c01
Author: Ido Ben-Hur <idoybh2@gmail.com>
Date:   Sun Nov 15 15:07:57 2020 +0200

    SystemUI: Bring back lockscreen media art [1/2]

    * @jhonboy121: inject SystemSettings and use it.

    Change-Id: I1f8c883aa586773f97ca4eeb0a43c7516451a36b
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>

commit 0b73600b5c7466cad1278eebbc274d0db7a9f5fc
Author: ezio84 <brabus84@gmail.com>
Date:   Thu Nov 19 20:38:58 2020 +0100

    SystemUI: Hide lockscreen media art if nothing is playing

    it's annoying to get the artwork even if the notification gets
    swiped out and media is paused, because the media session is still alive

    Change-Id: I3540e1429524d34029d273c5cbe216fa46e4d59e
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>

commit acb305769b7b926a8540ec9e8859d82b3bac4b4b (HEAD -> A12)
Author: jhonboy121 <alfredmathew05@gmail.com>
Date:   Mon Aug 2 01:32:43 2021 +0530

    SystemUI: allow adjusting keyguard media art blur[1/2]

    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>

Co-authored-by: jhonboy121 <alfredmathew05@gmail.com>
Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>

core/java/android/provider/Settings.java
@@ -5533,6 +5533,24 @@ public final class Settings {
         public static final String THREE_FINGER_GESTURE = "three_finger_gesture";
 
         /**
+         * Whether to show media album art on keyguard
+         * @hide
+         */
+        public static final String KEYGUARD_MEDIA_ART = "keyguard_media_art";
+
+        /**
+         * Whether to enable blur for media album art on keyguard
+         * @hide
+         */
+        public static final String KEYGUARD_MEDIA_ART_ENABLE_BLUR = "KEYGUARD_MEDIA_ART_enable_blur";
+
+        /**
+         * Adjust blur radius for media album art on keyguard
+         * @hide
+         */
+        public static final String KEYGUARD_MEDIA_ART_BLUR_RADIUS = "KEYGUARD_MEDIA_ART_blur_radius";
+
+        /**
          * IMPORTANT: If you add a new public settings you also have to add it to
          * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
          * it to PRIVATE_SETTINGS below. Also add a validator that can validate

SuperiorOS/android_frameworks_base

Change-Id: I2fac5702ca8c68d681b226f80307423cd4433737

core/java/android/provider/Settings.java
@@ -5593,6 +5593,24 @@ public final class Settings {
         public static final String STATUS_BAR_BRIGHTNESS_CONTROL = "status_bar_brightness_control";
 
         /**
+         * Whether to show media album art on keyguard
+         * @hide
+         */
+        public static final String KEYGUARD_MEDIA_ART = "keyguard_media_art";
+
+        /**
+         * Whether to enable blur for media album art on keyguard
+         * @hide
+         */
+        public static final String KEYGUARD_MEDIA_ART_ENABLE_BLUR = "keyguard_media_art_enable_blur";
+
+        /**
+         * Adjust blur radius for media album art on keyguard
+         * @hide
+         */
+        public static final String KEYGUARD_MEDIA_ART_BLUR_RADIUS = "keyguard_media_art_blur_radius";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *