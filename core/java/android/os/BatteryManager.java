6288ea9d7bb7110a1d510a5ecd21daad6d6fb7d1
Jayant-Deshmukh <jayantdeshmuk008@gmail.com>
2022-01-22 13:54:29 +0530

    Revert "frameworks: Reset battery stats [1/2]"
    This reverts commit 7ccadbdd6d871508d9c0fb3946154c1c8ef9f5ab.
    
    Change-Id: I4316481c8df817c5a91f6c80b73c517e1be495d4

--- core/java/android/os/BatteryManager.java
+++ core/java/android/os/BatteryManager.java
@@ -419,12 +419,4 @@ public class BatteryManager {
             throw e.rethrowFromSystemServer();
         }
     }
-
-    public void resetStatistics() {
-        try {
-            mBatteryStats.resetStatistics();
-        } catch (RemoteException e) {
-            throw e.rethrowFromSystemServer();
-        }
-    }
 }
