ff4e8da11496249f375d52584ac67b6ba72efd24
maitreya29 <maitreyapatni30@gmail.com>
2022-02-09 10:26:45 +0530

    Screenrecord: Allow to reduce 3 second screen record timer
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/screenrecord/ScreenRecordDialog.java
+++ packages/SystemUI/src/com/android/systemui/screenrecord/ScreenRecordDialog.java
@@ -50,6 +50,7 @@ public class ScreenRecordDialog extends SystemUIDialog {
     private static final List<ScreenRecordingAudioSource> MODES = Arrays.asList(INTERNAL, MIC,
             MIC_AND_INTERNAL);
     private static final long DELAY_MS = 3000;
+    private static final long NO_DELAY = 100;
     private static final long INTERVAL_MS = 1000;
     private static final String PREFS = "screenrecord_";
     private static final String PREF_DOT = "show_dot";
@@ -66,6 +67,7 @@ public class ScreenRecordDialog extends SystemUIDialog {
     private Switch mLowQualitySwitch;
     private Switch mLongerSwitch;
     private Switch mAudioSwitch;
+    private Switch mSkipSwitch;
     private Spinner mOptions;
 
     public ScreenRecordDialog(Context context, RecordingController controller,
@@ -105,6 +107,7 @@ public class ScreenRecordDialog extends SystemUIDialog {
         });
 
         mAudioSwitch = findViewById(R.id.screenrecord_audio_switch);
+        mSkipSwitch = findViewById(R.id.screenrecord_skip_switch);
         mStopDotSwitch = findViewById(R.id.screenrecord_stopdot_switch);
         mLowQualitySwitch = findViewById(R.id.screenrecord_lowquality_switch);
         mLongerSwitch = findViewById(R.id.screenrecord_longer_timeout_switch);
@@ -127,6 +130,7 @@ public class ScreenRecordDialog extends SystemUIDialog {
         boolean showStopDot = mStopDotSwitch.isChecked();
         boolean lowQuality = mLowQualitySwitch.isChecked();
         boolean longerDuration = mLongerSwitch.isChecked();
+        boolean skipTime = mSkipSwitch.isChecked();
         ScreenRecordingAudioSource audioMode = mAudioSwitch.isChecked()
                 ? (ScreenRecordingAudioSource) mOptions.getSelectedItem()
                 : NONE;
@@ -141,7 +145,7 @@ public class ScreenRecordDialog extends SystemUIDialog {
                 RecordingService.REQUEST_CODE,
                 RecordingService.getStopIntent(userContext),
                 PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE);
-        mController.startCountdown(DELAY_MS, INTERVAL_MS, startIntent, stopIntent);
+        mController.startCountdown(skipTime ? NO_DELAY : DELAY_MS, INTERVAL_MS, startIntent, stopIntent);
     }
 
     private void savePrefs() {
