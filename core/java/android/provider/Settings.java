1d881d3883bf74b1e7dd17376f3b6260415884c8
travarilo <travarilo@gmail.com>
2022-02-25 20:32:59 +0100

    SystemUI: Toggle data usage view on QS footer [1/2]
    genkzsz edits:
     * Add toggle
    
    Signed-off-by: minarypenguin <alexfinhart@gmail.com>
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    Change-Id: Ie156214d2f64efcde147aaaf16d38f250e9a7fa7

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5717,6 +5717,12 @@ public final class Settings {
          */
 
         /**
+         * Whether to show data usage in qs footer
+         * @hide
+         */
+        public static final String QS_FOOTER_DATA_USAGE = "qs_footer_data_usage";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
