1207f7a7b090e4e6690a0945c0afac66da65257b
spezi77 <spezi7713@gmx.net>
2022-03-17 16:19:28 +0100

    SystemUI: Fix NPE in AmbientIndicationContainer.
    java.lang.NullPointerException: Attempt to invoke virtual method 'void android.widget.TextView.setText(java.lang.CharSequence)' on a null object reference

--- packages/SystemUI/src/com/android/systemui/ambientmusic/AmbientIndicationContainer.java
+++ packages/SystemUI/src/com/android/systemui/ambientmusic/AmbientIndicationContainer.java
@@ -121,6 +121,7 @@ public class AmbientIndicationContainer extends AutoReinflateContainer implement
     @Override
     public void onPrimaryMetadataOrStateChanged(MediaMetadata metadata,
             @PlaybackState.State int state) {
+        if (mText == null) return;
         CharSequence npTitle = mMediaManager.getNowPlayingTrack();
         boolean nowPlayingAvailable = npTitle != null;
 
