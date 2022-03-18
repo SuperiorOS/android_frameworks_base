15f6f64c00821c5c085641577b323d22073ac822
LibXZR <i@xzr.moe>
2022-03-18 10:12:37 +0800

    base: Add support for window ignore secure [1/2]
    Feature: yt/KSCOPE-23
    Change-Id: Ia9447ca17f6b24152b64526c25710a77e1388ce6
    Signed-off-by: LibXZR <i@xzr.moe>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -11113,6 +11113,13 @@ public final class Settings {
                 "extra_automatic_power_save_mode";
 
         /**
+         * Control whether FLAG_SECURE is ignored for all windows.
+         * @hide
+         */
+        @Readable
+        public static final String WINDOW_IGNORE_SECURE = "window_ignore_secure";
+
+        /**
          * These entries are considered common between the personal and the managed profile,
          * since the managed profile doesn't get to change them.
          */
