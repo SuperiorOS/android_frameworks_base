bee06cff441098a6f1c136f20f87f465cbf5199d
Jayant-Deshmukh <jayantdeshmuk008@gmail.com>
2022-04-07 17:56:42 +0530

    [SQUASH]: Drop Screenshot/Partial QS Tile
    Revert "ScreenshotTile: Improve code"
    
    This reverts commit 2dea5f568a31f846cd1589bc2c91ea346f635e2a.
    
    Revert "ScreenshotTile: Make default state inactive"
    
    This reverts commit 7ff4309f545e22f366d54f5d5fa5271eb4af0ad6.
    
    Revert "ScreenshotTile: Use secondary label to indicate mode"
    
    This reverts commit 77ea04438f9ab6d252ae95fc86ffe48c2921cf89.
    
    Revert "base: Add partial/full screenshot QS tile"
    
    This reverts commit 24484c4ef813c779d690fb93baa18f010fbebb55.

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -51,7 +51,6 @@ import com.android.systemui.qs.tiles.ScreenRecordTile;
 import com.android.systemui.qs.tiles.SyncTile;
 import com.android.systemui.qs.tiles.UiModeNightTile;
 import com.android.systemui.qs.tiles.SoundTile;
-import com.android.systemui.qs.tiles.ScreenshotTile;
 import com.android.systemui.qs.tiles.UserTile;
 import com.android.systemui.qs.tiles.WifiTile;
 import com.android.systemui.qs.tiles.WorkModeTile;
@@ -103,7 +102,6 @@ public class QSFactoryImpl implements QSFactory {
     private final Provider<AmbientDisplayTile> mAmbientDisplayTileProvider;
     private final Provider<UsbTetherTile> mUsbTetherTileProvider;
     private final Provider<HeadsUpTile> mHeadsUpTileProvider;
-    private final Provider<ScreenshotTile> mScreenshotTileProvider;
 
     private final Lazy<QSHost> mQsHostLazy;
     private final Provider<CustomTile.Builder> mCustomTileBuilderProvider;
@@ -144,8 +142,7 @@ public class QSFactoryImpl implements QSFactory {
             Provider<SoundTile> soundTileProvider,
             Provider<AmbientDisplayTile> ambientDisplayTileProvider,
             Provider<UsbTetherTile> usbTetherTileProvider,
-            Provider<HeadsUpTile> headsUpTileProvider,
-            Provider<ScreenshotTile> screenshotTileProvider) {
+            Provider<HeadsUpTile> headsUpTileProvider) {
         mQsHostLazy = qsHostLazy;
         mCustomTileBuilderProvider = customTileBuilderProvider;
 
@@ -182,7 +179,6 @@ public class QSFactoryImpl implements QSFactory {
         mAmbientDisplayTileProvider = ambientDisplayTileProvider;
         mUsbTetherTileProvider = usbTetherTileProvider;
         mHeadsUpTileProvider = headsUpTileProvider;
-        mScreenshotTileProvider = screenshotTileProvider;
     }
 
     public QSTile createTile(String tileSpec) {
@@ -261,8 +257,6 @@ public class QSFactoryImpl implements QSFactory {
                 return mUsbTetherTileProvider.get();
             case "heads_up":
                 return mHeadsUpTileProvider.get();
-            case "screenshot":
-                return mScreenshotTileProvider.get();
         }
 
         // Custom tiles
