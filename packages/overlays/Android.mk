aadf449b23ef84d04d1de48bfb5258e240c926bd
Josh Fox <joshfox87@gmail.com>
2022-07-25 07:50:06 +0800

    overlays: Add notch bar killer
    Signed-off-by: minaripenguin <alexfinhart@gmail.com>
    Signed-off-by: S I P U N <sipunkumar85@gmail.com>

--- packages/overlays/Android.mk
+++ packages/overlays/Android.mk
@@ -32,6 +32,7 @@ LOCAL_REQUIRED_MODULES := \
 	NavigationBarModeGesturalOverlayExtraWideBack \
 	TransparentNavigationBarOverlay \
 	NotesRoleEnabledOverlay \
+        NotchBarKillerOverlay \
 	preinstalled-packages-platform-overlays.xml
 
 include $(BUILD_PHONY_PACKAGE)
