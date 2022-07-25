7a6fce08f52bff91b32f9c318cd38c78f1507899
Jaida Wu <i@neko.ink>
2022-07-25 16:10:07 +0800

    overlays: Add notchbar killer for left-notch devices
    Signed-off-by: minaripenguin <alexfinhart@gmail.com>
    Signed-off-by: S I P U N <sipunkumar85@gmail.com>

--- packages/overlays/Android.mk
+++ packages/overlays/Android.mk
@@ -38,6 +38,7 @@ LOCAL_REQUIRED_MODULES := \
 	TransparentNavigationBarOverlay \
 	NotesRoleEnabledOverlay \
         NotchBarKillerOverlay \
+	NotchBarKillerLeftrOverlay \
 	preinstalled-packages-platform-overlays.xml
 
 include $(BUILD_PHONY_PACKAGE)
