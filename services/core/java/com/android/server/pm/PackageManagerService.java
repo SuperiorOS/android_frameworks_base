89df71564b0dc585ed888fcd3a03882227df9fd1
Pranav Vashi <neobuddy89@gmail.com>
2022-08-23 05:45:06 +0000

    Restore cache clean up code in PackageManagerService
    AnierinBliss Forward port to Android 13
    
    Change-Id: Iee330e790c8a8e368791571377972a28ec16c0bb
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: AnierinB <anierin@evolution-x.org>
    Signed-off-by: S I P U N <sipunkumar85@gmail.com>

--- services/core/java/com/android/server/pm/PackageManagerService.java
+++ services/core/java/com/android/server/pm/PackageManagerService.java
@@ -2305,7 +2305,7 @@ public class PackageManagerService implements PackageSender, TestUtilityService 
             }
 
             mCacheDir = PackageManagerServiceUtils.preparePackageParserCache(
-                    mIsEngBuild, mIsUserDebugBuild, mIncrementalVersion);
+                    mIsEngBuild, mIsUserDebugBuild, mIncrementalVersion, mIsUpgrade);
 
             mInitialNonStoppedSystemPackages = mInjector.getSystemConfig()
                     .getInitialNonStoppedSystemPackages();
