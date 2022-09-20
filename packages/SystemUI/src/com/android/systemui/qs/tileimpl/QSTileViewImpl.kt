66094857ea227c0a05fc8a61d1b045a5bee9c4b2
str-br <alexfinhart@gmail.com>
2022-09-21 06:42:15 +0800

    [SQUASH]SystemUI: Improve QS styles
    Signed-off-by: minaripenguin37 <alexfinhart@gmail.com>
    Change-Id: I521244b0ab466f3b07c134a30f819d6d9681307b
    Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSTileViewImpl.kt
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSTileViewImpl.kt
@@ -633,7 +633,6 @@ open class QSTileViewImpl @JvmOverloads constructor(
             }
         }
     }
-
     private fun getLabelColorForState(state: Int, disabledByPolicy: Boolean = false): Int {
         return when {
             state == Tile.STATE_UNAVAILABLE || disabledByPolicy -> colorLabelUnavailable
