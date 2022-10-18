6e432f09e76d9895bea6dfa85c89cebc026e19ce
minaripenguin37 <alexfinhart@gmail.com>
2022-10-18 12:04:02 +0000

    SystemUI: Add extended monet themes
    Change-Id: Ia5830b38a2d882abab9df87d6c1e6e674eeb0c51
    Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>

--- packages/SystemUI/src/com/android/systemui/theme/ThemeOverlayApplier.java
+++ packages/SystemUI/src/com/android/systemui/theme/ThemeOverlayApplier.java
@@ -88,7 +88,11 @@ public class ThemeOverlayApplier implements Dumpable {
 
     static final String CUSTOM_THEME_BLACK = "com.android.system.theme.black";
 
-    static final String CUSTOM_THEME_VIVID = "com.android.system.theme.vivid";
+    static final String CUSTOM_THEME_VIVID = "com.android.system.monet.vivid";
+
+    static final String CUSTOM_THEME_SNOWPAINT = "com.android.system.monet.snowpaintdrop";
+
+    static final String CUSTOM_THEME_ESPRESSO = "com.android.system.monet.expresso";
 
     @VisibleForTesting
     static final String OVERLAY_CATEGORY_FONT = "android.theme.customization.font";
@@ -154,7 +158,9 @@ public class ThemeOverlayApplier implements Dumpable {
     static final List<String> CUSTOM_THEME_CATEGORIES = Lists.newArrayList(
             "",
             CUSTOM_THEME_BLACK,
-            CUSTOM_THEME_VIVID);
+            CUSTOM_THEME_VIVID,
+            CUSTOM_THEME_SNOWPAINT,
+            CUSTOM_THEME_ESPRESSO);
 
     /* Allowed overlay categories for each target package. */
     private final Map<String, Set<String>> mTargetPackageToCategories = new ArrayMap<>();
