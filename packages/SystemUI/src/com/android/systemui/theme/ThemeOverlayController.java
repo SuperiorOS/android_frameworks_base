516e7d0087d54c46bfe04578dad5e7243c829e77
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-11-21 18:30:02 +0530

    SystemUI: Reload theme only if black theme is enabled
    Currently it force reloads theme on UI mode change always
    for black theme to set correctly, But this has the side
    effect of janky transition between dark<->light mode and
    other glitches in QS. Perform this only if we have black
    theme actually enabled.
    
    Change-Id: I372bdb51dacc54305d47cd42a5b165ac35609bd8
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- packages/SystemUI/src/com/android/systemui/theme/ThemeOverlayController.java
+++ packages/SystemUI/src/com/android/systemui/theme/ThemeOverlayController.java
@@ -855,8 +855,7 @@ public class ThemeOverlayController implements CoreStartable, Dumpable {
         FabricatedOverlay[] fOverlays = null;
         boolean nightMode = (mContext.getResources().getConfiguration().uiMode
                 & Configuration.UI_MODE_NIGHT_MASK) == Configuration.UI_MODE_NIGHT_YES;
-        boolean isBlackTheme = mSecureSettings.getInt(Settings.Secure.SYSTEM_BLACK_THEME, 0) == 1
-                                && nightMode;
+        boolean isBlackTheme = isBlackThemeEnabled() && nightMode;
 
         mThemeManager.setIsBlackTheme(isBlackTheme);
 
@@ -901,10 +900,16 @@ public class ThemeOverlayController implements CoreStartable, Dumpable {
         return style;
     }
 
+    private boolean isBlackThemeEnabled() {
+        return mSecureSettings.getInt(Settings.Secure.SYSTEM_BLACK_THEME, 0) == 1;
+    }
+
     private final ConfigurationListener mConfigurationListener = new ConfigurationListener() {
         @Override
         public void onUiModeChanged() {
-            reevaluateSystemTheme(true /* forceReload */);
+            if (isBlackThemeEnabled()) {
+                reevaluateSystemTheme(true /* forceReload */);
+            }
         }
     };
 
