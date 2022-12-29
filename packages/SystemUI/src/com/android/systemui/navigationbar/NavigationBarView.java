ce6a12f108adc420c3d8e91d2dd0ddcd8e1eefd9
Pranav Vashi <neobuddy89@gmail.com>
2022-12-29 19:40:30 +0530

    SystemUI: Clean up redundancy in BurnInProtection
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>

--- packages/SystemUI/src/com/android/systemui/navigationbar/NavigationBarView.java
+++ packages/SystemUI/src/com/android/systemui/navigationbar/NavigationBarView.java
@@ -163,13 +163,6 @@ public class NavigationBarView extends FrameLayout implements
 
     private boolean mHomeHandleForceHidden;
 
-    private int mBasePaddingBottom;
-    private int mBasePaddingLeft;
-    private int mBasePaddingRight;
-    private int mBasePaddingTop;
-
-    private ViewGroup mNavigationBarContents;
-
     /**
      * Helper that is responsible for showing the right toast when a disallowed activity operation
      * occurred. In pinned mode, we show instructions on how to break out of this mode, whilst in
@@ -890,14 +883,16 @@ public class NavigationBarView extends FrameLayout implements
     }
 
     public void shiftNavigationBarItems(int horizontalShift, int verticalShift) {
-        if (mNavigationBarContents == null) {
+        ViewGroup navButtons = (ViewGroup) findViewById(R.id.nav_buttons);
+
+        if (navButtons == null) {
             return;
         }
 
-        mNavigationBarContents.setPaddingRelative(mBasePaddingLeft + horizontalShift,
-                mBasePaddingTop + verticalShift,
-                mBasePaddingRight + horizontalShift,
-                mBasePaddingBottom - verticalShift);
+        navButtons.setPaddingRelative(navButtons.getPaddingStart() + horizontalShift,
+                navButtons.getPaddingTop() + verticalShift,
+                navButtons.getPaddingEnd() + horizontalShift,
+                navButtons.getPaddingBottom() - verticalShift);
         invalidate();
     }
 
@@ -907,13 +902,6 @@ public class NavigationBarView extends FrameLayout implements
         mNavigationInflaterView = findViewById(R.id.navigation_inflater);
         mNavigationInflaterView.setButtonDispatchers(mButtonDispatchers);
 
-
-        mNavigationBarContents = (ViewGroup) findViewById(R.id.nav_buttons);
-
-        mBasePaddingLeft = mNavigationBarContents.getPaddingStart();
-        mBasePaddingTop = mNavigationBarContents.getPaddingTop();
-        mBasePaddingRight = mNavigationBarContents.getPaddingEnd();
-        mBasePaddingBottom = mNavigationBarContents.getPaddingBottom();
         updateOrientationViews();
         reloadNavIcons();
     }
