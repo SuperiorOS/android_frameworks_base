18cc9b96966379ec36b29a1273edfebbfb9947a8
ShevT <evgeny.shishkov@gmail.com>
2023-01-12 14:24:12 +0300

    Make overlay for 'Update battery info every second when device is charging'
     It is not necessary to use this for all devices, but only for those
     that have a problem with charging display (for example OnePlus 7 series)
    
    Change-Id: I98441087ec450ec3a2699a1a79b84bc32a1fe340
    Signed-off-by: Dmitrii <bankersenator@gmail.com>
    Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/KeyguardIndicationController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/KeyguardIndicationController.java
@@ -201,6 +201,7 @@ public class KeyguardIndicationController {
     private boolean mFaceDetectionRunning;
 
     private IBatteryPropertiesRegistrar mBatteryPropertiesRegistrar;
+    private boolean mAlternateFastchargeInfoUpdate;
 
     private KeyguardUpdateMonitorCallback mUpdateMonitorCallback;
 
@@ -327,6 +328,8 @@ public class KeyguardIndicationController {
         mBatteryPropertiesRegistrar =
                     IBatteryPropertiesRegistrar.Stub.asInterface(
                     ServiceManager.getService("batteryproperties"));
+        mAlternateFastchargeInfoUpdate =
+                    mContext.getResources().getBoolean(R.bool.config_alternateFastchargeInfoUpdate);
     }
 
     public void setIndicationArea(ViewGroup indicationArea) {
@@ -1174,7 +1177,7 @@ public class KeyguardIndicationController {
                 mKeyguardLogger.logException(e, "Error calling IBatteryStats");
                 mChargingTimeRemaining = -1;
             }
-            if (wasPluggedIn != mPowerPluggedIn) {
+            if ((wasPluggedIn != mPowerPluggedIn) && mAlternateFastchargeInfoUpdate) {
                 if (mPowerPluggedIn) {
                     mUpdateInfo.run();
                 } else {
