096d0d6d81d4ab05fb3b014cf374176eb92f2665
Edward Savage-Jones <edward.savage-jones@sony.com>
2023-01-19 12:08:55 +0100

    Add support for swapping recent and back button positions
    This adds support for swapping the recent and back
    button positions.
    
    Bug: 256952761
    Test: manual
    Co-Author: Keisuke Sugita <Keisuke.Sugita@sony.com>
    Change-Id: I8b6347ec47c44b04e1a774e763854ab3db1171c4
    [mnri: remove config toggle]
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- packages/SystemUI/src/com/android/systemui/navigationbar/NavigationBar.java
+++ packages/SystemUI/src/com/android/systemui/navigationbar/NavigationBar.java
@@ -58,9 +58,11 @@ import android.annotation.NonNull;
 import android.app.ActivityTaskManager;
 import android.app.IActivityTaskManager;
 import android.app.StatusBarManager;
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.res.Configuration;
+import android.database.ContentObserver;
 import android.graphics.Insets;
 import android.graphics.PixelFormat;
 import android.graphics.Point;
@@ -71,8 +73,11 @@ import android.os.Binder;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.IBinder;
+import android.os.Looper;
+import android.os.UserHandle;
 import android.os.RemoteException;
 import android.provider.DeviceConfig;
+import android.provider.Settings;
 import android.telecom.TelecomManager;
 import android.text.TextUtils;
 import android.util.Log;
@@ -237,6 +242,9 @@ public class NavigationBar extends ViewController<NavigationBarView> implements 
 
     private Locale mLocale;
     private int mLayoutDirection;
+
+    private ContentObserver mNavBarOrderContentObserver;
+    private ContentResolver mContentResolver;
 
     private Optional<Long> mHomeButtonLongPressDurationMs;
 
@@ -728,6 +736,18 @@ public class NavigationBar extends ViewController<NavigationBarView> implements 
 
         mNotificationShadeDepthController.addListener(mDepthListener);
         mTaskStackChangeListeners.registerTaskStackListener(mTaskStackListener);
+
+        // Navbar back/recents swap
+        mContentResolver = mContext.getContentResolver();
+        mNavBarOrderContentObserver = new ContentObserver(new Handler(Looper.getMainLooper())) {
+            @Override
+            public void onChange(boolean selfChange) {
+                mView.updateStates();
+            }
+        };
+        mContentResolver.registerContentObserver(
+                Settings.Secure.getUriFor(Settings.Secure.NAV_BAR_BUTTON_SWAP_ENABLED),
+                true, mNavBarOrderContentObserver, UserHandle.USER_ALL);
     }
 
     public void destroyView() {
@@ -742,6 +762,7 @@ public class NavigationBar extends ViewController<NavigationBarView> implements 
 
         mDeviceConfigProxy.removeOnPropertiesChangedListener(mOnPropertiesChangedListener);
         mTaskStackChangeListeners.unregisterTaskStackListener(mTaskStackListener);
+        mContentResolver.unregisterContentObserver(mNavBarOrderContentObserver);
     }
 
     @Override
