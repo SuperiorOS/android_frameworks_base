319fdaa672298b1c951c5ca591f06d3a99fb8d17
minaripenguin <minaripenguin@users.noreply.github.com>
2023-04-03 07:58:51 +0800

    SystemUI: Improve QS notification expansion animation
    * attempt to reduce jitter/janks caused by unnecessary animations
    
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
+++ packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
@@ -2529,6 +2529,8 @@ public final class NotificationPanelViewController implements Dumpable {
             alpha *= mClockPositionResult.clockAlpha;
         }
         mNotificationStackScrollLayoutController.setAlpha(alpha);
+        float finalAlpha = alpha > 0.84f ? alpha : 0f;
+        mNotificationStackScrollLayoutController.setAlpha(finalAlpha);
     }
 
     private float getFadeoutAlpha() {
