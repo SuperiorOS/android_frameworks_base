4b3974281ab02077d68f0e6753b5c621b5ad17aa
Pranav Vashi <neobuddy89@gmail.com>
2023-04-30 02:15:50 +0530

    KeyguardStatusBarView: Clean up cruft
    * We do not need to set carrier text margin as it was set properly in
      https://github.com/crdroidandroid/android_frameworks_base/commit/50ed26d88d0280caf2b166edeb4ae8b5cb217deb
    * Carrier text follows TextAppearance.StatusBar.Clock style, hence remove setting font size in code
    * Remove unused rounded corner padding leftover
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardStatusBarView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardStatusBarView.java
@@ -94,7 +94,6 @@ public class KeyguardStatusBarView extends RelativeLayout {
     private int mCutoutSideNudge = 0;
 
     private DisplayCutout mDisplayCutout;
-    private int mRoundedCornerPadding = 0;
     // right and left padding applied to this view to account for cutouts and rounded corners
     private Pair<Integer, Integer> mPadding = new Pair(0, 0);
 
@@ -161,18 +160,6 @@ public class KeyguardStatusBarView extends RelativeLayout {
                 mStatusIconContainer.getPaddingBottom()
         );
 
-        // Respect font size setting.
-        mCarrierLabel.setTextSize(TypedValue.COMPLEX_UNIT_PX,
-                getResources().getDimensionPixelSize(
-                        com.android.internal.R.dimen.text_size_small_material));
-        lp = (MarginLayoutParams) mCarrierLabel.getLayoutParams();
-
-        int marginStart = calculateMargin(
-                getResources().getDimensionPixelSize(R.dimen.keyguard_carrier_text_margin),
-                mPadding.first);
-        lp.setMarginStart(marginStart);
-
-        mCarrierLabel.setLayoutParams(lp);
         updateKeyguardStatusBarHeight();
     }
 
@@ -198,8 +185,6 @@ public class KeyguardStatusBarView extends RelativeLayout {
                 R.dimen.display_cutout_margin_consumption);
         mShowPercentAvailable = getContext().getResources().getBoolean(
                 com.android.internal.R.bool.config_battery_percentage_setting_available);
-        mRoundedCornerPadding = res.getDimensionPixelSize(
-                R.dimen.rounded_corner_content_padding);
     }
 
     private void updateVisibilities() {
