56643b88d5c1eb4ad2668ff440cded5f40845cf0
Pranav Vashi <neobuddy89@gmail.com>
2023-06-13 23:05:59 +0530

    StatusBarSignalPolicy: Set initialized to false on destroy
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarSignalPolicy.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarSignalPolicy.java
@@ -130,6 +130,7 @@ public class StatusBarSignalPolicy implements SignalCallback,
         mTunerService.removeTunable(this);
         mNetworkController.removeCallback(this);
         mSecurityController.removeCallback(this);
+        mInitialized = false;
     }
 
     private void updateVpn() {
