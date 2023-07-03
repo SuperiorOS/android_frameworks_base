e421a9be726e238601556e7b05cf1cc56f96f1a5
minaripenguin <minaripenguin@users.noreply.github.com>
2023-07-03 14:45:48 +0800

    Implement extended feature support to misc apps
    Change-Id: I8643f0ec50b8074c037ba5e45095605c409ddee1
    
    core: Extend pixel experience features
    
    * features were taken from xmlpak module, credits and thanks to: RadekBledowski
    
    test: add pixel_experience_2022_midyear.xml to product/etc/sysconfig, check if spoofing is broken, disable/enable spoofing, unlimited photo toggle working as intended on both scenarios.
    
    Change-Id: I5c96969baa13e37c41971857d51fea0cc4054979
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    
    core: Integrate and Enable Pixel features capabilities
    
    Co-authored-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Change-Id: Ica76bf9b51aacf021306cc01506461ae02b3bffe
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    
    ApplicationPackageManager: Extend freeform window feature to all apps
    
    Change-Id: Iaa3e500fd6a80f4e1718218ff6d42fcb1eb79c9c
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    
    Co-authored-by: RadekBledowski <radekxmen.bledowski@gmail.com>
    Co-authored-by: Shivan Singh <shivan.0972@gmail.com>
    Change-Id: If48cc4bb8a6fe3ccc66845169d1b3235246a97eb
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- core/java/android/app/ApplicationPackageManager.java
+++ core/java/android/app/ApplicationPackageManager.java
@@ -810,8 +810,66 @@ public class ApplicationPackageManager extends PackageManager {
                 }
             };
 
+    private static final String[] featuresPixel = {
+            "com.google.android.apps.photos.PIXEL_2019_PRELOAD",
+            "com.google.android.apps.photos.PIXEL_2019_MIDYEAR_PRELOAD",
+            "com.google.android.apps.photos.PIXEL_2018_PRELOAD",
+            "com.google.android.apps.photos.PIXEL_2017_PRELOAD",
+            "com.google.android.feature.ASI",
+            "com.google.android.feature.ANDROID_ONE_EXPERIENCE",
+            "com.google.android.feature.GOOGLE_FI_BUNDLED",
+            "com.google.android.feature.LILY_EXPERIENCE",
+            "com.google.android.feature.TURBO_PRELOAD",
+            "com.google.android.feature.WELLBEING",
+            "com.google.android.feature.PIXEL_2024_EXPERIENCE",
+            "com.google.android.feature.PIXEL_2024_MIDYEAR_EXPERIENCE",
+            "com.google.android.feature.PIXEL_2023_EXPERIENCE",
+            "com.google.android.feature.PIXEL_2023_MIDYEAR_EXPERIENCE",
+            "com.google.android.feature.PIXEL_2022_EXPERIENCE",
+            "com.google.android.feature.PIXEL_2022_MIDYEAR_EXPERIENCE",
+            "com.google.android.feature.PIXEL_2021_EXPERIENCE",
+            "com.google.android.feature.PIXEL_2021_MIDYEAR_EXPERIENCE",
+            "com.google.android.feature.PIXEL_2020_EXPERIENCE",
+            "com.google.android.feature.PIXEL_2020_MIDYEAR_EXPERIENCE",
+            "com.google.android.feature.PIXEL_2019_EXPERIENCE",
+            "com.google.android.feature.PIXEL_2019_MIDYEAR_EXPERIENCE",
+            "com.google.android.feature.PIXEL_2018_EXPERIENCE",
+            "com.google.android.feature.PIXEL_2017_EXPERIENCE",
+            "com.google.android.feature.PIXEL_EXPERIENCE",
+            "com.google.android.feature.GOOGLE_BUILD",
+            "com.google.android.feature.GOOGLE_EXPERIENCE",
+            "com.google.lens.feature.IMAGE_INTEGRATION",    
+            "com.google.lens.feature.CAMERA_INTEGRATION",
+            "com.google.photos.trust_debug_certs",
+            "com.google.android.feature.AER_OPTIMIZED",
+            "com.google.android.feature.NEXT_GENERATION_ASSISTANT",
+            "android.software.game_service",
+            "com.google.android.feature.GMS_GAME_SERVICE",
+            "com.google.android.feature.EXCHANGE_6_2",
+            "com.google.android.apps.dialer.call_recording_audio",
+            "com.google.android.apps.dialer.SUPPORTED"
+    };
+
+    private static final String[] featuresNexus = {
+            "com.google.android.apps.photos.NEXUS_PRELOAD",
+            "com.google.android.apps.photos.nexus_preload"
+    };
+    
+    private static final String[] featuresAndroid = {
+            "android.software.freeform_window_management"
+    };
+
     @Override
     public boolean hasSystemFeature(String name, int version) {
+        String packageName = ActivityThread.currentPackageName();
+        if (packageName != null &&
+                packageName.equals("com.google.android.apps.photos") &&
+                SystemProperties.getBoolean("persist.sys.pixelprops.gphotos", true)) {
+            if (Arrays.asList(featuresPixel).contains(name)) return false;
+            if (Arrays.asList(featuresNexus).contains(name)) return true;
+        }
+        if (Arrays.asList(featuresPixel).contains(name)) return true;
+        if (Arrays.asList(featuresAndroid).contains(name)) return true;
         return mHasSystemFeatureCache.query(new HasSystemFeatureQuery(name, version));
     }
 
