202afbf8c38fa983fee9486327490e8eb0b0cf39
maxwen <max.weninger@gmail.com>
2023-11-25 11:24:59 +0000

    base: Add Lockscreen Weather with OmniJaws
    Co-authored-by: maxwen <max.weninger@gmail.com>
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: I3c94fb5d002bcc942524b3aa1e78ddf0d0ac0991
    Signed-off-by: S I P U N <sipunkumar85@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6153,6 +6153,16 @@ public final class Settings {
         public static final String THREE_FINGER_GESTURE = "three_finger_gesture";
 
         /**
+         * @hide
+         */
+        public static final String LOCKSCREEN_WEATHER_ENABLED = "lockscreen_weather_enabled";
+
+        /**
+         * @hide
+         */
+        public static final String LOCKSCREEN_WEATHER_LOCATION = "lockscreen_weather_location";
+
+        /**
          * These are all public system settings
          *
          * @hide
