b37352c8f8fac384d4fa863be4eeaa606cb68547
someone5678 <someone5678@users.noreply.github.com>
2024-03-20 12:26:42 +0900

    SystemUI: Tuner: Move to SwitchPreferenceCompat
    Change-Id: I03b43e30fbe12c969aba79071443f638cb2b20dc
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: S I P U N <sipunkumar85@gmail.com>

--- packages/SystemUI/src/com/android/systemui/tuner/DemoModeFragment.java
+++ packages/SystemUI/src/com/android/systemui/tuner/DemoModeFragment.java
@@ -25,7 +25,7 @@ import androidx.preference.Preference;
 import androidx.preference.Preference.OnPreferenceChangeListener;
 import androidx.preference.PreferenceFragment;
 import androidx.preference.PreferenceScreen;
-import androidx.preference.SwitchPreference;
+import androidx.preference.SwitchPreferenceCompat;
 
 import com.android.internal.logging.MetricsLogger;
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
@@ -51,8 +51,8 @@ public class DemoModeFragment extends PreferenceFragment implements OnPreference
         "managed_profile",
     };
 
-    private SwitchPreference mEnabledSwitch;
-    private SwitchPreference mOnSwitch;
+    private SwitchPreferenceCompat mEnabledSwitch;
+    private SwitchPreferenceCompat mOnSwitch;
 
     private DemoModeController mDemoModeController;
     private GlobalSettings mGlobalSettings;
@@ -70,10 +70,10 @@ public class DemoModeFragment extends PreferenceFragment implements OnPreference
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         Context context = getContext();
-        mEnabledSwitch = new SwitchPreference(context);
+        mEnabledSwitch = new SwitchPreferenceCompat(context);
         mEnabledSwitch.setTitle(R.string.enable_demo_mode);
         mEnabledSwitch.setOnPreferenceChangeListener(this);
-        mOnSwitch = new SwitchPreference(context);
+        mOnSwitch = new SwitchPreferenceCompat(context);
         mOnSwitch.setTitle(R.string.show_demo_mode);
         mOnSwitch.setEnabled(false);
         mOnSwitch.setOnPreferenceChangeListener(this);
